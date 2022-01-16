7f7797f2d5fa641d804a66f033cc0bba38ba8d58
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-01-16 23:31:16 +0100

    fix: homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export const load = async () => ({
+		status: 302,
+		redirect: '/login'
+	});
+</script>
