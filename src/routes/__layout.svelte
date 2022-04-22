04324c24f89cef2c82320395a6e833af881d9a40
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-04-22 12:11:48 +0200

    fix: remove layout reset

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,6 +10,8 @@
     import { onMount } from 'svelte';
     import Notifications from '$lib/layout/notifications.svelte';
     import { base } from '$app/paths';
+    import { browser } from '$app/env';
+    import { app } from '$lib/stores/app';
 
     onMount(async () => {
         try {
@@ -21,9 +23,15 @@
                 await goto(`${base}/console`);
             }
         } catch (error) {
-            console.error(error);
+            await goto(`${base}/login`);
         }
     });
+
+    $: {
+        if (browser) {
+            document.body.setAttribute('class', `theme-${$app.theme}`);
+        }
+    }
 </script>
 
 <Notifications />
---
+++ src/routes/console/__layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Shell from '$lib/layout/shell.svelte';
+    import SideNavigation from '$lib/layout/navigation.svelte';
+    import Header from '$lib/layout/header.svelte';
+    import Footer from '$lib/layout/footer.svelte';
+</script>
+
+<Shell>
+    <svelte:fragment slot="header">
+        <Header />
+    </svelte:fragment>
+    <svelte:fragment slot="side">
+        <SideNavigation />
+    </svelte:fragment>
+    <slot />
+    <Footer />
+</Shell>
