89f176d0748d7017517524a7a7a8837e92d124ac
Arman <arman@appwrite.io>
2022-05-06 10:57:09 +0200

    test: drop list test

---
+++ tests/unit/components/dropList.test.ts
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom';
+import { render, fireEvent } from '@testing-library/svelte';
+import { DropList } from '../../../src/lib/components';
+
+const data = {
+    show: true,
+    position: 'top',
+    horizontal: 'right'
+};
+
+test('shows drop list component', () => {
+    render(DropList, { ...data });
+    const wrapper = document.querySelector('div');
+    const dropList = document.querySelector('ul');
+
+    expect(wrapper).toBeInTheDocument();
+    expect(dropList).toBeInTheDocument();
+});
+
+test('hide drop list on body click', async () => {
+    render(DropList, { ...data });
+    const body = document.querySelector('body');
+    const dropList = document.querySelector('ul');
+
+    await fireEvent.click(body);
+    expect(dropList).not.toBeInTheDocument();
+});
+
+test('hide drop list on wrapper click', async () => {
+    render(DropList, { ...data });
+    const wrapper = document.querySelector('div');
+    const dropList = document.querySelector('ul');
+
+    await fireEvent.click(wrapper);
+    expect(dropList).not.toBeInTheDocument();
+});
