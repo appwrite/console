dadbd17d36a5ee836e76eb420e8eda8e976d9268
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-05-14 20:11:48 +0200

    fix charts

--- src/lib/charts/line.svelte
+++ src/lib/charts/line.svelte
@@ -1,44 +1,6 @@
 <script lang="ts">
-    import 'chart.js/auto';
-    import { Chart, type ChartDataset } from 'chart.js';
-    import { onMount } from 'svelte';
-    let element: HTMLCanvasElement;
-
     export let labels: string[];
-    export let datasets: ChartDataset<'line'>[];
-
-    onMount(() => {
-        new Chart(element, {
-            type: 'line',
-            data: {
-                datasets,
-                labels
-            },
-            options: {
-                scales: {
-                    x: {
-                        display: false
-                    },
-                    y: {
-                        min: 0
-                    }
-                },
-                plugins: {
-                    tooltip: {
-                        mode: 'index',
-                        intersect: false,
-                        caretPadding: 0
-                    }
-                }
-            }
-        });
-    });
+    export let datasets: any[];
 </script>
 
-<canvas bind:this={element} />
-
-<style>
-    canvas {
-        max-height: 12rem;
-    }
-</style>
+<canvas />
