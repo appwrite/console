1c70598d3f0391524ca082fbc469fe83fde8400d
Arman <arman@appwrite.io>
2022-05-16 17:10:01 +0200

    feat: added scroll functionality to buttons

--- src/lib/components/tabs.svelte
+++ src/lib/components/tabs.svelte
@@ -1,9 +1,33 @@
+<script>
+    function slide(direction) {
+        let container = document.getElementById('tabs-list');
+        let scrollCompleted = 0;
+        let slideVar = setInterval(function () {
+            if (direction == 'left') {
+                container.scrollLeft -= 10;
+            } else {
+                container.scrollLeft += 10;
+            }
+            scrollCompleted += 10;
+            if (scrollCompleted >= 100) {
+                window.clearInterval(slideVar);
+            }
+        }, 10);
+    }
+</script>
+
 <div class="tabs">
-    <button class="tabs-button-scroll is-start" aria-label="Show items in start side"
+    <button
+        class="tabs-button-scroll is-start"
+        aria-label="Show items in start side"
+        on:click={() => slide('left')}
         ><span class="icon-cheveron-left" aria-hidden="true" /></button>
-    <button class="tabs-button-scroll is-end" aria-label="Show items in end side"
+    <button
+        class="tabs-button-scroll is-end"
+        aria-label="Show items in end side"
+        on:click={() => slide('right')}
         ><span class="icon-cheveron-right" aria-hidden="true" /></button>
-    <ul class="tabs-list scroll-shadow-horizontal">
+    <ul id="tabs-list" class="tabs-list scroll-shadow-horizontal">
         <slot />
     </ul>
 </div>
