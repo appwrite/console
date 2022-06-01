4618ead22e04f973741df89d5b0149a25f2547ad
Arman <arman@appwrite.io>
2022-06-01 16:45:36 +0200

    feat: moved set user limit to security page

--- src/routes/console/[project]/users/security.svelte
+++ src/routes/console/[project]/users/security.svelte
@@ -1,9 +1,23 @@
 <script>
     import { Card } from '$lib/components';
     import { Container } from '$lib/layout';
+    import SetUserLimit from './_setUserLimit.svelte';
+    import { project } from '../store';
+
+    $: authLimit = $project.authLimit;
+    let showUserLimitModal = false;
 </script>
 
 <Container>
+    <p>
+        {authLimit ? `${authLimit} Users allowed` : 'Unlimited Users'}
+        <button on:click={() => (showUserLimitModal = true)} class=" is-text link">
+            <span class="text">{authLimit ? 'Change Limit' : 'Set Limit'}</span>
+        </button>
+    </p>
+
+    <SetUserLimit {authLimit} bind:showUserLimitModal />
+
     <h7 class="heading-level-7">Users</h7>
     <Card />
     <h7 class="heading-level-7">Session length</h7>
