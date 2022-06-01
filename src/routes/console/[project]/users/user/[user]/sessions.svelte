d767950657d0b8b164fba5559b5c63585424ecdb
Arman <arman@appwrite.io>
2022-06-01 18:50:52 +0200

    feat: start work on new session layout

--- src/routes/console/[project]/users/user/[user]/sessions.svelte
+++ src/routes/console/[project]/users/user/[user]/sessions.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import { Card, List, ListItem, Pagination } from '$lib/components';
+    import { Card, Empty, List, ListItem, Pagination } from '$lib/components';
     import { Button } from '$lib/elements/forms';
     import { Container } from '$lib/layout';
     import { addNotification } from '$lib/stores/notifications';
@@ -36,45 +36,58 @@
 </script>
 
 <Container>
-    <h1>Sessions</h1>
     {#await request}
         <div aria-busy="true" />
     {:then response}
-        <Card>
-            <List>
-                {#each response.sessions as session}
-                    <ListItem
-                        avatar={sdkForProject.avatars
-                            .getBrowser(session.clientCode, 64, 64)
-                            .toString()}>
-                        <svelte:fragment slot="header">
-                            <h2 class="sessions-item-title">
-                                <span class="text">
-                                    <span class="browser-name">{session.clientName}</span>
-                                    <span class="browser-version">{session.clientVersion}</span>
-                                    <span>on</span>
-                                    <span class="browser-os">{session.osName}</span>
-                                    <span class="browser-os-version">{session.osVersion}</span>
-                                </span>
-                            </h2>
-                            {#if session.current}
-                                <span class="pill is-success">Current Session</span>
-                            {/if}
-                        </svelte:fragment>
-                        <svelte:fragment slot="info">
-                            {session.ip} / {session.countryName}
-                        </svelte:fragment>
-                        <svelte:fragment slot="action">
-                            <Button danger on:click={() => deleteSession(session.$id)}
-                                >Logout</Button>
-                        </svelte:fragment>
-                    </ListItem>
-                {:else}
-                    <p>No sessions available.</p>
-                {/each}
-            </List>
+        {#if response.total}
+            <Card>
+                <List>
+                    {#each response.sessions as session}
+                        <ListItem
+                            avatar={sdkForProject.avatars
+                                .getBrowser(session.clientCode, 64, 64)
+                                .toString()}>
+                            <svelte:fragment slot="header">
+                                <h2 class="sessions-item-title">
+                                    <span class="text">
+                                        <span class="browser-name">{session.clientName}</span>
+                                        <span class="browser-version">{session.clientVersion}</span>
+                                        <span>on</span>
+                                        <span class="browser-os">{session.osName}</span>
+                                        <span class="browser-os-version">{session.osVersion}</span>
+                                    </span>
+                                </h2>
+                                {#if session.current}
+                                    <span class="pill is-success">Current Session</span>
+                                {/if}
+                            </svelte:fragment>
+                            <svelte:fragment slot="info">
+                                {session.ip} / {session.countryName}
+                            </svelte:fragment>
+                            <svelte:fragment slot="action">
+                                <Button danger on:click={() => deleteSession(session.$id)}
+                                    >Logout</Button>
+                            </svelte:fragment>
+                        </ListItem>
+                    {/each}
+                </List>
+            </Card>
+        {:else}
+            <Empty centered>
+                <div class="u-flex u-flex-vertical u-cross-center">
+                    <div class="common-section">
+                        <p>No Session Available</p>
+                    </div>
+                    <div class="common-section">
+                        <Button secondary href="#">Documentation</Button>
+                    </div>
+                </div>
+            </Empty>
+        {/if}
+        <div class="u-flex common-section u-main-space-between">
+            <p class="text">Total results: {response.total}</p>
             <Pagination {limit} bind:offset sum={response.total} />
-        </Card>
+        </div>
         {#if response.total}
             <Button danger on:click={deleteAllSessions}>Logout from all sessions</Button>
         {/if}
