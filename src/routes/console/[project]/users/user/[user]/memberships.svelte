1bf9a5475f8e089e4d1d7923a2ce40d27041c418
Arman <arman@appwrite.io>
2022-06-08 17:18:16 +0200

    feat: update membership page

--- src/routes/console/[project]/users/user/[user]/memberships.svelte
+++ src/routes/console/[project]/users/user/[user]/memberships.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import { Card, Pagination, Empty } from '$lib/components';
+    import { Pagination, Empty } from '$lib/components';
     import {
         Table,
         TableHeader,
         TableBody,
         TableRow,
-        TableCell,
         TableCellHead,
         TableCellLink,
         TableCellText
     } from '$lib/elements/table';
     import { Button } from '$lib/elements/forms';
     import { Container } from '$lib/layout';
-    // import { addNotification } from '$lib/stores/notifications';
+    import { base } from '$app/paths';
     import { sdkForProject } from '$lib/stores/sdk';
+
     const getAvatar = (name: string) => sdkForProject.avatars.getInitials(name, 30, 30).toString();
+    const project = $page.params.project;
 
     let offset = 0;
     let showDeleteAll = false;
@@ -32,40 +33,43 @@
         {#if response.total}
             <div class="u-flex u-main-space-end  common-section">
                 <Button secondary on:click={() => (showDeleteAll = true)}>
-                    <span class="icon-plus" aria-hidden="true" />
                     <span class="text">Delete All</span>
                 </Button>
             </div>
-            <Card>
-                <Table>
-                    <TableHeader>
-                        <TableCellHead width={30} />
-                        <TableCellHead>Name</TableCellHead>
-                        <TableCellHead>Role</TableCellHead>
-                        <TableCellHead width={30} />
-                    </TableHeader>
-                    <TableBody>
-                        {#each response.memberships as membership}
-                            <TableRow>
-                                <TableCell onlyDesktop>
-                                    <div class="image">
-                                        <img
-                                            class="avatar"
-                                            width="30"
-                                            height="30"
-                                            src={getAvatar(membership.userName)}
-                                            alt={membership.userName} />
-                                    </div>
-                                </TableCell>
-                                <TableCellLink href="#" title="Name">
-                                    {membership.userName ? membership.userName : 'n/a'}
-                                </TableCellLink>
-                                <TableCellText title="Role">ROLE</TableCellText>
-                            </TableRow>
-                        {/each}
-                    </TableBody>
-                </Table>
-            </Card>
+            <Table>
+                <TableHeader>
+                    <TableCellHead>Name</TableCellHead>
+                    <TableCellHead>Role</TableCellHead>
+                    <TableCellHead width={30} />
+                </TableHeader>
+                <TableBody>
+                    {#each response.memberships as membership}
+                        <TableRow>
+                            <TableCellLink
+                                href={`${base}/console/${project}/users/user/${membership.userId}`}
+                                title="Name">
+                                <div class="u-flex u-gap-12">
+                                    <img
+                                        class="avatar"
+                                        width="40"
+                                        height="40"
+                                        src={getAvatar(membership.userName)}
+                                        alt={membership.userName} />
+                                    <span>{membership.userName ? membership.userName : 'n/a'}</span>
+                                </div>
+                            </TableCellLink>
+                            <TableCellText title="Role">{membership.roles}</TableCellText>
+                            <TableCellText title="">
+                                <button
+                                    class="button is-only-icon is-text"
+                                    aria-label="Delete item">
+                                    <span class="icon-trash" aria-hidden="true" />
+                                </button>
+                            </TableCellText>
+                        </TableRow>
+                    {/each}
+                </TableBody>
+            </Table>
         {:else}
             <Empty centered>
                 <div class="u-flex u-flex-vertical u-cross-center">
