759e0fe59a1a0f0cee15eb4aaf8847699c300f3f
Arman <arman@appwrite.io>
2022-06-13 14:42:48 +0200

    fix: now work as intented

--- src/lib/components/alert.svelte
+++ src/lib/components/alert.svelte
@@ -1,43 +1,20 @@
 <script lang="ts">
     import { createEventDispatcher } from 'svelte';
-    import type { Notification } from '../stores/notifications';
 
     export let dismissible = false;
-    export let type: Notification['type'] = 'info';
-
-    let icon: string;
-    switch (type) {
-        case 'success':
-            icon = 'check-circle';
-            break;
-        case 'warning':
-            icon = 'exclamation';
-            break;
-
-        case 'error':
-            icon = 'exclamation-circle';
-            break;
-
-        case 'info':
-            icon = 'info';
-            break;
-
-        default:
-            icon = '';
-            break;
-    }
+    export let type: '' | 'info' | 'success' | 'warning' | 'danger' = 'info';
 
     const dispatch = createEventDispatcher();
 </script>
 
-<section class="alert is-info common-section">
+<section class={`alert is-${type} common-section`}>
     <div class="alert-grid">
         {#if dismissible}
             <button
                 class="x-button"
                 aria-label="close alert box"
                 on:click={() => dispatch('dismiss')}>
-                <span class={`icon-${icon}`} aria-hidden="true" />
+                <span class="icon-x" aria-hidden="true" />
             </button>
         {/if}
         <span class="icon-info" aria-hidden="true" />
