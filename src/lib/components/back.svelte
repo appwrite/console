82df90a10c43a6be1d2ad71f25b5c2743f9d05f3
Arman <arman@appwrite.io>
2022-06-14 11:00:59 +0200

    feat: update back form link to button

--- src/lib/components/back.svelte
+++ src/lib/components/back.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+    import { goto } from '$app/navigation';
+
     export let href: string;
+
+    function goBack() {
+        if (history.length > 1) {
+            history.back();
+        } else goto(href);
+    }
 </script>
 
-<a {href}>
+<button on:click={() => goBack()}>
     <i class="icon-cheveron-left" />
     <slot />
-</a>
+</button>
