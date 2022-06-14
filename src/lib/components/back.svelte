76ef1ea0d66bd75d503e6773075c7daff163328c
Arman <arman@appwrite.io>
2022-06-14 11:56:09 +0200

    fix: revert changes to back component

--- src/lib/components/back.svelte
+++ src/lib/components/back.svelte
@@ -1,16 +1,8 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
-
     export let href: string;
-
-    function goBack() {
-        if (history.length > 1) {
-            history.back();
-        } else goto(href);
-    }
 </script>
 
-<button on:click={() => goBack()}>
+<a {href}>
     <i class="icon-cheveron-left" />
     <slot />
-</button>
+</a>
