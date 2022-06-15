0208e2abe6790323572ccc14d3911cb3253cc1e4
Arman <arman@appwrite.io>
2022-06-15 15:09:52 +0200

    feat: session(s) can now be deleted

---
+++ src/routes/console/[project]/users/user/[user]/_deleteAllSessions.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import { Modal } from '$lib/components';
+    import { Button, Form } from '$lib/elements/forms';
+    import { addNotification } from '$lib/stores/notifications';
+    import { sdkForProject } from '$lib/stores/sdk';
+    import { user } from './store';
+
+    export let showDeleteAll = false;
+
+    const deleteUser = async () => {
+        try {
+            await sdkForProject.users.deleteSessions($page.params.user);
+            showDeleteAll = false;
+        } catch (error) {
+            addNotification({
+                type: 'error',
+                message: error.message
+            });
+        }
+    };
+</script>
+
+<Form on:submit={deleteUser}>
+    <Modal bind:show={showDeleteAll}>
+        <svelte:fragment slot="header">Delete All Sessions</svelte:fragment>
+
+        <p>
+            Are you sure you want to delete <b>all of {$user.name}'s sessions?</b>
+        </p>
+        <svelte:fragment slot="footer">
+            <Button text on:click={() => (showDeleteAll = false)}>Cancel</Button>
+            <Button secondary submit>Delete</Button>
+        </svelte:fragment>
+    </Modal>
+</Form>
--- src/routes/console/[project]/users/user/[user]/sessions.svelte
+++ src/routes/console/[project]/users/user/[user]/sessions.svelte
@@ -1,37 +1,27 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import { Card, Empty, List, ListItem, Pagination } from '$lib/components';
+    import { Empty, Pagination } from '$lib/components';
+    import {
+        Table,
+        TableBody,
+        TableHeader,
+        TableRow,
+        TableCellHead,
+        TableCellText
+    } from '$lib/elements/table';
     import { Button } from '$lib/elements/forms';
     import { Container } from '$lib/layout';
-    import { addNotification } from '$lib/stores/notifications';
     import { sdkForProject } from '$lib/stores/sdk';
+    import DeleteAllSessions from './_deleteAllSessions.svelte';
+    import DeleteSessions from './_deleteSession.svelte';
+    import { toLocaleDateTime } from '$lib/helpers/date';
 
     let offset = 0;
+    let showDelete = false;
     let showDeleteAll = false;
+    let selectedSessionId: string = null;
 
-    const limit = 25;
-    const deleteSession = async (id: string) => {
-        try {
-            await sdkForProject.users.deleteSession($page.params.user, id);
-        } catch (error) {
-            addNotification({
-                type: 'error',
-                message: error.message
-            });
-        }
-    };
-    const deleteAllSessions = async () => {
-        try {
-            if (confirm('You really want to delete all sessions?')) {
-                await sdkForProject.users.deleteSessions($page.params.user);
-            }
-        } catch (error) {
-            addNotification({
-                type: 'error',
-                message: error.message
-            });
-        }
-    };
+    const limit = 5;
 
     $: request = sdkForProject.users.getSessions($page.params.user);
 </script>
@@ -47,38 +37,48 @@
                     <span class="text">Delete All</span>
                 </Button>
             </div>
-            <Card>
-                <List>
+            <Table>
+                <TableHeader>
+                    <TableCellHead>Browser</TableCellHead>
+                    <TableCellHead>Device</TableCellHead>
+                    <TableCellHead>IP</TableCellHead>
+                    <TableCellHead width={30} />
+                </TableHeader>
+                <TableBody>
                     {#each response.sessions as session}
-                        <ListItem
-                            avatar={sdkForProject.avatars
-                                .getBrowser(session.clientCode, 64, 64)
-                                .toString()}>
-                            <svelte:fragment slot="header">
-                                <h2 class="sessions-item-title">
-                                    <span class="text">
-                                        <span class="browser-name">{session.clientName}</span>
-                                        <span class="browser-version">{session.clientVersion}</span>
-                                        <span>on</span>
-                                        <span class="browser-os">{session.osName}</span>
-                                        <span class="browser-os-version">{session.osVersion}</span>
+                        <TableRow>
+                            <TableCellText title="Client">
+                                <img
+                                    height="32"
+                                    width="32"
+                                    src={`/icons/color/${session?.clientName.toLocaleLowerCase()}.svg`}
+                                    alt={session.clientName} />
+                                <span class="">
+                                    <p>
+                                        {session.clientName}
+                                    </p>
+                                    <span class="u-small">
+                                        Last activity: {toLocaleDateTime(session.lastActivity)}
                                     </span>
-                                </h2>
-                                {#if session.current}
-                                    <span class="pill is-success">Current Session</span>
-                                {/if}
-                            </svelte:fragment>
-                            <svelte:fragment slot="info">
-                                {session.ip} / {session.countryName}
-                            </svelte:fragment>
-                            <svelte:fragment slot="action">
-                                <Button danger on:click={() => deleteSession(session.$id)}
-                                    >Logout</Button>
-                            </svelte:fragment>
-                        </ListItem>
+                                </span>
+                            </TableCellText>
+
+                            <TableCellText title="IP">{session.ip}</TableCellText>
+                            <TableCellText title="">
+                                <button
+                                    class="button is-only-icon is-text"
+                                    aria-label="Delete item"
+                                    on:click={() => {
+                                        selectedSessionId = session.$id;
+                                        showDelete = true;
+                                    }}>
+                                    <span class="icon-trash" aria-hidden="true" />
+                                </button>
+                            </TableCellText>
+                        </TableRow>
                     {/each}
-                </List>
-            </Card>
+                </TableBody>
+            </Table>
         {:else}
             <Empty centered>
                 <div class="u-flex u-flex-vertical u-cross-center">
@@ -99,8 +99,8 @@
             <p class="text">Total results: {response.total}</p>
             <Pagination {limit} bind:offset sum={response.total} />
         </div>
-        {#if response.total}
-            <Button danger on:click={deleteAllSessions}>Logout from all sessions</Button>
-        {/if}
     {/await}
 </Container>
+
+<DeleteSessions {selectedSessionId} bind:showDelete />
+<DeleteAllSessions bind:showDeleteAll />
