fdefd3cf70aa85621355b6b4747e60650122bbac
Arman <arman@appwrite.io>
2022-06-20 15:51:39 +0200

    chore: remove unused import

--- src/routes/console/[project]/storage/index.svelte
+++ src/routes/console/[project]/storage/index.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { page } from '$app/stores';
     import { goto } from '$app/navigation';
-    import { sdkForProject } from '$lib/stores/sdk';
     import { Button } from '$lib/elements/forms';
     import { Empty, Pagination, Tiles, Tooltip } from '$lib/components';
     import { Pill } from '$lib/elements';
