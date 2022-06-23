75be1d7c1d607bb8ae46b2b1b169cada0e64fc48
Arman <arman@appwrite.io>
2022-06-23 17:45:56 +0200

    layout fix

--- src/routes/console/[project]/users/usage.svelte
+++ src/routes/console/[project]/users/usage.svelte
@@ -15,32 +15,34 @@
 
 <Container>
     {#if data}
-        <ul class="drop-tabs">
-            <li class="drop-tabs-item">
-                <button
-                    class="drop-tabs-button"
-                    on:click={() => (currentRange = '24h')}
-                    disabled={currentRange === '24h'}>
-                    <span class="text">24h</span>
-                </button>
-            </li>
-            <li class="drop-tabs-item">
-                <button
-                    class="drop-tabs-button"
-                    on:click={() => (currentRange = '30d')}
-                    disabled={currentRange === '30d'}>
-                    <span class="text">30d</span>
-                </button>
-            </li>
-            <li class="drop-tabs-item">
-                <button
-                    class="drop-tabs-button"
-                    on:click={() => (currentRange = '90d')}
-                    disabled={currentRange === '90d'}>
-                    <span class="text">90d</span>
-                </button>
-            </li>
-        </ul>
+        <div class="u-flex u-main-end common-section">
+            <ul class="drop-tabs">
+                <li class="drop-tabs-item">
+                    <button
+                        class="drop-tabs-button"
+                        on:click={() => (currentRange = '24h')}
+                        disabled={currentRange === '24h'}>
+                        <span class="text">24h</span>
+                    </button>
+                </li>
+                <li class="drop-tabs-item">
+                    <button
+                        class="drop-tabs-button"
+                        on:click={() => (currentRange = '30d')}
+                        disabled={currentRange === '30d'}>
+                        <span class="text">30d</span>
+                    </button>
+                </li>
+                <li class="drop-tabs-item">
+                    <button
+                        class="drop-tabs-button"
+                        on:click={() => (currentRange = '90d')}
+                        disabled={currentRange === '90d'}>
+                        <span class="text">90d</span>
+                    </button>
+                </li>
+            </ul>
+        </div>
         <Card>
             <h6 class="heading-level-6">Users</h6>
             <p>Count of users over time</p>
