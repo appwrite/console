55ca344ec0354b7b84571f53e7fb92dd05108ac2
Arman <arman@appwrite.io>
2022-06-24 13:40:10 +0200

    feat: add tooltip on copy

--- src/lib/components/copy.svelte
+++ src/lib/components/copy.svelte
@@ -3,13 +3,15 @@
 
     export let value: string;
 
+    let showTooltip = 'none';
+
     const copy = async () => {
         try {
             await navigator.clipboard.writeText(value);
-            addNotification({
-                message: 'Copied to clipboard.',
-                type: 'success'
-            });
+            showTooltip = 'block';
+            setTimeout(() => {
+                showTooltip = 'none';
+            }, 1000);
         } catch (error) {
             addNotification({
                 message: error.message,
@@ -21,7 +23,12 @@
 
 <div class="input-text-wrapper is-with-end-button">
     <input {value} type="text" class="input-text" disabled />
-    <button type="button" class="input-button" aria-label="Click to copy." on:click={copy}>
+    <button type="button" class="input-button tooltip" aria-label="Click to copy." on:click={copy}>
         <span class="icon-duplicate" aria-hidden="true" />
+        <div class="tootip">
+            <span class="tooltip-popup" style={`display: ${showTooltip}`} role="tooltip">
+                Copied
+            </span>
+        </div>
     </button>
 </div>
