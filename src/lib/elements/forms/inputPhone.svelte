bf42ba7b9d1737eac67178625e13792f267a7576
Arman <arman@appwrite.io>
2022-07-05 11:24:50 +0200

    feat: create inputPhone & test

---
+++ src/lib/elements/forms/inputPhone.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { FormItem } from '.';
+
+    export let label: string;
+    export let id: string;
+    export let value = '';
+    export let placeholder = '';
+    export let required = false;
+    export let disabled = false;
+    export let autofocus = false;
+    export let autocomplete = true;
+    export let maxlength: number = null;
+
+    let element: HTMLInputElement;
+
+    onMount(() => {
+        if (element && autofocus) {
+            element.focus();
+        }
+    });
+</script>
+
+<FormItem>
+    <label class="label" for={id}>{label}</label>
+    <div class="input-text-wrapper">
+        <input
+            {id}
+            {placeholder}
+            {disabled}
+            {required}
+            {maxlength}
+            autocomplete={autocomplete ? 'on' : 'off'}
+            type="tel"
+            class="input-text"
+            bind:value
+            bind:this={element} />
+    </div>
+</FormItem>
---
+++ tests/unit/elements/inputPhone.test.ts
@@ -0,0 +1,54 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
+import { InputPhone } from '../../../src/lib/elements/forms';
+
+test('shows phone input', () => {
+    const { getByText, getByLabelText } = render(InputPhone, { id: 'input', label: 'input' });
+    const input = getByLabelText('input');
+
+    expect(getByText('input')).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute('type', 'tel');
+});
+
+test('shows phone input - required', () => {
+    const { getByLabelText } = render(InputPhone, { id: 'input', label: 'input', required: true });
+
+    expect(getByLabelText('input')).toBeRequired();
+});
+
+test('shows phone input - disabled', () => {
+    const { getByLabelText } = render(InputPhone, { id: 'input', label: 'input', disabled: true });
+
+    expect(getByLabelText('input')).toBeDisabled();
+});
+
+test('shows phone input - autofocus', () => {
+    const { getByLabelText } = render(InputPhone, { id: 'input', label: 'input', autofocus: true });
+
+    expect(getByLabelText('input')).toHaveFocus();
+});
+
+test('shows phone input - placeholder', () => {
+    const { getByPlaceholderText } = render(InputPhone, {
+        id: 'input',
+        label: 'input',
+        placeholder: 'find me'
+    });
+
+    expect(getByPlaceholderText('find me')).toBeInTheDocument();
+});
+
+test('state', async () => {
+    const { component, getByLabelText } = render(InputPhone, {
+        id: 'input',
+        label: 'input',
+        value: ''
+    });
+    const input = getByLabelText('input');
+
+    expect(component.value).toEqual('');
+    await userEvent.type(input, 'lorem');
+    expect(component.value).toEqual('lorem');
+});
