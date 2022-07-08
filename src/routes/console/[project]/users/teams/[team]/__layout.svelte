da3d9ad7dd75b631e28d81ed946ab0ee3f297d8d
Arman <arman@appwrite.io>
2022-07-08 15:33:33 +0200

    fix: `handle` is called when store changes

--- src/routes/console/[project]/users/teams/[team]/__layout.svelte
+++ src/routes/console/[project]/users/teams/[team]/__layout.svelte
@@ -42,6 +42,10 @@
             }
         ]);
     }
+
+    $: if ($team) {
+        handle();
+    }
 </script>
 
 <svelte:head>
--- src/routes/console/[project]/users/user/[user]/__layout.svelte
+++ src/routes/console/[project]/users/user/[user]/__layout.svelte
@@ -46,6 +46,10 @@
             }
         ]);
     }
+
+    $: if ($user) {
+        handle();
+    }
 </script>
 
 <svelte:head>
