403ccd8720de98204ea0b98ce57e0e0a4141d4b9
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-07-11 13:33:59 +0200

    fix: charts chunked import

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -27,6 +27,7 @@ test('login page has a working sign up link', async ({ page }) => {
     const signup = page.locator('a[href="/register"]');
     expect(await signup.isVisible());
     await signup.click();
+    await page.waitForNavigation({ waitUntil: 'networkidle' });
     expect(page.url()).toContain('/register');
     expect(await page.locator('Register').isVisible());
 });
