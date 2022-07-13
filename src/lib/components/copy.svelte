b5931df942b55c9703b0ced4f675ab1722e96163
Arman <arman@appwrite.io>
2022-07-13 10:30:00 +0200

    fix: text & remove timeout to change text

--- src/lib/components/copy.svelte
+++ src/lib/components/copy.svelte
@@ -2,15 +2,12 @@
     import { addNotification } from '$lib/stores/notifications';
 
     export let value: string;
-    let text = 'Copy ID';
+    let text = 'Click to copy';
 
     const copy = async () => {
         try {
             await navigator.clipboard.writeText(value);
-            text = 'Copied ID';
-            setTimeout(() => {
-                text = 'Copy ID';
-            }, 2000);
+            text = 'Copied';
         } catch (error) {
             addNotification({
                 message: error.message,
@@ -20,7 +17,10 @@
     };
 </script>
 
-<span class="tooltip " on:mouseenter={() => (text = 'Copy ID')} on:click|preventDefault={copy}>
+<span
+    class="tooltip "
+    on:mouseenter={() => (text = 'Click to copy')}
+    on:click|preventDefault={copy}>
     <slot />
     <span class="tooltip-popup is-bottom" role="tooltip"> {text} </span>
 </span>
