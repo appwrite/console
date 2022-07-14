cbf21b15a19ed9b8428589f787bc2ad00d8409d1
Arman <arman@appwrite.io>
2022-07-14 17:55:08 +0200

    feat: update table with sticky column

--- src/lib/elements/table/cell.svelte
+++ src/lib/elements/table/cell.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     export let onlyDesktop = false;
     export let width: number = null;
+    export let main = false;
 </script>
 
 <div
     width={width?.toString() ?? ''}
+    class:is-main={main}
     class="table-col"
     class:is-only-desktop={onlyDesktop}
     role="cell">
--- src/lib/elements/table/table.svelte
+++ src/lib/elements/table/table.svelte
@@ -1,3 +1,7 @@
-<div class="table is-vertical u-margin-block-start-32" role="table">
+<script>
+    export let sticky = false;
+</script>
+
+<div class:is-sticky-scroll={sticky} class="table is-vertical u-margin-block-start-32" role="table">
     <slot />
 </div>
