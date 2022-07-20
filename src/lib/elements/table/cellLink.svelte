95918690ffa089bff7b1901190f95fc150603892
Arman <arman@appwrite.io>
2022-07-20 13:32:58 +0200

    fix: made main optional

--- src/lib/elements/table/cellLink.svelte
+++ src/lib/elements/table/cellLink.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     export let href: string;
     export let title: string;
+    export let main = false;
 </script>
 
-<div class="table-col is-main" data-title={title} role="cell">
+<div class="table-col" class:is-main={main} data-title={title} role="cell">
     <a role="button" tabindex="0" class="link" {href}><slot /></a>
 </div>
