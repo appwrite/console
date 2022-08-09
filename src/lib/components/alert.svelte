308d0bbee9c85a851eafe40629e58c156907f076
Arman <arman@appwrite.io>
2022-08-09 13:22:21 +0200

    fix: icon in alert

--- src/lib/components/alert.svelte
+++ src/lib/components/alert.svelte
@@ -6,10 +6,24 @@
     export let type: '' | 'info' | 'success' | 'warning' | 'danger' = 'info';
     export let buttons: Buttons[] = [];
 
+    let icon = {
+        success: 'icon-check-circle',
+        warning: 'icon-exclamation',
+        error: 'icon-exclamation-circle',
+        info: 'icon-info'
+    };
+
+    let typeClass = {
+        success: 'is-success',
+        warning: 'is-warning',
+        error: 'is-danger',
+        info: 'is-info'
+    };
+
     const dispatch = createEventDispatcher();
 </script>
 
-<section class={`alert is-${type} `}>
+<section class={`alert ${type ? typeClass[type] : ''}`}>
     <div class="alert-grid">
         {#if dismissible}
             <button
@@ -19,7 +33,7 @@
                 <span class="icon-x" aria-hidden="true" />
             </button>
         {/if}
-        <span class="icon-info" aria-hidden="true" />
+        <span class={type ? icon[type] : 'icon-info'} aria-hidden="true" />
         <div class="content">
             {#if $$slots.title}
                 <h6 class="title">
