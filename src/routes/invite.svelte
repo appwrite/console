8ce7c6040bf631f1c6feece9ceb1123b092b6e28
Arman <arman@appwrite.io>
2022-09-06 18:34:47 +0200

    feat: accept invite function

--- src/routes/invite.svelte
+++ src/routes/invite.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
     import { base } from '$app/paths';
-    import { Button, Form, FormList, InputCheckbox } from '$lib/elements/forms';
+    import { Button, Form, FormList, InputChoice } from '$lib/elements/forms';
     import { addNotification } from '$lib/stores/notifications';
     import { sdkForConsole } from '$lib/stores/sdk';
     import { user } from '$lib/stores/user';
     import { Unauthenticated } from '$lib/layout';
     import { page } from '$app/stores';
+    import { onMount } from 'svelte';
 
+    onMount(() => {
+        userId = $page.url.searchParams.get('userId');
+        secret = $page.url.searchParams.get('secret');
+        teamId = $page.url.searchParams.get('teamId');
+        membershipId = $page.url.searchParams.get('membershipId');
+    });
+
+    let teamId: string, membershipId: string, userId: string, secret: string;
     let terms = false;
     const acceptInvite = async () => {
-        console.log($page);
         try {
-            // await sdkForConsole.account.createEmailSession(mail, pass);
+            await sdkForConsole.teams.updateMembershipStatus(teamId, membershipId, userId, secret);
             user.fetchUser();
             addNotification({
                 type: 'success',
@@ -37,10 +45,20 @@
     <svelte:fragment>
         <Form on:submit={acceptInvite}>
             <FormList>
-                <InputCheckbox bind:value={terms} id="terms" label="terms" />
+                <InputChoice required bind:value={terms} id="terms" label="terms" showLabel={false}>
+                    By accepting the invitation, you agree to the <a
+                        class="link"
+                        href="https://appwrite.io/policy/terms"
+                        target="_blank">Terms and Conditions</a>
+                    and
+                    <a class="link" href="https://appwrite.io/policy/privacy" target="_blank">
+                        Privacy Policy</a
+                    >.</InputChoice>
 
-                <Button secondary>Cancel</Button>
-                <Button submit>Accept</Button>
+                <div class="u-flex u-main-end u-gap-12">
+                    <Button secondary>Cancel</Button>
+                    <Button submit>Accept</Button>
+                </div>
             </FormList>
         </Form>
     </svelte:fragment>
