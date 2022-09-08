11cabddc75362656211f48bdc744e9b4a4c4099f
Arman <arman@appwrite.io>
2022-09-08 09:21:19 +0200

    feat: initial commit

--- src/routes/console/project-[project]/settings/index.svelte
+++ src/routes/console/project-[project]/settings/index.svelte
@@ -1,15 +1,31 @@
 <script lang="ts">
-    import { Card } from '$lib/components';
-    import { Button, Form, FormItem, InputText } from '$lib/elements/forms';
+    import { page } from '$app/stores';
+    import { CardGrid, CopyInput } from '$lib/components';
+    import { Button, Form, FormList, InputText } from '$lib/elements/forms';
     import { Container } from '$lib/layout';
     import { addNotification } from '$lib/stores/notifications';
     import { sdkForConsole } from '$lib/stores/sdk';
+    import { onMount } from 'svelte';
     import { project } from '../store';
 
-    const update = async () => {
+    let name: string = null;
+    const endpoint = import.meta.env.VITE_APPWRITE_ENDPOINT.toString();
+
+    onMount(async () => {
+        await project.load($page.params.project);
+        console.log($project);
+
+        name ??= $project.name;
+    });
+
+    const updateName = async () => {
         try {
-            await sdkForConsole.projects.update($project.$id, $project.name);
+            await sdkForConsole.projects.update($project.$id, name);
             await project.load($project.$id);
+            addNotification({
+                type: 'success',
+                message: 'Project name has been updated'
+            });
         } catch (error) {
             addNotification({
                 type: 'error',
@@ -20,15 +36,43 @@
 </script>
 
 <Container>
-    <h1>Overview</h1>
     {#if $project}
-        <Card>
-            <Form on:submit={update}>
-                <InputText id="name" label="Name" bind:value={$project.name} required />
-                <FormItem>
-                    <Button submit>Update</Button>
-                </FormItem>
-            </Form>
-        </Card>
+        <Form on:submit={updateName}>
+            <CardGrid>
+                <h6 class="heading-level-7">Update Name</h6>
+
+                <svelte:fragment slot="aside">
+                    <FormList>
+                        <InputText
+                            id="name"
+                            label="Name"
+                            bind:value={name}
+                            required
+                            placeholder="Enter name" />
+                    </FormList>
+                </svelte:fragment>
+
+                <svelte:fragment slot="actions">
+                    <Button disabled={name === $project.name} submit>Update</Button>
+                </svelte:fragment>
+            </CardGrid>
+        </Form>
+
+        <CardGrid>
+            <h6 class="heading-level-7">API Credentials</h6>
+            <p>
+                Access Appwrite services using your API Endpoint and Project ID. You can connect
+                Appwrite to your applications and server-side code by <a href="#/" class="link"
+                    >integrating a new platform</a>
+                or
+                <a href="#/" class="link">creating an API key</a>.
+            </p>
+            <svelte:fragment slot="aside">
+                <FormList>
+                    <CopyInput label="Project ID" showLabel={true} value={$project.$id} />
+                    <CopyInput label="API Endpoint" showLabel={true} value={endpoint} />
+                </FormList>
+            </svelte:fragment>
+        </CardGrid>
     {/if}
 </Container>
