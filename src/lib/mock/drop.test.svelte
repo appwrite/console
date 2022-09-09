c78e1938e5e708b89777ea7376a7aeea01325193
Arman <arman@appwrite.io>
2022-09-09 14:11:50 +0200

    test: create mock to fix test

---
+++ src/lib/mock/drop.test.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { DropList } from '$lib/components';
+
+    export let show = false;
+</script>
+
+<DropList bind:show>
+    <button id="button" on:click={() => (show = !show)}>test</button>
+
+    <svelte:fragment slot="list"><li id="list">list</li></svelte:fragment>
+    <svelte:fragment slot="other"><span id="other">other</span></svelte:fragment>
+</DropList>
