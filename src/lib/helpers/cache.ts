8067b608f877d0a04ede9d8beefab7b82c1e852c
Arman <arman@appwrite.io>
2022-09-12 17:38:51 +0200

    feat: updated implementation by Torsten + fix lint error by replacing `{ }` with Record

--- src/lib/helpers/cache.ts
+++ src/lib/helpers/cache.ts
@@ -1,17 +1,20 @@
 import { browser } from '$app/env';
-import type { Writable } from 'svelte/store';
+import { writable, type Writable } from 'svelte/store';
 
-type Stores = {
-    store: Partial<Writable<unknown>>;
-    name: string;
-}[];
+export function cachedStore<TModel, TMethods = Record<string, unknown>>(
+    id: string,
+    callback: (store: Writable<TModel>) => TMethods
+): TMethods & Writable<TModel> {
+    const store = writable<TModel>(browser ? JSON.parse(sessionStorage.getItem(id)) : null);
 
-export function cache(stores: Stores) {
     if (browser) {
-        stores.forEach((item) => {
-            item.store.subscribe((n) =>
-                sessionStorage?.setItem(item.name, JSON.stringify(n ?? ''))
-            );
-        });
+        store.subscribe((n) => sessionStorage?.setItem(id, JSON.stringify(n ?? '')));
     }
+
+    return {
+        subscribe: store.subscribe,
+        set: store.set,
+        update: store.update,
+        ...callback(store)
+    };
 }
