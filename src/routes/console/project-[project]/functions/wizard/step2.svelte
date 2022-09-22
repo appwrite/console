93a2609943a9f21cf62334496f8eddc5df658551
Arman <arman@appwrite.io>
2022-09-22 17:26:33 +0200

    feat: WIP step 2

--- src/routes/console/project-[project]/functions/wizard/step2.svelte
+++ src/routes/console/project-[project]/functions/wizard/step2.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
     import { WizardStep } from '$lib/layout';
+    import { Button } from '$lib/elements/forms';
+    import { DropList, DropListItem } from '$lib/components';
     // import { createFunction } from './store';
+
+    let showDropdown = false;
+    let showUsers = false;
+    let showTeams = false;
 </script>
 
 <WizardStep>
@@ -13,4 +19,19 @@
     </svelte:fragment>
 
     ROLE
+
+    <DropList bind:show={showDropdown} position="bottom" arrow={false}>
+        <Button text on:click={() => (showDropdown = !showDropdown)}>
+            <span class="icon-plus" aria-hidden="true" />
+            <span class="u-text">Add role</span>
+        </Button>
+
+        <svelte:fragment slot="list">
+            <DropListItem>Any</DropListItem>
+            <DropListItem>All guests</DropListItem>
+            <DropListItem>All users</DropListItem>
+            <DropListItem on:click={() => (showUsers = true)}>Select users</DropListItem>
+            <DropListItem on:click={() => (showTeams = true)}>Select teams</DropListItem>
+        </svelte:fragment>
+    </DropList>
 </WizardStep>
