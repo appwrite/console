c0b06a394114cec3b55e2236887161a6510a31a7
Arman <arman@appwrite.io>
2022-10-06 17:34:45 +0200

    feat: create card container, update empty

---
+++ src/lib/components/cardContainer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { Empty } from '$lib/components';
+    import { cardLimit } from '$lib/stores/layout';
+
+    export let offset = 0;
+    export let limit = $cardLimit;
+    export let total = 0;
+</script>
+
+<ul
+    class="grid-box common-section u-margin-block-start-32"
+    style={`--grid-gap:1.5rem; --grid-item-size:${total > 3 ? '22rem' : '25rem'};`}>
+    <slot />
+
+    {#if total < limit + offset && (total % 2 !== 0 || total % 4 !== 0)}
+        <Empty isButton on:click>
+            <slot name="empty" />
+        </Empty>
+    {/if}
+</ul>
