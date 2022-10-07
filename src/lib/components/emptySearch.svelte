91649ed7439f63af26f8ef7ac030629c382ca9e0
Arman <arman@appwrite.io>
2022-10-07 11:24:13 +0200

    feat: empty search component

---
+++ src/lib/components/emptySearch.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { app } from '$lib/stores/app';
+    import Light from '$lib/images/search-light.svg';
+    import Dark from '$lib/images/search-dark.svg';
+</script>
+
+<article class="card u-grid u-cross-center u-width-full-line common-section">
+    <div class="u-flex u-flex-vertical u-cross-center u-gap-24">
+        {#if $app.themeInUse === 'dark'}
+            <img src={Dark} alt="create" aria-hidden="true" />
+        {:else}
+            <img src={Light} alt="create" aria-hidden="true" />
+        {/if}
+        <slot />
+    </div>
+</article>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -13,6 +13,7 @@ export { default as UploadBox } from './uploadBox.svelte';
 export { default as List } from './list.svelte';
 export { default as ListItem } from './listItem.svelte';
 export { default as Empty } from './empty.svelte';
+export { default as EmptySearch } from './emptySearch.svelte';
 export { default as DropList } from './dropList.svelte';
 export { default as DropListItem } from './dropListItem.svelte';
 export { default as DropListLink } from './dropListLink.svelte';
