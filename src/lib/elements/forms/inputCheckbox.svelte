42b31f307364ca200c3a5cf1d1b24120bfe0b0a1
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-10-10 12:53:38 +0200

    chore: remove unused change

--- src/lib/elements/forms/inputCheckbox.svelte
+++ src/lib/elements/forms/inputCheckbox.svelte
@@ -32,7 +32,6 @@
             {id}
             {disabled}
             {required}
-            on:change
             type="checkbox"
             bind:this={element}
             bind:checked={value}
