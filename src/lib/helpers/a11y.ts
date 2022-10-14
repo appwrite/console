cee7c466b43ba227eb0564db96cfef0acb4a7db8
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-10-14 19:19:33 +0200

    fix: types

--- src/lib/helpers/a11y.ts
+++ src/lib/helpers/a11y.ts
@@ -1,6 +1,10 @@
-export function clickOnEnter(event: KeyboardEvent) {
+export function clickOnEnter(
+    event: KeyboardEvent & {
+        currentTarget: HTMLElement;
+    }
+) {
     if (event.key === 'Enter' && event.target === event.currentTarget) {
         event.preventDefault();
-        (event.currentTarget as HTMLElement).click();
+        event.currentTarget.click();
     }
 }
