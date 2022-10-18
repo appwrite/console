a59f6e15ce1f41ecc365bcb5ebe1212efcb00be2
Arman <arman@appwrite.io>
2022-10-18 11:15:34 +0200

    fix: a11y problem

--- src/lib/elements/table/row.svelte
+++ src/lib/elements/table/row.svelte
@@ -1,3 +1,7 @@
-<div class="table-row" role="row" on:click>
+<script lang="ts">
+    import { clickOnEnter } from '$lib/helpers/a11y';
+</script>
+
+<div class="table-row" role="row" on:keyup={clickOnEnter} on:click|preventDefault>
     <slot />
 </div>
