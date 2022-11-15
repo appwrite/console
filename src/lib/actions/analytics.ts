3175c8c42fd852433a2a73ff3a565f73ef387b5c
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-11-15 17:22:50 +0100

    feat: send events to growth

--- src/lib/actions/analytics.ts
+++ src/lib/actions/analytics.ts
@@ -2,6 +2,7 @@ import Analytics from 'analytics';
 import googleAnalytics from '@analytics/google-analytics';
 import { get } from 'svelte/store';
 import { page } from '$app/stores';
+import { user } from '$lib/stores/user';
 
 const analytics = Analytics({
     app: 'appwrite',
@@ -16,8 +17,9 @@ export function trackEvent(name: string, data: object = null): void {
     if (!isTrackingAllowed()) {
         return;
     }
-
-    analytics.track(name, { ...data, path: get(page).routeId });
+    const path = get(page).routeId;
+    analytics.track(name, { ...data, path });
+    sendEventToGrowth(name, path, data);
 }
 
 export function trackPageView(path: string) {
@@ -27,6 +29,26 @@ export function trackPageView(path: string) {
 
     analytics.page({
         path
+    });
+}
+
+function sendEventToGrowth(event: string, path: string, data: object = null): void {
+    const { email, name } = get(user);
+
+    fetch('https://growth.appwrite.io/v1/analytics', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            action: event,
+            url: window.location.origin + path,
+            data: {
+                email,
+                name,
+                ...data
+            }
+        })
     });
 }
 
