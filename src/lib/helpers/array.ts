b5870f1642b9f75ad0f629ca8c7b8003044bc2ab
tglide <26071571+TGlide@users.noreply.github.com>
2022-11-23 16:26:33 +0000

    chore: add jsdoc to array helper

--- src/lib/helpers/array.ts
+++ src/lib/helpers/array.ts
@@ -14,7 +14,16 @@ export function symmetricDifference(arr1: unknown[], arr2: unknown[]) {
     return difference(arr1, arr2).concat(difference(arr2, arr1));
 }
 
-export function remove<T>(arr: T[], index: number) {
+/**
+ * Removes the element at the specified index from the array, and returns a new array.
+ *
+ * @export
+ * @template T
+ * @param {T[]} arr
+ * @param {number} index
+ * @returns {T[]}
+ */
+export function remove<T>(arr: T[], index: number): T[] {
     // Remove the element at the given index, return a new array
     return [...arr.slice(0, index), ...arr.slice(index + 1)];
 }
