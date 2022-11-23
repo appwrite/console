95e5d10c65a4862c348e129bc120913534e5e7b2
Steven Nguyen <stnguyen90@users.noreply.github.com>
2022-11-23 08:39:18 -0800

    Ensure updating function doesn't reset other params
    For any optional param not sent to the API, the values are reset to the
    default value. This update ensures we pass the existing values so that
    they aren't reset.

--- src/routes/console/project-[project]/functions/function-[function]/store.ts
+++ src/routes/console/project-[project]/functions/function-[function]/store.ts
@@ -2,5 +2,5 @@ import { page } from '$app/stores';
 import { derived, writable, type Writable } from 'svelte/store';
 import type { Models } from '@aw-labs/appwrite-console';
 
-export const func = derived(page, ($page) => $page.data.function);
+export const func = derived(page, ($page) => $page.data.function as Models.Function);
 export const execute: Writable<Models.Function> = writable();
