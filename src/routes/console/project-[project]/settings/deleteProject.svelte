1a59d3f3a348d661d4fb8c23d1bae51c6054f83c
jalajcodes <jalaj@mail.com>
2022-12-27 11:03:08 +0530

    Rename id

--- src/routes/console/project-[project]/settings/deleteProject.svelte
+++ src/routes/console/project-[project]/settings/deleteProject.svelte
@@ -43,7 +43,7 @@
         <InputText
             label={`Enter "${$project.name}" to continue`}
             placeholder="Enter name"
-            id={`delete-${$project.name}`}
+            id="project-name"
             autofocus
             required
             bind:value={name} />
