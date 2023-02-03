f406fe2aced08f10f9c6446b88d48ce21bf6a39a
Eldad Fux <eldad.fux@gmail.com>
2023-02-03 08:47:04 +0200

    Incossisntent permission description

--- src/routes/console/project-[project]/functions/wizard/step2.svelte
+++ src/routes/console/project-[project]/functions/wizard/step2.svelte
@@ -7,14 +7,12 @@
 <WizardStep>
     <svelte:fragment slot="title">Execute access</svelte:fragment>
     <svelte:fragment slot="subtitle">
-        Choose who can execute this function using the client API. Check out our documentation for
-        more on <a
+        Choose who can execute this function using the client API.  For more information, check out
+        the <a
             href="https://appwrite.io/docs/permissions"
             target="_blank"
             rel="noopener noreferrer"
-            class="link">
-            Permissions</a
-        >.
+            class="link">Permissions Guide</a> in our documentation.
     </svelte:fragment>
 
     <Roles bind:roles={$createFunction.execute} />
