bd7331e871a05bc95aa375d398ef6073c6415362
Arman <arman@appwrite.io>
2023-02-06 10:25:00 +0100

    fix: add role

--- src/lib/elements/table/rowButton.svelte
+++ src/lib/elements/table/rowButton.svelte
@@ -2,6 +2,6 @@
     import { clickOnEnter } from '$lib/helpers/a11y';
 </script>
 
-<tr class="table-row" tabindex="0" on:keyup={clickOnEnter} on:click|preventDefault>
+<tr class="table-row" role="button" tabindex="0" on:keyup={clickOnEnter} on:click|preventDefault>
     <slot />
 </tr>
