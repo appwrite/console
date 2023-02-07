453f010bfd17356db9003ab057d78bfde7c35b75
shimon <shimon@appwrite.io>
2023-02-07 16:47:01 +0200

    support usage metrics update

--- src/routes/console/project-[project]/auth/usage/[[period]]/+page.ts
+++ src/routes/console/project-[project]/auth/usage/[[period]]/+page.ts
@@ -7,10 +7,6 @@ export const load: PageLoad = async ({ params, parent }) => {
     const { period } = params;
     const response = await sdkForProject.users.getUsage(period ?? '30d');
     return {
-        count: response.usersCount as unknown as Models.Metric[],
-        created: response.usersCreate as unknown as Models.Metric[],
-        read: response.usersRead as unknown as Models.Metric[],
-        updated: response.usersUpdate as unknown as Models.Metric[],
-        deleted: response.usersDelete as unknown as Models.Metric[]
+        usersTotal: response.usersTotal as unknown as Models.Metric[]
     };
 };
