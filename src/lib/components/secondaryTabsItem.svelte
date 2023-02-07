449864d609382b911f7e36bb6e503903ee8fae2d
Arman <arman@appwrite.io>
2023-02-07 10:37:52 +0100

    fix: rename drop-tabs to secondary-tabs

---
+++ src/lib/components/secondaryTabsItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let href: string;
+    export let disabled = false;
+</script>
+
+<li class="secondary-tabs-item">
+    {#if href}
+        {#if disabled}
+            <button class="secondary-tabs-button" disabled>
+                <span class="text"><slot /></span>
+            </button>
+        {:else}
+            <a class="secondary-tabs-button" {href}>
+                <span class="text"><slot /></span>
+            </a>
+        {/if}
+    {:else}
+        <button class="secondary-tabs-button" {disabled} on:click>
+            <span class="text"><slot /></span>
+        </button>
+    {/if}
+</li>
--- tests/unit/components/dropTabsItem.test.ts
+++ tests/unit/components/dropTabsItem.test.ts
@@ -1,22 +1,22 @@
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 import { render, fireEvent } from '@testing-library/svelte';
-import { DropTabsItem } from '../../../src/lib/components';
+import { SecondaryTabsItem } from '../../../src/lib/components';
 
 test('shows button', () => {
-    const { getByRole } = render(DropTabsItem);
+    const { getByRole } = render(SecondaryTabsItem);
 
     expect(getByRole('button')).toBeInTheDocument();
 });
 
 test('shows button - disabled', () => {
-    const { getByRole } = render(DropTabsItem, { disabled: true });
+    const { getByRole } = render(SecondaryTabsItem, { disabled: true });
 
     expect(getByRole('button')).toBeDisabled();
 });
 
 test('shows button - on:click', async () => {
-    const { getByRole, component } = render(DropTabsItem);
+    const { getByRole, component } = render(SecondaryTabsItem);
     const button = getByRole('button');
 
     const callback = vi.fn();
