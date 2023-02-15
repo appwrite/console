fcba38ba66f112a89022d402443c76ce8803d14f
Arman <arman@appwrite.io>
2023-02-15 15:02:41 +0100

    fix: file name alignment

--- src/routes/console/project-[project]/storage/bucket-[bucket]/[[page]]/+page.svelte
+++ src/routes/console/project-[project]/storage/bucket-[bucket]/[[page]]/+page.svelte
@@ -97,7 +97,7 @@
                     {#if file.chunksTotal / file.chunksUploaded !== 1}
                         <TableRow>
                             <TableCell title="Name">
-                                <div class="u-flex u-gap-12 u-main-space-between">
+                                <div class="u-flex u-gap-12 u-main-space-between u-cross-center">
                                     <span class="avatar is-size-small is-color-empty" />
 
                                     <span class="text u-trim">{file.name}</span>
@@ -136,7 +136,7 @@
                         <TableRowLink
                             href={`${base}/console/project-${projectId}/storage/bucket-${bucketId}/file-${file.$id}`}>
                             <TableCell title="Name">
-                                <div class="u-flex u-gap-12">
+                                <div class="u-flex u-gap-12 u-cross-center">
                                     <Avatar size={32} src={getPreview(file.$id)} name={file.name} />
                                     <span class="text u-trim">{file.name}</span>
                                 </div>
