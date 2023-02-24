e39beba213e34f3274b775901025816e31b3d827
Arman <arman@appwrite.io>
2023-02-24 11:53:15 +0100

    chore: remove old store

--- src/routes/console/organization-[organization]/billing/store.ts
+++ src/routes/console/organization-[organization]/billing/store.ts
@@ -1,35 +1,7 @@
 import { page } from '$app/stores';
-
-import { derived, writable } from 'svelte/store';
+import { derived } from 'svelte/store';
 
 export const publicKey = import.meta.env?.VITE_STRIPE_PUBLIC_KEY?.toString() as string | undefined;
 export const secretKey = import.meta.env?.VITE_STRIPE_SECRET_KEY?.toString() as string | undefined;
-
-function createCustomerStore() {
-    const { subscribe, set } = writable<Stripe.Customer | Stripe.DeletedCustomer>(undefined);
-    return {
-        subscribe,
-        set,
-        load: async (id: string) => {
-            const customer: Stripe.Customer | Stripe.DeletedCustomer =
-                await stripe.customers.retrieve(id);
-            set(customer);
-        },
-        create: async (params: Stripe.CustomerCreateParams) => {
-            const customer: Stripe.Customer = await stripe.customers.create(params);
-            set(customer);
-        },
-        update: async (id: string, params: Stripe.CustomerUpdateParams) => {
-            const customer: Stripe.Customer = await stripe.customers.update(id, params);
-            set(customer);
-        },
-        remove: async (id: string) => {
-            const customer: Stripe.DeletedCustomer = await stripe.customers.del(id);
-            set(customer);
-        }
-    };
-}
-
-export const customer = createCustomerStore();
 
 export const paymentMethods = derived(page, ($page) => $page.data.paymentMethods);
