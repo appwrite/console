8e8186b6f51d0585256486f37838a1c715851902
Matej Bačo <matejbaco2000@gmail.com>
2023-02-27 13:46:23 +0100

    Update to new specs

--- src/lib/stores/logs.ts
+++ src/lib/stores/logs.ts
@@ -4,7 +4,7 @@ import { writable } from 'svelte/store';
 export const log = writable<{
     show: boolean;
     func: Models.Function;
-    data: Models.Execution | Models.Deployment;
+    data: Models.Execution<any> | Models.Deployment;
 }>({
     show: false,
     func: null,
