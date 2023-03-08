53c77dd0e29f530b4311115a6266295abe307df3
tglide <26071571+TGlide@users.noreply.github.com>
2023-03-08 17:14:25 +0000

    refactor: add sdk export

--- src/lib/stores/sdk.ts
+++ src/lib/stores/sdk.ts
@@ -19,7 +19,7 @@ const endpoint = VARS.APPWRITE_ENDPOINT ?? `${globalThis?.location?.origin}/v1`;
 const clientConsole = new Client();
 clientConsole.setEndpoint(endpoint).setProject('console');
 
-const sdkForConsole = {
+export const sdkForConsole = {
     client: clientConsole,
     account: new Account(clientConsole),
     avatars: new Avatars(clientConsole),
@@ -31,7 +31,7 @@ const sdkForConsole = {
     users: new Users(clientConsole)
 };
 
-function sdkForProject() {
+export function sdkForProject() {
     const clientProject = new Client();
     clientProject.setEndpoint(endpoint).setMode('admin');
 
@@ -56,4 +56,40 @@ function sdkForProject() {
     };
 }
 
-export { sdkForConsole, sdkForProject };
+export const sdk = {
+    forConsole: {
+        client: clientConsole,
+        account: new Account(clientConsole),
+        avatars: new Avatars(clientConsole),
+        functions: new Functions(clientConsole),
+        health: new Health(clientConsole),
+        locale: new Locale(clientConsole),
+        projects: new Projects(clientConsole),
+        teams: new Teams(clientConsole),
+        users: new Users(clientConsole)
+    },
+    get forProject() {
+        const clientProject = new Client();
+        clientProject.setEndpoint(endpoint).setMode('admin');
+
+        const pathname = window.location.pathname;
+        const projectMatch = pathname.match(/project-([a-zA-Z0-9]+)/);
+        if (projectMatch) {
+            clientProject.setProject(projectMatch[1]);
+        }
+
+        return {
+            client: clientProject,
+            account: new Account(clientProject),
+            avatars: new Avatars(clientProject),
+            databases: new Databases(clientProject),
+            functions: new Functions(clientProject),
+            health: new Health(clientProject),
+            locale: new Locale(clientProject),
+            project: new Project(clientProject),
+            storage: new Storage(clientProject),
+            teams: new Teams(clientProject),
+            users: new Users(clientProject)
+        };
+    }
+};
