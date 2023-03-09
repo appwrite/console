2a877d844d335cd02ed35506f13d0da9b8a7606f
tglide <26071571+TGlide@users.noreply.github.com>
2023-03-09 16:38:48 +0000

    refactor: add truncate option to output

--- src/lib/components/output.svelte
+++ src/lib/components/output.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+    import { truncate as truncateAction } from '$lib/actions/truncate';
     import Copy from './copy.svelte';
 
     export let value: string;
+    export let truncate = false;
 </script>
 
 <Copy {value}>
     <div class="interactive-text-output is-textarea">
-        <span class="text u-line-height-1-5 u-break-all"><slot /></span>
+        <span
+            class="text u-line-height-1-5 u-break-all"
+            use:truncateAction={{ n: 8, enabled: truncate }}><slot /></span>
 
         <div class="u-flex u-cross-child-start u-gap-8">
             <button class="interactive-text-output-button" aria-label="copy text">
