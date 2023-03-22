47d3a2b393fef916143ac1a4667d026ce4306f3e
Arman <arman@appwrite.io>
2023-03-22 12:23:00 +0100

    feat: create paginator component

---
+++ src/lib/components/paginator.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import PaginationInline from './paginationInline.svelte';
+
+    export let items = [];
+    export let limit = 5;
+
+    let offset = 0;
+
+    $: paginatedItems = items.slice(offset, offset + limit);
+</script>
+
+<div class="container">
+    <slot {paginatedItems} />
+</div>
+
+<div class="u-flex u-margin-block-start-32 u-main-space-between">
+    <p class="text">Total results: {items?.length}</p>
+    <PaginationInline {limit} bind:offset sum={items?.length} hidePages />
+</div>
