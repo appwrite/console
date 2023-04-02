d996e624896e51c235641285c11698d6983018c5
kabhamo <mkabha54@gmail.com>
2023-04-02 12:40:04 +0300

    fix the padding-top style

--- src/lib/components/code.svelte
+++ src/lib/components/code.svelte
@@ -100,7 +100,7 @@
     :not(pre) > code[class*='language-'],
     pre[class*='language-'] {
         background: hsl(var(--p-box-background-color));
-        padding: 0;
+        padding-top: 4%;
         margin: 0;
     }
     .prism-token {
