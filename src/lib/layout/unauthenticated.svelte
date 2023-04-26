3ba62756f0271dec221207adb6d3724a44fbea6c
tglide <26071571+TGlide@users.noreply.github.com>
2023-04-26 15:49:57 +0100

    feat: beta signup page desktop

--- src/lib/layout/unauthenticated.svelte
+++ src/lib/layout/unauthenticated.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-    import AppwriteLogo from '$lib/images/appwrite.svg';
-    import { app } from '$lib/stores/app';
     import { base } from '$app/paths';
-    import { user } from '$lib/stores/user';
-    import LoginLight from '$lib/images/login/login-light-mode.svg';
+    import AppwriteLogo from '$lib/images/appwrite.svg';
+    import AppwriteCloudLogo from '$lib/images/appwrite-cloud.svg';
+    import AppwriteCloudBg from '$lib/images/login/cloud-bg.svg';
     import LoginDark from '$lib/images/login/login-dark-mode.svg';
+    import LoginLight from '$lib/images/login/login-light-mode.svg';
+    import { app } from '$lib/stores/app';
     import { sdkForConsole } from '$lib/stores/sdk';
+    import { user } from '$lib/stores/user';
+    import { isCloud } from '$lib/system';
 
     export let imgLight = LoginLight;
     export let imgDark = LoginDark;
@@ -35,50 +38,105 @@
 </script>
 
 <main class="grid-1-1 is-full-page" id="main">
-    <section class="grid-1-1-col-1 u-flex u-flex-vertical">
-        <div
-            class="container u-margin-block-start-20"
-            style="--p-container-max-size: var(--container-size-medium);">
-            <a href={user ? '/console' : '/'}>
-                <img src={AppwriteLogo} width="196" height="47" class="u-block" alt="Appwrite" />
+    {#if isCloud}
+        <section
+            class="grid-1-1-col-1 u-flex u-flex-vertical cloud-section"
+            style:--url={`url(${AppwriteCloudBg})`}>
+            <div class="u-margin-block-start-auto" />
+            <div class="u-margin-block-start-auto" />
+            <div class="u-margin-block-start-auto" />
+            <div class="u-margin-block-start-auto" />
+
+            <a class="logo" href={user ? '/console' : '/'}>
+                <img
+                    src={AppwriteCloudLogo}
+                    width="196"
+                    height="47"
+                    class="u-block"
+                    alt="Appwrite" />
             </a>
-        </div>
 
-        <div class="u-margin-block-start-auto" />
+            <p class="header">Cloud is Live</p>
 
-        <div class="u-margin-block-start-auto is-no-mobile" />
-        <div
-            class="container u-margin-block-start-20 is-no-mobile u-flex u-main-center"
-            style="--p-container-max-size: var(--container-size-large);">
-            {#if $app.themeInUse === 'dark'}
-                <img src={imgDark} alt="" class="u-only-dark" />
-            {:else}
-                <img src={imgLight} alt="" class="u-only-light" />
-            {/if}
-        </div>
+            <div class="u-margin-block-start-auto" />
 
-        <div class="u-margin-block-start-auto" />
+            <p class="beta">Now in public <span>beta</span></p>
 
-        <div class="u-margin-block-start-auto is-no-mobile" />
-        <div
-            class="container u-text-color-light-gray is-no-mobile"
-            style="--p-container-max-size:var(--container-size-small); --p-container-padding-inline:1rem;">
-            <p>Integrate with your favourite technologies</p>
-            <ul
-                class="u-flex u-main-center u-flex-wrap u-gap-16 u-margin-block-start-32 u-line-height-1 u-opacity-0-5">
-                {#each technologies as tech}
-                    <li>
-                        <img
-                            src={`${base}/icons/${$app.themeInUse}/grayscale/${tech}.svg`}
-                            alt={tech}
-                            aria-hidden="true"
-                            aria-label={tech} />
-                    </li>
-                {/each}
-            </ul>
-        </div>
-        <div class="u-margin-block-start-40" />
-    </section>
+            <div class="u-margin-block-start-auto" />
+            <div class="u-margin-block-start-auto" />
+            <div class="u-margin-block-start-auto" />
+
+            <div class="u-margin-block-start-auto is-no-mobile" />
+            <div
+                class="container u-text-color-light-gray is-no-mobile"
+                style="--p-container-max-size:var(--container-size-small); --p-container-padding-inline:1rem;">
+                <p>Integrate with your favourite technologies</p>
+                <ul
+                    class="u-flex u-main-center u-flex-wrap u-gap-16 u-margin-block-start-32 u-line-height-1 u-opacity-0-5">
+                    {#each technologies as tech}
+                        <li>
+                            <img
+                                src={`${base}/icons/${$app.themeInUse}/grayscale/${tech}.svg`}
+                                alt={tech}
+                                aria-hidden="true"
+                                aria-label={tech} />
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+            <div class="u-margin-block-start-40" />
+        </section>
+    {:else}
+        <section class="grid-1-1-col-1 u-flex u-flex-vertical">
+            <div
+                class="container u-margin-block-start-20"
+                style="--p-container-max-size: var(--container-size-medium);">
+                <a href={user ? '/console' : '/'}>
+                    <img
+                        src={AppwriteLogo}
+                        width="196"
+                        height="47"
+                        class="u-block"
+                        alt="Appwrite" />
+                </a>
+            </div>
+
+            <div class="u-margin-block-start-auto" />
+
+            <div class="u-margin-block-start-auto is-no-mobile" />
+            <div
+                class="container u-margin-block-start-20 is-no-mobile u-flex u-main-center"
+                style="--p-container-max-size: var(--container-size-large);">
+                {#if $app.themeInUse === 'dark'}
+                    <img src={imgDark} alt="" class="u-only-dark" />
+                {:else}
+                    <img src={imgLight} alt="" class="u-only-light" />
+                {/if}
+            </div>
+
+            <div class="u-margin-block-start-auto" />
+
+            <div class="u-margin-block-start-auto is-no-mobile" />
+            <div
+                class="container u-text-color-light-gray is-no-mobile"
+                style="--p-container-max-size:var(--container-size-small); --p-container-padding-inline:1rem;">
+                <p>Integrate with your favourite technologies</p>
+                <ul
+                    class="u-flex u-main-center u-flex-wrap u-gap-16 u-margin-block-start-32 u-line-height-1 u-opacity-0-5">
+                    {#each technologies as tech}
+                        <li>
+                            <img
+                                src={`${base}/icons/${$app.themeInUse}/grayscale/${tech}.svg`}
+                                alt={tech}
+                                aria-hidden="true"
+                                aria-label={tech} />
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+            <div class="u-margin-block-start-40" />
+        </section>
+    {/if}
     <section class="grid-1-1-col-2 u-flex u-main-center u-cross-center">
         <div class="container u-flex u-flex-vertical u-cross-center u-height-100-percent">
             <div class="u-margin-block-start-auto" />
@@ -110,3 +168,48 @@
         </div>
     </section>
 </main>
+
+<style lang="scss">
+    .cloud-section {
+        background: var(--url);
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        color: hsl(var(--color-neutral-5));
+
+        .logo {
+            display: block;
+            align-self: center;
+        }
+
+        .header {
+            text-align: center;
+            align-self: center;
+            font-family: 'Poppins';
+            font-size: 5.25rem;
+            font-weight: 700;
+        }
+
+        .beta {
+            text-align: center;
+            align-self: center;
+            font-family: 'Poppins';
+            font-size: 1.125rem;
+            font-weight: 700;
+            text-transform: uppercase;
+            line-height: 130%;
+            letter-spacing: 0.25em;
+
+            > span {
+                background: linear-gradient(
+                    180deg,
+                    rgba(252, 252, 255, 0.5) 0%,
+                    rgba(252, 252, 255, 0.25) 100%
+                );
+                border-radius: 5.38636px;
+                text-align: center;
+                padding: 0.5rem 0.5rem 0.5rem 0.75rem;
+            }
+        }
+    }
+</style>
