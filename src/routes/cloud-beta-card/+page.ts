5fe587c9a8eded6975e1b42b6094929b222905fa
tglide <26071571+TGlide@users.noreply.github.com>
2023-04-26 15:49:57 +0100

    feat: beta signup page desktop

--- src/routes/cloud-beta-card/+page.ts
+++ src/routes/cloud-beta-card/+page.ts
@@ -1,8 +1,8 @@
-import { Mode, MODE } from '$lib/system';
+import { isCloud } from '$lib/system';
 import { redirect } from '@sveltejs/kit';
 
 export async function load({ parent }) {
-    if (MODE !== Mode.CLOUD) throw redirect(303, '/');
+    if (!isCloud) throw redirect(303, '/');
     const { account } = await parent();
     if (!account) throw redirect(303, '/login');
 }
