f44b51ccf492a605940640b134308140b02a3dc8
tglide <26071571+TGlide@users.noreply.github.com>
2023-04-28 18:58:32 +0100

    chore: remove mocked card

--- src/routes/card/helpers.ts
+++ src/routes/card/helpers.ts
@@ -1,8 +1,8 @@
 import { VARS } from '$lib/system';
 
 export function getCardImgUrls(userId: string) {
-    const frontImg = `${VARS.APPWRITE_ENDPOINT}/cards/cloud?mock=normal`;
-    const backImg = `${VARS.APPWRITE_ENDPOINT}/cards/cloud-back?mock=normal`;
+    const frontImg = `${VARS.APPWRITE_ENDPOINT}/cards/cloud?userId=${userId}`;
+    const backImg = `${VARS.APPWRITE_ENDPOINT}/cards/cloud-back?userId=${userId}`;
     const ogImg = `${VARS.APPWRITE_ENDPOINT}/cards/cloud-og?userId=${userId}`;
 
     return { frontImg, backImg, ogImg };
