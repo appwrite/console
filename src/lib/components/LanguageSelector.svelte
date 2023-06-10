47004fb29296958bef34c0d0b771260d0cc19c91
Anshuman Goswami <anshumangoswami07@gmail.com>
2023-06-10 14:22:52 +0530

    update language selector

--- src/lib/components/LanguageSelector.svelte
+++ src/lib/components/LanguageSelector.svelte
@@ -4,6 +4,12 @@
     import { locales } from '$lib/i18n/i18n-util';
     import { loadLocaleAsync } from '$lib/i18n/i18n-util.async';
 
+    const lang = [
+        { value: 'de', lng: 'German' },
+        { value: 'en', lng: 'English' },
+        { value: 'it', lng: 'Italian' }
+    ];
+
     const chooseLocale = async (locale: Locales) => {
         await loadLocaleAsync(locale);
         setLocale(locale);
@@ -17,7 +23,7 @@
 
 <select bind:value={localeToSelect}>
     <option value="" selected disabled>language selector</option>
-    {#each locales as locale}
-        <option value={locale}>{locale}</option>
+    {#each locales as locale, i}
+        <option value={locale}>{locale === lang[i].value ? lang[i].lng : locale}</option>
     {/each}
 </select>
