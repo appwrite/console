e92e6ecb59571559bb4a6b0e5fe4a974dc1eb363
Arman <arman@appwrite.io>
2023-07-17 16:00:19 +0200

    feat: logs

--- src/lib/layout/logs.svelte
+++ src/lib/layout/logs.svelte
@@ -1,43 +1,28 @@
 <script lang="ts">
     import { toLocaleDateTime } from '$lib/helpers/date';
-    import { humanFileSize } from '$lib/helpers/sizeConvertion';
     import { log } from '$lib/stores/logs';
-    import { Output, Status, Tab, Tabs } from '../components';
-    import { Button } from '$lib/elements/forms';
+    import { Heading, Id, Status, Tab, Tabs } from '../components';
     import { base } from '$app/paths';
     import { app } from '$lib/stores/app';
     import { sdk } from '$lib/stores/sdk';
     import { page } from '$app/stores';
     import { calculateTime } from '$lib/helpers/timeConversion';
     import type { Models } from '@appwrite.io/console';
+    import { header } from '$lib/stores/layout';
 
-    let selectedTab: string;
+    let selectedRequest = 'parameters';
+    let selectedResponse = 'logs';
     let rawData: string;
 
-    function isDeployment(
-        data: Models.Deployment | Models.Execution<any>
-    ): data is Models.Deployment {
-        if ('buildId' in data) {
-            selectedTab = 'logs';
-            rawData = `${sdk.forConsole.client.config.endpoint}/functions/${$log.func.$id}/deployment/${$log.data.$id}?mode=admin&project=${$page.params.project}`;
-            return true;
-        }
-    }
+    $: console.log($log.data);
 
     function isExecution(
         data: Models.Deployment | Models.Execution<any>
     ): data is Models.Execution<any> {
         if ('trigger' in data) {
-            selectedTab = 'logs';
             rawData = `${sdk.forConsole.client.config.endpoint}/functions/${$log.func.$id}/execution/${$log.data.$id}?mode=admin&project=${$page.params.project}`;
             return true;
         }
-    }
-
-    function scrollToTop() {
-        document
-            .getElementsByClassName('code-panel-content')[0]
-            .scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     }
 
     function handleKeydown(event: KeyboardEvent) {
@@ -53,7 +38,10 @@
 {#if $log.data}
     <section class="cover-frame" data-private>
         <header class="cover-frame-header u-flex u-gap-16 u-main-space-between u-cross-center">
-            <h1 class="body-text-1 u-bold">Function ID: {$log.func.$id}</h1>
+            <div class="u-flex u-gap-8 u-cross-center">
+                <h1 class="body-text-1 u-bold">Function ID:</h1>
+                <Id value={$log.func.$id}>{$log.func.$id}</Id>
+            </div>
             <button
                 on:click={() => ($log.show = false)}
                 class="button is-text is-only-icon"
@@ -62,80 +50,8 @@
                 <span class="icon-x" aria-hidden="true" />
             </button>
         </header>
-        {#if isDeployment($log.data)}
-            {@const size = humanFileSize($log.data.size)}
-            <div class="cover-frame-content u-flex u-flex-vertical">
-                <div class="u-flex u-gap-16">
-                    <div class="avatar is-size-large">
-                        <img
-                            height="28"
-                            width="28"
-                            src={`${base}/icons/${$app.themeInUse}/color/${
-                                $log.func.runtime.split('-')[0]
-                            }.svg`}
-                            alt="technology" />
-                    </div>
-                    <div>
-                        <div class="u-flex u-gap-12 u-cross-center">
-                            <h2 class="body-text-2 u-bold">Deployment ID:</h2>
-                            <Output value={$log.data.$id}>
-                                {$log.data.$id}
-                            </Output>
-                        </div>
-                        <time class="u-block"
-                            >Created at: {toLocaleDateTime($log.data.$createdAt)}</time>
-                        <div>Size: {size.value} {size.unit}</div>
-                    </div>
-                    <div class="status u-margin-inline-start-auto">
-                        <div class="status u-margin-inline-start-auto">
-                            <Status status={$log.data.status}>{$log.data.status}</Status>
 
-                            <time>{calculateTime($log.data.buildTime)}</time>
-                        </div>
-                    </div>
-                </div>
-                <div class="tabs u-margin-block-start-48 u-sep-block-end">
-                    <Tabs>
-                        <Tab
-                            selected={selectedTab === 'logs'}
-                            on:click={() => (selectedTab = 'logs')}>
-                            Logs
-                        </Tab>
-                        <Tab
-                            selected={selectedTab === 'errors'}
-                            on:click={() => (selectedTab = 'errors')}>
-                            Errors
-                        </Tab>
-                    </Tabs>
-                </div>
-                <div class="theme-dark u-stretch u-margin-block-start-32 u-overflow-hidden">
-                    <section class="code-panel">
-                        <header class="code-panel-header">
-                            <div class="u-flex u-gap-16 u-margin-inline-start-auto">
-                                <Button text external href={rawData}>
-                                    <span class="icon-external-link" aria-hidden="true" />
-                                    <span class="text">Raw data</span>
-                                </Button>
-                                <Button secondary on:click={scrollToTop}>
-                                    <span class="text">Scroll to top</span>
-                                </Button>
-                            </div>
-                        </header>
-                        {#if selectedTab === 'logs'}
-                            <code class="code-panel-content">
-                                {$log.data.buildStdout ? $log.data.buildStdout : 'No logs recorded'}
-                            </code>
-                        {:else}
-                            <code class="code-panel-content">
-                                {$log.data.buildStderr
-                                    ? $log.data.buildStderr
-                                    : 'No errors recorded'}
-                            </code>
-                        {/if}
-                    </section>
-                </div>
-            </div>
-        {:else if isExecution($log.data)}
+        {#if isExecution($log.data)}
             <div class="cover-frame-content u-flex u-flex-vertical">
                 <div class="u-flex u-gap-16">
                     <div class="avatar is-size-large">
@@ -148,60 +64,106 @@
                             alt="technology" />
                     </div>
                     <div>
-                        <div class="u-flex u-gap-12 u-cross-center">
-                            <h2 class="body-text-2 u-bold">Execution ID:</h2>
-                            <Output value={$log.data.$id}>
-                                {$log.data.$id}
-                            </Output>
-                        </div>
-                        <time class="u-block">
-                            Created at: {toLocaleDateTime($log.data.$createdAt)}</time>
+                        <h2 class="body-text-2 u-bold">Execution ID:</h2>
+                        <Id value={$log.data.$id}>{$log.data.$id}</Id>
                     </div>
-                    <div>
-                        <p>Triggered by: <b>{$log.data.trigger}</b></p>
-                    </div>
+                    <ul>
+                        <li class="text">
+                            <b>Total duration: </b>
+                            <time>
+                                {calculateTime($log.data.duration)}
+                            </time>
+                        </li>
+                        <li class="text">
+                            <b>Cold-start duration:</b>
+                            <time> ?? </time>
+                        </li>
+                        <li class="text">
+                            <b>Created at:</b>
+                            <time>
+                                {toLocaleDateTime($log.data.$createdAt)}
+                            </time>
+                        </li>
+                        <li class="text">
+                            <b>Host:</b>
+                            <span> ??</span>
+                        </li>
+                    </ul>
                     <div class="status u-margin-inline-start-auto">
                         <Status status={$log.data.status}>{$log.data.status}</Status>
-
-                        <time>{calculateTime($log.data.duration)}</time>
                     </div>
                 </div>
-                <div class="tabs u-margin-block-start-48 u-sep-block-end">
-                    <Tabs>
-                        <Tab
-                            selected={selectedTab === 'logs'}
-                            on:click={() => (selectedTab = 'logs')}>
-                            Logs
-                        </Tab>
-                        <Tab
-                            selected={selectedTab === 'errors'}
-                            on:click={() => (selectedTab = 'errors')}>
-                            Errors
-                        </Tab>
-                    </Tabs>
-                </div>
+
                 <div class="theme-dark u-stretch u-margin-block-start-32 u-overflow-hidden">
                     <section class="code-panel">
-                        <header class="code-panel-header">
-                            <div class="u-flex u-gap-16 u-margin-inline-start-auto">
-                                <Button text external href={rawData}>
-                                    <span class="icon-external-link" aria-hidden="true" />
-                                    <span class="text">Raw data</span>
-                                </Button>
-                                <Button secondary on:click={scrollToTop}>
-                                    <span class="text">Scroll to top</span>
-                                </Button>
+                        <header
+                            class="code-panel-header u-flex u-main-space-between u-width-full-line">
+                            <div class="u-flex u-gap-24">
+                                <div class="u-flex u-gap-16">
+                                    <h4 class="u-bold">Method:</h4>
+                                    <span>{$log.data.method}</span>
+                                </div>
+                                <div class="u-flex u-gap-16">
+                                    <h4 class="u-bold">Path:</h4>
+                                    <span>{$log.data.path}</span>
+                                </div>
                             </div>
+
+                            <p>Triggered by: <b>{$log.data.trigger}</b></p>
                         </header>
-                        {#if selectedTab === 'logs'}
-                            <code class="code-panel-content">
-                                {$log.data.logs ? $log.data.logs : 'No logs recorded'}
-                            </code>
-                        {:else if selectedTab === 'errors'}
-                            <code class="code-panel-content">
-                                {$log.data.errors ? $log.data.errors : 'No errors recorded'}
-                            </code>
-                        {/if}
+                        <div class="code-panel-content grid-1-2" style="u-grid">
+                            <div class="grid-1-2-col-1 u-flex u-flex-vertical u-gap-16">
+                                <Heading tag="h3" size="6">Request</Heading>
+                                <div class="u-sep-block-end">
+                                    <Tabs>
+                                        <Tab
+                                            selected={selectedRequest === 'parameters'}
+                                            on:click={() => (selectedRequest = 'parameters')}>
+                                            Parameters ({header?.parameters?.length ?? 0})
+                                        </Tab>
+                                        <Tab
+                                            selected={selectedRequest === 'headers'}
+                                            on:click={() => (selectedRequest = 'headers')}>
+                                            Headers ({header?.headers?.length ?? 0})
+                                        </Tab>
+                                        <Tab
+                                            selected={selectedRequest === 'body'}
+                                            on:click={() => (selectedRequest = 'body')}>
+                                            Body
+                                        </Tab>
+                                    </Tabs>
+                                </div>
+
+                                test
+                            </div>
+                            <div class="grid-1-2-col-2 u-flex u-flex-vertical u-gap-16">
+                                <Heading tag="h3" size="6">Response</Heading>
+                                <div class="u-sep-block-end">
+                                    <Tabs>
+                                        <Tab
+                                            selected={selectedResponse === 'logs'}
+                                            on:click={() => (selectedResponse = 'logs')}>
+                                            Logs
+                                        </Tab>
+                                        <Tab
+                                            selected={selectedResponse === 'errors'}
+                                            on:click={() => (selectedResponse = 'errors')}>
+                                            Errors
+                                        </Tab>
+                                        <Tab
+                                            selected={selectedResponse === 'headers'}
+                                            on:click={() => (selectedResponse = 'headers')}>
+                                            Headers
+                                        </Tab>
+                                        <Tab
+                                            selected={selectedResponse === 'body'}
+                                            on:click={() => (selectedResponse = 'body')}>
+                                            Body
+                                        </Tab>
+                                    </Tabs>
+                                </div>
+                            </div>
+                        </div>
                     </section>
                 </div>
             </div>
