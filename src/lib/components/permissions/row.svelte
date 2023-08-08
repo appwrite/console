00056f96c4a6e95c607cc3cdb72692dfe51dee0c
Haimantika mitra <yourgitemail@example.com>
2023-08-08 21:38:00 +0530

    Added custom role option for verified users

--- src/lib/components/permissions/row.svelte
+++ src/lib/components/permissions/row.svelte
@@ -38,6 +38,8 @@
             <div>Guests</div>
         {:else if role === 'any'}
             <div>Any</div>
+        {:else if !role.startsWith('user') && !role.startsWith('team') }
+            <div>Users/Verfified</div>
         {:else}
             <div
                 class="u-trim-1"
