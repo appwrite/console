9f11cc237c9f764340aa41a0a68416a11216cb80
Arman <arman@appwrite.io>
2023-08-10 20:42:38 +0200

    fix: added invalidation back

--- src/routes/console/project-[project]/functions/createFunction.svelte
+++ src/routes/console/project-[project]/functions/createFunction.svelte
@@ -41,7 +41,8 @@
                     sdk.forProject.functions.createVariable(response.$id, v.key, v.value)
                 )
             );
-            goto(`${base}/console/project-${projectId}/functions/function-${response.$id}`);
+            await invalidate(Dependencies.FUNCTIONS);
+            await goto(`${base}/console/project-${projectId}/functions/function-${response.$id}`);
             addNotification({
                 message: `${$createFunction.name} has been created`,
                 type: 'success'
