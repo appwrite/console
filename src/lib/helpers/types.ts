29f709316c16d7c204f1e38242525498c4bfeea7
tglide <26071571+TGlide@users.noreply.github.com>
2023-08-11 12:35:04 +0100

    fix: simplify deepKeys type to fix checks

--- src/lib/helpers/types.ts
+++ src/lib/helpers/types.ts
@@ -1,10 +1,5 @@
 import type { Writable } from 'svelte/store';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type DeepKeys<T extends Record<string, any>> = {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [K in keyof T]: T[K] extends Record<string, any> ? `${K}.${DeepKeys<T[K]>}` : K;
-}[keyof T];
 export type WritableValue<T> = T extends Writable<infer U> ? U : never;
 
 export function isHTMLElement(el: unknown): el is HTMLElement {
