a4e275644a4a98e6f91ce47d2652e218b124e5be
Arman <arman@appwrite.io>
2023-08-21 16:04:25 +0200

    feat: github readme parsing

--- src/routes/console/project-[project]/functions/templates/template-[template]/+page.svelte
+++ src/routes/console/project-[project]/functions/templates/template-[template]/+page.svelte
@@ -5,6 +5,20 @@
     import { Container } from '$lib/layout';
     import { connectTemplate } from '$lib/wizards/functions/cover.svelte';
     import { template } from './store';
+
+    async function fetchReadme() {
+        const runtime = $template.runtimes[0];
+        const url = `https://api.github.com/repos/${$template.providerOwner}/${$template.providerRepositoryId}/contents/${runtime.providerRootDirectory}/README.md`;
+        console.log(url);
+
+        const response = await fetch(url, {
+            headers: {
+                Accept: 'application/vnd.github.html'
+            }
+        });
+        const readmeHTML = await response.text();
+        return readmeHTML;
+    }
 </script>
 
 <Container>
@@ -67,6 +81,77 @@
                 </div>
             </Card>
             <Card>{@html $template.instructions}</Card>
+            {#await fetchReadme() then readmeHTML}
+                <Card>
+                    {@html readmeHTML}
+                </Card>
+            {/await}
         </section>
     </div>
 </Container>
+
+<style lang="scss">
+    /* svelte-ignore css-unused-selector */
+    :global(.md) {
+        @import './node_modules/@appwrite.io/pink/src/_index.scss';
+        :global(.anchor) {
+            display: none;
+        }
+        :global(h1) {
+            @extend .heading-level-4;
+            @extend .common-section;
+        }
+        :global(h2) {
+            @extend .u-margin-block-start-16;
+            @extend .heading-level-5;
+        }
+        :global(h3) {
+            @extend .u-margin-block-start-16;
+            @extend .heading-level-6;
+        }
+        :global(p) {
+            @extend .u-margin-block-start-8;
+        }
+        :global(strong) {
+            @extend .u-bold;
+        }
+        :global(p),
+        :global(h1),
+        :global(h2),
+        :global(h3),
+        :global(td) {
+            :global(code) {
+                @extend .inline-code;
+            }
+        }
+        :global(.snippet-clipboard-content) {
+            @extend .box;
+            @extend .u-margin-block-start-8;
+        }
+
+        :global(.highlight) {
+            @extend .box;
+            @extend .u-margin-block-start-8;
+        }
+
+        > :global(table) {
+            outline: 2px solid red;
+            @extend .table;
+        }
+    }
+
+    /* svelte-ignore css-unused-selector */
+    :global(.theme-dark) {
+        :global(.inline-code) {
+            --p-bg-color-inline-code: red;
+            --p-text-color-inline-code: var(--color-neutral-30);
+        }
+        :global(.highlight),
+        :global(.snippet-clipboard-content) {
+            --p-box-text-color: var(--color-neutral-5);
+            --p-box-background-color-default: var(--color-neutral-200);
+            --p-box-background-color-hover: var(--color-neutral-150);
+            --p-box-border-color: var(--color-neutral-150);
+        }
+    }
+</style>
