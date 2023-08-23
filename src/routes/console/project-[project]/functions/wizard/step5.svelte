2c748b789d7bfadd566c2cb9c9e3a8125ad0c5c0
Vincent (Wen Yu) Ge <vincent@appwrite.io>
2023-08-23 17:53:41 -0400

    Add more links to documentation

--- src/routes/console/project-[project]/functions/wizard/step5.svelte
+++ src/routes/console/project-[project]/functions/wizard/step5.svelte
@@ -45,7 +45,13 @@
 <WizardStep>
     <svelte:fragment slot="title">Variables</svelte:fragment>
     <svelte:fragment slot="subtitle">
-        Create the environment variables or secret keys that will be passed to your function.
+        Create the environment variables or secret keys that will be passed to your function. <a
+            href="https://appwrite.io/docs/permissions"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="link">
+            permissions documentation
+        </a>.
     </svelte:fragment>
     {#if $createFunction.vars.length}
         <Table noStyles>
