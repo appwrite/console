086633927b327d534198d29239a7cc523f19eede
tglide <26071571+TGlide@users.noreply.github.com>
2023-08-24 16:18:44 +0100

    persistent saved default in create attribute screens

--- src/routes/console/project-[project]/databases/database-[database]/collection-[collection]/attributes/datetime.svelte
+++ src/routes/console/project-[project]/databases/database-[database]/collection-[collection]/attributes/datetime.svelte
@@ -39,9 +39,30 @@
     export let editing = false;
     export let data: Partial<Models.AttributeDatetime>;
 
-    $: if (data.required || data.array) {
-        data.default = null;
+    import { createConservative } from '$lib/helpers/stores';
+
+    let savedDefault = data.default;
+
+    function handleDefaultState(hideDefault: boolean) {
+        if (hideDefault) {
+            savedDefault = data.default;
+            data.default = null;
+        } else {
+            data.default = savedDefault;
+        }
     }
+
+    const {
+        stores: { required, array },
+        listen
+    } = createConservative<Partial<Models.AttributeDatetime>>({
+        required: false,
+        array: false,
+        ...data
+    });
+    $: listen(data);
+
+    $: handleDefaultState($required || $array);
 </script>
 
 <InputDateTime
