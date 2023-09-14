ec99705fd08ca9253d894f71d19fe2a2094a03c4
loks0n <22452787+loks0n@users.noreply.github.com>
2023-09-14 11:56:44 +0100

    chore: remove unused store

--- src/routes/console/project-[project]/functions/templates/template-[template]/store.ts
+++ src/routes/console/project-[project]/functions/templates/template-[template]/store.ts
@@ -1,17 +1,5 @@
 import { page } from '$app/stores';
-import type { MarketplaceTemplate, Runtime } from '$lib/stores/marketplace';
+import type { MarketplaceTemplate } from '$lib/stores/marketplace';
 import { derived } from 'svelte/store';
 
 export const template = derived(page, ($page) => $page.data.template as MarketplaceTemplate);
-
-export const baseRuntimes = derived(template, ($template) => {
-    const baseRuntimes = new Map<string, Runtime>();
-    $template.runtimes.forEach((runtime) => {
-        const runtimeBase = runtime.name.split('-')[0];
-        baseRuntimes.set(runtimeBase, runtime);
-    });
-    return {
-        ...template,
-        runtimes: [...baseRuntimes.values()]
-    };
-});
