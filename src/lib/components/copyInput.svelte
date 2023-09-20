3c2aeb9a698a7703ab05a5c62ad40ea1e0208c83
Nikhil <nikhilanand2432@gmail.com>
2023-09-20 18:28:25 +0530

    Fix copy tooltip

--- src/lib/components/copyInput.svelte
+++ src/lib/components/copyInput.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
     import { tooltip } from '$lib/actions/tooltip';
     import { copy } from '$lib/helpers/copy';
-
     import { addNotification } from '$lib/stores/notifications';
-
     export let value: string;
     export let label: string = null;
     export let showLabel = false;
 
-    let content = 'Click to copy';
-
+    let content = 'Copy';
+    
     const handleCopy = async () => {
         const success = await copy(value);
 
@@ -23,24 +21,28 @@
         }
     };
 </script>
-
+<style>
+    span.icon-duplicate {
+        cursor: pointer;
+    }
+</style>
 <div>
     <label class:u-hide={!showLabel} class="label" for={label}>{label}</label>
     <div class="input-text-wrapper" style="--amount-of-buttons:1">
         <input {value} id={label} type="text" class="input-text" readonly />
         <div class="options-list">
-            <button
-                type="button"
-                class="options-list-button"
-                aria-label="Click to copy."
+           
+            <span 
+                class="icon-duplicate" aria-label="Copy"
                 on:click={handleCopy}
-                on:mouseenter={() => setTimeout(() => (content = 'Click to copy'))}
+                on:mouseenter={() => setTimeout(() => (content = 'Copy'))}
                 use:tooltip={{
                     content,
-                    hideOnClick: false
-                }}>
-                <span class="icon-duplicate" aria-hidden="true" />
-            </button>
+                    hideOnClick: false,
+                    appendTo: 'parent',
+                }}
+             />
+           
         </div>
     </div>
 </div>
