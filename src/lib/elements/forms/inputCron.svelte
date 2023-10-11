24bee149fa52e0f29a971937967ee160feb66a93
Shashankh Kanni <shashank.kanni@gmail.com>
2023-10-11 18:46:03 +0530

    simplified regex

--- src/lib/elements/forms/inputCron.svelte
+++ src/lib/elements/forms/inputCron.svelte
@@ -18,7 +18,7 @@
     let element: HTMLInputElement;
     let error: string;
 
-    const pattern = String.raw`/^(\*|\?|[0-5]?\d)(\,(\*|\?|[0-5]?\d)|\-(\*|\?|[0-5]?\d)|\/\d+)?\s+(\*|\?|[01]?\d|2[0-3])(\,(\*|\?|[01]?\d|2[0-3])|\-(\*|\?|[01]?\d|2[0-3])|\/\d+)?\s+(\*|\?|[1-9]|[12]\d|3[01])(W?)(\,(\*|\?|[1-9]|[12]\d|3[01])|\-(\*|\?|[1-9]|[12]\d|3[01])|\/\d+)?\s+(\*|\?|[1-9]|1[0-2]|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)(\,(\*|\?|[1-9]|1[0-2]|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)|\-(\*|\?|[1-9]|1[0-2]|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)|\/\d+)?\s+(\*|\?|[0-7]((\#?)(\d?))(\w?)|sun|mon|tue|wed|thu|fri|sat)(\,(\*|\?|[0-7]((\#?)(\d?))(\w?)|sun|mon|tue|wed|thu|fri|sat)|\-(\*|\?|[0-7]((\#?)(\d?))(\w?)|sun|mon|tue|wed|thu|fri|sat)|\/\d((\#?)(\d?))(\w?)+)?$/i`;
+    const pattern = String.raw`^.*\s+.*\s+.*\s+.*\s+.*`;
     onMount(() => {
         if (element && autofocus) {
             element.focus();
