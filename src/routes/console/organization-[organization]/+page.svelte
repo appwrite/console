ac05c0410905d3fdd8c9ff2b9c035d223b2149c1
wess <me@wess.io>
2023-10-23 11:20:03 -0400

    Fixes missing li tag for links and buttons

--- src/routes/console/organization-[organization]/+page.svelte
+++ src/routes/console/organization-[organization]/+page.svelte
@@ -135,35 +135,37 @@
             event="project"
             on:click={() => (showCreate = true)}>
             {#each data.projects.projects as project}
-                <GridItem1 href={`${base}/console/project-${project.$id}`}>
-                    <svelte:fragment slot="eyebrow">
-                        {project?.platforms?.length ? project?.platforms?.length : 'No'} apps
-                    </svelte:fragment>
-                    <svelte:fragment slot="title">
-                        {project.name}
-                    </svelte:fragment>
-                    {#if allServiceDisabled(project)}
-                        <p>
-                            <span class="icon-pause" aria-hidden="true" /> All services are disabled.
-                        </p>
-                    {/if}
-                    {@const platforms = filterPlatforms(
-                        project.platforms.map((platform) => getPlatformInfo(platform.type))
-                    )}
-                    {#each platforms as platform, i}
-                        {#if i < 3}
+                <li>
+                    <GridItem1 href={`${base}/console/project-${project.$id}`}>
+                        <svelte:fragment slot="eyebrow">
+                            {project?.platforms?.length ? project?.platforms?.length : 'No'} apps
+                        </svelte:fragment>
+                        <svelte:fragment slot="title">
+                            {project.name}
+                        </svelte:fragment>
+                        {#if allServiceDisabled(project)}
+                            <p>
+                                <span class="icon-pause" aria-hidden="true" /> All services are disabled.
+                            </p>
+                        {/if}
+                        {@const platforms = filterPlatforms(
+                            project.platforms.map((platform) => getPlatformInfo(platform.type))
+                        )}
+                        {#each platforms as platform, i}
+                            {#if i < 3}
+                                <Pill>
+                                    <span class={`icon-${platform.icon}`} aria-hidden="true" />
+                                    {platform.name}
+                                </Pill>
+                            {/if}
+                        {/each}
+                        {#if platforms?.length > 3}
                             <Pill>
-                                <span class={`icon-${platform.icon}`} aria-hidden="true" />
-                                {platform.name}
+                                +{project.platforms.length - 3}
                             </Pill>
                         {/if}
-                    {/each}
-                    {#if platforms?.length > 3}
-                        <Pill>
-                            +{project.platforms.length - 3}
-                        </Pill>
-                    {/if}
-                </GridItem1>
+                    </GridItem1>
+                </li>
             {/each}
             <svelte:fragment slot="empty">
                 <p>Create a new project</p>
