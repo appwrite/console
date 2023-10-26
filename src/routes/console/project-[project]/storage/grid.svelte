4353690a9ff1eab31ac6d9630295717031280f7d
wess <me@wess.io>
2023-10-26 13:13:46 -0400

    Removes unused var

---
+++ src/routes/console/project-[project]/storage/grid.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+    import { page } from '$app/stores';
+    import { CardContainer, GridItem1, Id } from '$lib/components';
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+    const project = $page.params.project;
+</script>
+
+<CardContainer total={data.buckets.total} event="bucket">
+    {#each data.buckets.buckets as bucket}
+        <GridItem1 href={`${base}/console/project-${project}/storage/bucket-${bucket.$id}`}>
+            <svelte:fragment slot="title">{bucket.name}</svelte:fragment>
+            <Id value={bucket.$id}>{bucket.$id}</Id>
+        </GridItem1>
+    {/each}
+    <svelte:fragment slot="empty">
+        <p>Create a new bucket</p>
+    </svelte:fragment>
+</CardContainer>
