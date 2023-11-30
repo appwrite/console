7ee2935c53987c0e335e488560013f9387899557
德华 刘 <christyjacob4@gmail.com>
2023-11-30 10:06:24 +0000

    fix: review comments

--- src/routes/console/createOrganization.svelte
+++ src/routes/console/createOrganization.svelte
@@ -10,6 +10,7 @@
     import { Submit, trackEvent, trackError } from '$lib/actions/analytics';
     import { ID } from '@appwrite.io/console';
     import Alert from '$lib/components/alert.svelte';
+    import { isCloud } from '$lib/system';
 
     export let show = false;
 
@@ -44,15 +45,18 @@
 </script>
 
 <Modal title="Create new organization" {error} onSubmit={create} size="big" bind:show>
-    <Alert type="info">
-        <svelte:fragment slot="title">Get ready for Appwrite Pro</svelte:fragment>
-        We will soon introduce the much-anticipated Pro plan. Your account will continue to have access
-        to <b>one free Starter organization</b>. If you manage more than one organization, you will need to
-        either upgrade to the Pro plan, transfer your projects to a Pro organization, or migrate to self-hosting.
-        <svelte:fragment slot="buttons">
-            <Button href="https://appwrite.io/pricing" external text>Learn more</Button>
-        </svelte:fragment>
-    </Alert>
+    {#if isCloud}
+        <Alert type="info">
+            <svelte:fragment slot="title">Get ready for Appwrite Pro</svelte:fragment>
+            We will soon introduce the much-anticipated Pro plan. Your account will continue to have
+            access to <b>one free Starter organization</b>. If you manage more than one
+            organization, you will need to either upgrade to the Pro plan, transfer your projects to
+            a Pro organization, or migrate to self-hosting.
+            <svelte:fragment slot="buttons">
+                <Button href="https://appwrite.io/pricing" external text>Learn more</Button>
+            </svelte:fragment>
+        </Alert>
+    {/if}
     <FormList>
         <InputText
             id="organization-name"
