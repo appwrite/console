8bd7c2753e7043bfc76dcfa68821bbad2ca2e9c3
Arman <arman@appwrite.io>
2023-12-04 15:22:52 +0100

    feat: refactor coupons

--- src/routes/console/wizard/cloudOrganizationChangeTier/confirmDetails.svelte
+++ src/routes/console/wizard/cloudOrganizationChangeTier/confirmDetails.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import { Box, CreditCardBrandImage } from '$lib/components';
+    import { CouponInput } from '$lib/components/billing';
     import { Pill } from '$lib/elements';
-    import { Button, FormList, InputText, InputTextarea } from '$lib/elements/forms';
+    import { FormList, InputTextarea } from '$lib/elements/forms';
     import { toLocaleDate } from '$lib/helpers/date';
     import { WizardStep } from '$lib/layout';
     import type { Coupon } from '$lib/sdk/billing';
@@ -21,13 +22,13 @@
         : $organization.billingCurrentInvoiceDate;
 
     let coupon: string = null;
-    let comment: string = null;
     let couponData: Partial<Coupon> = {
         code: null,
         status: null,
         credits: null
     };
-    let showCoupon: boolean = false;
+    let comment: string = null;
+
     async function fetchCard() {
         try {
             const card = await sdk.forConsole.billing.getPaymentMethod(
@@ -37,30 +38,6 @@
         } catch (error) {
             console.log(error);
         }
-    }
-
-    async function addCoupon() {
-        try {
-            const response = await sdk.forConsole.billing.getCoupon(coupon);
-            coupon = null;
-            couponData = response;
-            showCoupon = true;
-            $changeOrganizationTier.couponCode = response.$id;
-        } catch (error) {
-            console.log(error);
-            couponData.code = coupon;
-            couponData.status = 'error';
-            showCoupon = true;
-        }
-    }
-
-    function removeCoupon() {
-        couponData = {
-            code: null,
-            status: null,
-            credits: null
-        };
-        showCoupon = false;
     }
 
     $: downgradeToStarter = $changeOrganizationTier.billingPlan === 'tier-0';
@@ -123,41 +100,7 @@
         {/if}
 
         <Box class="u-margin-block-start-32 u-flex u-flex-vertical u-gap-16" radius="small">
-            <FormList gap={8}>
-                <InputText
-                    placeholder="Coupon code"
-                    id="code"
-                    label="Add credits"
-                    disabled={showCoupon && couponData?.status !== 'error'}
-                    bind:value={coupon}>
-                    <Button
-                        secondary
-                        disabled={(showCoupon && couponData?.status !== 'error') || !coupon}
-                        on:click={addCoupon}>
-                        Apply
-                    </Button>
-                </InputText>
-                {#if showCoupon}
-                    {#if couponData?.status === 'error'}
-                        <div>
-                            <span class="icon-exclamation-circle u-color-text-danger" />
-                            <span>
-                                {couponData.code} is not a valid promo code
-                            </span>
-                        </div>
-                    {:else}
-                        <div class="u-flex u-main-space-between u-cross-center">
-                            <div>
-                                <span class="icon-tag u-color-text-success" />
-                                <span>
-                                    {couponData.code} applied (-${couponData.credits})
-                                </span>
-                            </div>
-                            <Button text on:click={removeCoupon}>Remove</Button>
-                        </div>
-                    {/if}
-                {/if}
-            </FormList>
+            <CouponInput bind:coupon bind:couponData />
             {#if $changeOrganizationTier.billingPlan !== 'tier-0'}
                 <span class="u-flex u-main-space-between">
                     <p class="text">{plan.name} plan</p>
@@ -167,7 +110,7 @@
                     <p class="text">Additional members ({collaboratorsNumber})</p>
                     <p class="text">${collaboratorPrice * collaboratorsNumber}</p>
                 </span>
-                {#if showCoupon && couponData?.status !== 'error'}
+                {#if couponData?.status === 'active'}
                     <span class="u-flex u-main-space-between">
                         <p class="text">Credits applied ({couponData.credits})</p>
                         <p class="text">-${couponData.credits}</p>
@@ -178,7 +121,7 @@
             <span class="u-flex u-main-space-between">
                 <p class="text">Estimated total (in USD)</p>
                 <p class="text">
-                    ${showCoupon && couponData?.status !== 'error'
+                    ${couponData?.status === 'active'
                         ? totalExpences - couponData.credits || 0
                         : totalExpences}
                 </p>
