bf8c88c264f6daa18100dd2c6ac23a89a082695f
Arman <arman@appwrite.io>
2023-12-12 14:25:02 +0100

    feat: show max size alert on wizard

--- src/routes/console/project-[project]/storage/bucket-[bucket]/create-file/step1.svelte
+++ src/routes/console/project-[project]/storage/bucket-[bucket]/create-file/step1.svelte
@@ -1,18 +1,50 @@
 <script lang="ts">
-    import { CustomId } from '$lib/components';
+    import { Alert, CustomId } from '$lib/components';
     import { Pill } from '$lib/elements';
-    import { FormList, InputFile } from '$lib/elements/forms';
+    import { Button, FormList, InputFile } from '$lib/elements/forms';
+    import { humanFileSize, sizeToBytes } from '$lib/helpers/sizeConvertion';
     import WizardStep from '$lib/layout/wizardStep.svelte';
+    import { getServiceLimit, tierToPlan } from '$lib/stores/billing';
+    import { organization } from '$lib/stores/organization';
+    import { wizard } from '$lib/stores/wizard';
+    import { isCloud } from '$lib/system';
+    import ChangeOrganizationTierCloud from '$routes/console/changeOrganizationTierCloud.svelte';
     import { bucket } from '../store';
     import { createFile } from './store';
 
     let showCustomId = false;
+    const service = getServiceLimit('fileSize');
 </script>
 
 <WizardStep>
     <svelte:fragment slot="title">Create file</svelte:fragment>
     <svelte:fragment slot="subtitle">Upload a file to add it to your bucket.</svelte:fragment>
-    <FormList>
+
+    {#if isCloud}
+        {@const size = humanFileSize(sizeToBytes(service, 'MB', 1000))}
+        {@const plan = tierToPlan($organization?.billingPlan)}
+        <Alert type="info">
+            <p class="text">
+                The {plan.name} plan has a maximum upload file size limit of {Math.floor(
+                    parseInt(size.value)
+                )}{size.unit}.
+                {#if $organization?.billingPlan === 'tier-0'}
+                    Upgrade to allow files of a larger size.
+                {/if}
+            </p>
+            <svelte:fragment slot="action">
+                {#if $organization?.billingPlan === 'tier-0'}
+                    <div class="alert-buttons u-flex">
+                        <Button text on:click={() => wizard.start(ChangeOrganizationTierCloud)}>
+                            Upgrade plan
+                        </Button>
+                    </div>
+                {/if}
+            </svelte:fragment>
+        </Alert>
+    {/if}
+
+    <FormList class="u-margin-block-start-24">
         <div class="u-width-full-line u-overflow-hidden">
             <InputFile
                 required
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,49 +1,51 @@
+import { dev } from '$app/environment';
 import { version, build, files } from '$service-worker';
+if (!dev) {
+    const name = `cache-${version}`;
+    const cachedFiles = [...build, ...files];
 
-const name = `cache-${version}`;
-const cachedFiles = [...build, ...files];
+    self.addEventListener('install', (event: ExtendableEvent) => {
+        event.waitUntil(caches.open(name).then((cache) => cache.addAll(cachedFiles)));
+    });
 
-self.addEventListener('install', (event: ExtendableEvent) => {
-    event.waitUntil(caches.open(name).then((cache) => cache.addAll(cachedFiles)));
-});
+    self.addEventListener('activate', (event: ExtendableEvent) => {
+        event.waitUntil(
+            caches.keys().then(async (keys) => {
+                for (const key of keys) {
+                    if (!key.includes(version)) caches.delete(key);
+                }
+            })
+        );
+    });
 
-self.addEventListener('activate', (event: ExtendableEvent) => {
-    event.waitUntil(
-        caches.keys().then(async (keys) => {
-            for (const key of keys) {
-                if (!key.includes(version)) caches.delete(key);
-            }
-        })
-    );
-});
+    self.addEventListener('fetch', (event: FetchEvent) => {
+        const { request } = event;
 
-self.addEventListener('fetch', (event: FetchEvent) => {
-    const { request } = event;
+        if (request.method !== 'GET' || request.headers.has('range')) return;
 
-    if (request.method !== 'GET' || request.headers.has('range')) return;
+        const url = new URL(request.url);
+        const cached = caches.match(request);
 
-    const url = new URL(request.url);
-    const cached = caches.match(request);
+        if (url.origin === location.origin && cachedFiles.includes(url.pathname)) {
+            // always return build files from cache
+            event.respondWith(cached);
+        } else if (url.protocol === 'https:' || location.hostname === 'localhost') {
+            // hit the network for everything else...
+            const promise = fetch(request);
 
-    if (url.origin === location.origin && cachedFiles.includes(url.pathname)) {
-        // always return build files from cache
-        event.respondWith(cached);
-    } else if (url.protocol === 'https:' || location.hostname === 'localhost') {
-        // hit the network for everything else...
-        const promise = fetch(request);
+            // ...and cache successful responses...
+            promise.then((response) => {
+                // cache successful responses
+                if (response.ok && response.type === 'basic') {
+                    const clone = response.clone();
+                    caches.open(name).then((cache) => {
+                        cache.put(request, clone);
+                    });
+                }
+            });
 
-        // ...and cache successful responses...
-        promise.then((response) => {
-            // cache successful responses
-            if (response.ok && response.type === 'basic') {
-                const clone = response.clone();
-                caches.open(name).then((cache) => {
-                    cache.put(request, clone);
-                });
-            }
-        });
-
-        // ...but if it fails, fall back to cache if available
-        event.respondWith(promise.catch(() => cached || promise));
-    }
-});
+            // ...but if it fails, fall back to cache if available
+            event.respondWith(promise.catch(() => cached || promise));
+        }
+    });
+}
