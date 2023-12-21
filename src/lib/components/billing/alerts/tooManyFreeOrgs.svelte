969743ddbe67423a09ec8c44e36e5e695e8116af
Arman <arman@appwrite.io>
2023-12-21 17:59:59 +0100

    fix: another date

--- src/lib/components/billing/alerts/tooManyFreeOrgs.svelte
+++ src/lib/components/billing/alerts/tooManyFreeOrgs.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <HeaderAlert title="You are limited to one free organization per account">
-    All but one organization will be automatically upgraded to a Pro plan on <b>01 Feb 2024</b>. You
-    can add a payment method or transfer projects from your settings.
+    All but one organization will be automatically upgraded to a Pro plan on <b>31 January 2024</b>.
+    You can add a payment method or transfer projects from your settings.
 
     <svelte:fragment slot="buttons">
         <Button secondary href="/console/account/organizations">View organizations</Button>
