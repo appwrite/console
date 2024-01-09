899541593234e0487ea8c37a096fef029bd36de1
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-01-09 14:18:59 +0100

    fix: dpa button

--- src/routes/console/organization-[organization]/settings/downloadDPA.svelte
+++ src/routes/console/organization-[organization]/settings/downloadDPA.svelte
@@ -4,19 +4,11 @@
     import { sdk } from '$lib/stores/sdk';
 
     async function downloadPdf() {
+        console.log('download pdf');
         const today = new Date().toISOString();
         const prefs = await sdk.forConsole.account.getPrefs();
         const newPrefs = { ...prefs, DPA: today };
         sdk.forConsole.account.updatePrefs(newPrefs);
-
-        const baseUrl = window.location.origin;
-        const response = await fetch(`${baseUrl}/Appwrite_DPA.pdf`);
-        const blob = await response.blob();
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = 'Appwrite_DPA.pdf';
-        link.click();
     }
 </script>
 
@@ -26,7 +18,10 @@
     </div>
     <p class="text">
         After downloading, have the DPA signed by your organization's compliance authority (e.g.,
-        CEO or Compliance Manager) and submit it to privacy@appwrite.io.
+        CEO or Compliance Manager) and submit it to <a
+            class="link"
+            href="mailto:privacy@appwrite.io">privacy@appwrite.io</a
+        >.
     </p>
     <svelte:fragment slot="aside">
         <Box>
@@ -39,8 +34,10 @@
             </p>
             <Button
                 secondary
+                external
                 class="u-margin-block-start-16"
                 on:click={downloadPdf}
+                href="/Appwrite_DPA.pdf"
                 event="download_dpa">
                 <span class="icon-download" aria-hidden="true" />
                 <span class="text">Download</span>
