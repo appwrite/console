09d94be14a956b4e95901ebad1a8e0d0359002a2
Arman <arman@appwrite.io>
2024-01-16 13:33:56 +0100

    chore: add missing event

--- src/lib/elements/table/body.svelte
+++ src/lib/elements/table/body.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { trackEvent } from '$lib/actions/analytics';
     import { getServiceLimit, type PlanServices } from '$lib/stores/billing';
     import { wizard } from '$lib/stores/wizard';
     import { isCloud } from '$lib/system';
@@ -10,6 +11,7 @@
     export let service: PlanServices = null;
     export let name = service;
     export let total: number = null;
+    export let event: string = null;
 
     let columns = 0;
 
@@ -34,7 +36,14 @@
             <span class="u-flex u-gap-24 u-main-center u-cross-center">
                 <slot name="limit" {upgradeMethod} {limit}>
                     <span class="text">Upgrade your plan to add {name} to your organization</span>
-                    <Button secondary on:click={upgradeMethod}>Upgrade plan</Button>
+                    <Button
+                        secondary
+                        on:click={upgradeMethod}
+                        on:click={() =>
+                            trackEvent('click_organization_upgrade', {
+                                from: 'button',
+                                source: event ?? 'table_row_limit_reached'
+                            })}>Upgrade plan</Button>
                 </slot>
             </span>
         </td>
