5c03c73731364ab7cea502530f1213fa28df0917
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-01-17 01:43:29 +0000

    Update filterable subscriber columns
    Don't allow filtering on userName since the attribute isn't in the
    collection to be filtered.
    
    Allow filtering on type since it is available and it would be userful.
    
    Change $createdAt type to datetime to allow filtering by greater/less
    than.

--- src/routes/console/project-[project]/messaging/topics/topic-[topic]/subscribers/store.ts
+++ src/routes/console/project-[project]/messaging/topics/topic-[topic]/subscribers/store.ts
@@ -3,9 +3,9 @@ import { writable } from 'svelte/store';
 
 export const columns = writable<Column[]>([
     { id: '$id', title: 'Subscriber ID', type: 'string', show: true, width: 140 },
-    { id: 'userName', title: 'Name', type: 'string', show: true, width: 100 },
+    { id: 'userName', title: 'Name', type: 'string', show: true, filter: false, width: 100 },
     { id: 'targetId', title: 'Target ID', type: 'string', show: true, width: 140 },
     { id: 'target', title: 'Target', type: 'string', show: true, filter: false, width: 140 },
-    { id: 'type', title: 'Type', type: 'string', show: true, filter: false, width: 80 },
-    { id: '$createdAt', title: 'Created', type: 'string', show: true, width: 100 }
+    { id: 'type', title: 'Type', type: 'string', show: true, width: 80 },
+    { id: '$createdAt', title: 'Created', type: 'datetime', show: true, width: 100 }
 ]);
