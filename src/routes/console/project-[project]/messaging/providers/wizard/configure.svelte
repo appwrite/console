28525fe9f9ee1bf5dd83350807e5c8aa62d8d0c9
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-02-08 14:23:03 -0800

    Removed required prop from InputSwitch
    Having required will prevent submitting with value == false.

--- src/routes/console/project-[project]/messaging/providers/wizard/configure.svelte
+++ src/routes/console/project-[project]/messaging/providers/wizard/configure.svelte
@@ -131,7 +131,6 @@
                 <InputSwitch
                     label={input.label}
                     id={input.name}
-                    required={!input.optional}
                     bind:value={$providerParams[$provider][input.name]}>
                     <svelte:fragment slot="description">
                         {input.description}
