f2c008bb570fc7c535d6b1eb572622f9e7c95235
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-02-12 16:22:57 -0800

    Update copy for delete message card and modal
    Clarify behavior based on message status

--- src/routes/console/project-[project]/messaging/message-[message]/delete.svelte
+++ src/routes/console/project-[project]/messaging/message-[message]/delete.svelte
@@ -2,9 +2,10 @@
     import { BoxAvatar, CardGrid, Heading } from '$lib/components';
     import { Button } from '$lib/elements/forms';
     import { toLocaleDateTime } from '$lib/helpers/date';
-    import { MessageType } from '@appwrite.io/console';
+    import type { Models } from '@appwrite.io/console';
     import DeleteModal from './deleteModal.svelte';
-    import { message } from './store';
+
+    export let message: Models.Message & { data: Record<string, unknown> };
 
     let showDelete = false;
 </script>
@@ -12,32 +13,30 @@
 <CardGrid>
     <Heading tag="h6" size="7">Delete message</Heading>
     <p>
-        The message will be permanently deleted, including all data associated with this message.
-        This action is irreversible.
+        The message will be permanently deleted{message.status === 'scheduled'
+            ? ', and its delivery will be canceled'
+            : ''}. This action is irreversible.
     </p>
     <svelte:fragment slot="aside">
         <BoxAvatar>
             <svelte:fragment slot="title">
                 <h6 class="u-bold u-trim-1" data-private>
-                    {$message.data.title ??
-                        $message.data.subject ??
-                        $message.data.content ??
+                    {message.data.title ??
+                        message.data.subject ??
+                        message.data.content ??
                         'Message'}
                 </h6>
             </svelte:fragment>
             <p>
-                Last updated: {toLocaleDateTime($message.$updatedAt)}
+                Last updated: {toLocaleDateTime(message.$updatedAt)}
             </p>
         </BoxAvatar>
     </svelte:fragment>
 
     <svelte:fragment slot="actions">
-        <Button
-            disabled={$message.status === MessageType.Processing}
-            secondary
-            on:click={() => (showDelete = true)}
-            event="delete_file">Delete</Button>
+        <Button secondary on:click={() => (showDelete = true)} event="delete_message"
+            >Delete</Button>
     </svelte:fragment>
 </CardGrid>
 
-<DeleteModal bind:show={showDelete} />
+<DeleteModal {message} bind:show={showDelete} />
