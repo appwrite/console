aaab1118a0649f6e9c1f73b2a9f14630cc351aa0
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-02-24 17:48:46 +0100

    fix: various issues

--- src/lib/components/filters/content.svelte
+++ src/lib/components/filters/content.svelte
@@ -134,7 +134,7 @@
                 bind:value={operatorKey} />
         </ul>
         {#if column && operator && !operator?.hideInput}
-            <div class="u-margin-block-start-8">
+            <ul class="u-margin-block-start-8">
                 {#if column.type === 'integer' || column.type === 'double'}
                     <InputNumber id="value" bind:value placeholder="Enter value" />
                 {:else if column.type === 'boolean'}
@@ -150,7 +150,7 @@
                 {:else}
                     <InputText id="value" bind:value placeholder="Enter value" />
                 {/if}
-            </div>
+            </ul>
         {/if}
         <Button text disabled={isDisabled} class="u-margin-block-start-4" submit>
             <i class="icon-plus" />
---
+++ src/lib/helpers/exceptions.ts
@@ -0,0 +1,1 @@
+export class AppwriteInvalidArgumentException extends Error {}
