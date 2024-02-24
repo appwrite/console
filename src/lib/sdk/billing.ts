1f6e4a9c0a290d90ac4c57ed07c6b7ca24ff24e0
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-02-24 18:11:19 +0100

    fix: analyzer

--- src/lib/sdk/billing.ts
+++ src/lib/sdk/billing.ts
@@ -1,4 +1,4 @@
-import type { Client, Models, Query } from '@appwrite.io/console';
+import type { Client, Models } from '@appwrite.io/console';
 import type { Organization, OrganizationList } from '../stores/organization';
 import type { PaymentMethod } from '@stripe/stripe-js';
 import type { Tier } from '$lib/stores/billing';
@@ -274,7 +274,7 @@ export class Billing {
         this.client = client;
     }
 
-    async listOrganization(queries: Query[] = []): Promise<OrganizationList> {
+    async listOrganization(queries: string[] = []): Promise<OrganizationList> {
         const path = `/organizations`;
         const params = {
             queries
@@ -744,7 +744,7 @@ export class Billing {
 
     //ACCOUNT
 
-    async listPaymentMethods(queries: [] = []): Promise<PaymentList> {
+    async listPaymentMethods(queries: string[] = []): Promise<PaymentList> {
         const path = `/account/payment-methods`;
         const params = {
             queries
@@ -865,7 +865,7 @@ export class Billing {
         );
     }
 
-    async listAddresses(queries: Query[] = []): Promise<AddressesList> {
+    async listAddresses(queries: string[] = []): Promise<AddressesList> {
         const path = `/account/billing-addresses`;
         const params = {
             queries
