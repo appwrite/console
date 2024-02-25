f196bcfb485adfb36324aabf32a3449471319bbd
Matej Bačo <matejbaco2000@gmail.com>
2024-02-25 17:50:01 +0100

    Fix 2FA deletion copy

--- src/routes/console/project-[project]/auth/user-[user]/deleteMfa.svelte
+++ src/routes/console/project-[project]/auth/user-[user]/deleteMfa.svelte
@@ -4,8 +4,6 @@
     import { Modal } from '$lib/components';
     import { Dependencies } from '$lib/constants';
     import { Button } from '$lib/elements/forms';
-    import FormList from '$lib/elements/forms/formList.svelte';
-    import InputDigits from '$lib/elements/forms/inputDigits.svelte';
     import { addNotification } from '$lib/stores/notifications';
     import { sdk } from '$lib/stores/sdk';
     import { AuthenticatorType } from '@appwrite.io/console';
@@ -13,7 +11,6 @@
 
     export let showDelete = false;
 
-    let code: string;
     let error: string;
 
     async function deleteProvider() {
@@ -34,23 +31,23 @@
     }
 
     $: if (showDelete) {
-        code = '';
         error = '';
     }
 </script>
 
 <Modal
-    title="Delete authentication provider"
+    title="Delete authentication method"
     bind:show={showDelete}
     onSubmit={deleteProvider}
     icon="exclamation"
     state="warning"
     bind:error
     headerDivider={false}>
+    <p class="u-bold">
+        Deleting the authentication method will disable multi-factor authentication for this
+        account. To re-enable it, user will need to add a new authentication method.
+    </p>
     <p>Are you sure you want to delete this authentication method from your account?</p>
-    <FormList>
-        <InputDigits autofocus required bind:value={code} />
-    </FormList>
 
     <svelte:fragment slot="footer">
         <Button text on:click={() => (showDelete = false)}>Cancel</Button>
