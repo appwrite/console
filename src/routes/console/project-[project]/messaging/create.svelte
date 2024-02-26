41a7caa6b5a33abf73e64c295376a72ac222a4f5
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-02-25 22:47:03 -0800

    refactor(messaging): remove unused update message wizard code

--- src/routes/console/project-[project]/messaging/create.svelte
+++ src/routes/console/project-[project]/messaging/create.svelte
@@ -7,13 +7,12 @@
     import { sdk } from '$lib/stores/sdk';
     import { Submit, trackError, trackEvent } from '$lib/actions/analytics';
     import { addNotification } from '$lib/stores/notifications';
-    import { goto, invalidate } from '$app/navigation';
+    import { goto } from '$app/navigation';
     import { base } from '$app/paths';
     import { project } from '../store';
     import { wizard } from '$lib/stores/wizard';
-    import { providerType, messageParams, operation } from './wizard/store';
+    import { providerType, messageParams } from './wizard/store';
     import { ID, MessageStatus, MessagingProviderType, type Models } from '@appwrite.io/console';
-    import { Dependencies } from '$lib/constants';
 
     async function create() {
         try {
@@ -112,115 +111,9 @@
         }
     }
 
-    async function update() {
-        try {
-            let response: Models.Message;
-
-            const messageId = $messageParams[$providerType].messageId;
-
-            const params = $messageParams[$providerType];
-
-            console.log(params);
-
-            switch ($providerType) {
-                case MessagingProviderType.Email:
-                    response = await sdk.forProject.messaging.updateEmail(
-                        messageId,
-                        $messageParams[$providerType].topics,
-                        $messageParams[$providerType].users,
-                        $messageParams[$providerType].targets,
-                        $messageParams[$providerType].subject,
-                        $messageParams[$providerType].content,
-                        $messageParams[$providerType].status,
-                        $messageParams[$providerType].html,
-                        undefined,
-                        undefined,
-                        $messageParams[$providerType].scheduledAt
-                    );
-                    break;
-                case MessagingProviderType.Sms:
-                    response = await sdk.forProject.messaging.updateSms(
-                        messageId,
-                        $messageParams[$providerType].topics,
-                        $messageParams[$providerType].users,
-                        $messageParams[$providerType].targets,
-                        $messageParams[$providerType].content,
-                        $messageParams[$providerType].status,
-                        $messageParams[$providerType].scheduledAt
-                    );
-                    break;
-                case MessagingProviderType.Push:
-                    {
-                        const customData: Record<string, string> = {};
-                        const { data } = $messageParams[MessagingProviderType.Push];
-                        if (data && data.length > 0) {
-                            data.forEach((item) => {
-                                if (item[0] === '') return;
-                                customData[item[0]] = item[1];
-                            });
-                        }
-
-                        response = await sdk.forProject.messaging.updatePush(
-                            messageId,
-                            $messageParams[$providerType].topics,
-                            $messageParams[$providerType].users,
-                            $messageParams[$providerType].targets,
-                            $messageParams[$providerType].title,
-                            $messageParams[$providerType].body,
-                            customData,
-                            undefined,
-                            undefined,
-                            undefined,
-                            undefined,
-                            undefined,
-                            undefined,
-                            undefined,
-                            $messageParams[$providerType].status,
-                            $messageParams[$providerType].scheduledAt
-                        );
-                    }
-                    break;
-            }
-            wizard.hide();
-            let message = '';
-            switch (response.status) {
-                case MessageStatus.Draft:
-                    message = 'The message has been saved as draft.';
-                    break;
-                case MessageStatus.Processing:
-                    message = 'The message is queued for processing.';
-                    break;
-                case 'scheduled':
-                    // TODO: fix message status
-                    message = 'The message has been scheduled.';
-                    break;
-            }
-            addNotification({
-                type: 'success',
-                message
-            });
-            await invalidate(Dependencies.MESSAGING_MESSAGE);
-            trackEvent(Submit.MessagingMessageUpdate, {
-                providerType: $providerType,
-                status: params.status
-            });
-            await goto(`${base}/console/project-${$project.$id}/messaging/message-${response.$id}`);
-        } catch (error) {
-            addNotification({
-                type: 'error',
-                message: error.message
-            });
-            trackError(error, Submit.MessagingMessageUpdate);
-        }
-    }
-
     async function saveDraft() {
         $messageParams[$providerType].status = MessageStatus.Draft;
-        if ($operation === 'create') {
-            create();
-        } else {
-            update();
-        }
+        create();
     }
 
     const stepsComponents: WizardStepsType = new Map();
@@ -256,8 +149,4 @@
     });
 </script>
 
-<Wizard
-    title={$operation === 'create' ? 'Create message' : 'Update message'}
-    steps={stepsComponents}
-    on:finish={$operation === 'create' ? create : update}
-    finalAction="Send" />
+<Wizard title="Create message" steps={stepsComponents} on:finish={create} finalAction="Send" />
--- src/routes/console/project-[project]/messaging/wizard/smsFormList.svelte
+++ src/routes/console/project-[project]/messaging/wizard/smsFormList.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import { messageParams, providerType, operation } from './store';
+    import { messageParams, providerType } from './store';
     import { Button, FormList, InputEmail, InputRadio, InputTextarea } from '$lib/elements/forms';
     import { Pill } from '$lib/elements';
-    import { CustomId, Modal } from '$lib/components';
+    import { Modal } from '$lib/components';
     import { user } from '$lib/stores/user';
     import { clickOnEnter } from '$lib/helpers/a11y';
     import { ID, MessageStatus, MessagingProviderType } from '@appwrite.io/console';
@@ -83,21 +83,13 @@
                 </svelte:fragment>
             </Modal>
         </div>
-        {#if $operation === 'create'}
-            {#if !showCustomId}
-                <div>
-                    <Pill button on:click={() => (showCustomId = !showCustomId)}
-                        ><span class="icon-pencil" aria-hidden="true" /><span class="text">
-                            Message ID
-                        </span></Pill>
-                </div>
-            {:else}
-                <CustomId
-                    bind:show={showCustomId}
-                    name="Message"
-                    bind:id={$messageParams[$providerType].messageId}
-                    autofocus={false} />
-            {/if}
+        {#if !showCustomId}
+            <div>
+                <Pill button on:click={() => (showCustomId = !showCustomId)}
+                    ><span class="icon-pencil" aria-hidden="true" /><span class="text">
+                        Message ID
+                    </span></Pill>
+            </div>
         {/if}
     </FormList>
     <SMSPhone content={$messageParams[$providerType]['content']} classes="is-only-desktop" />
