bc9ddf084e261b2df965c88d4613075f7dbd1b32
wess <me@wess.io>
2024-02-26 11:54:40 -0500

    lint fixes

--- src/routes/console/project-[project]/settings/webhooks/failedModal.svelte
+++ src/routes/console/project-[project]/settings/webhooks/failedModal.svelte
@@ -7,7 +7,7 @@
     import Button from '$lib/elements/forms/button.svelte';
 
     export let show: boolean;
-    export let webhook: { [key: string]: any };
+    export let webhook: { [key: string]: string|number|boolean };
 
     const projectId = $page.params.project;
     const webhookPath = `${base}/console/project-${projectId}/settings/webhooks/${webhook.$id}`;
