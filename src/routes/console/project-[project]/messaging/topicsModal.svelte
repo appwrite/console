bbee6a19295e993b7ea75d588ef419217c06230a
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-02-27 17:53:37 -0800

    feat(messaging): show count of selected in topics/targets modals
    Also fix spacing in the modals.

--- src/routes/console/project-[project]/messaging/topicsModal.svelte
+++ src/routes/console/project-[project]/messaging/topicsModal.svelte
@@ -91,86 +91,112 @@
 </script>
 
 <Modal {title} bind:show onSubmit={submit} on:close={reset} headerDivider={false} size="big">
-    <p class="text">
-        Select existing topics you want to send this message to its targets. The message will be
-        sent only to <ProviderType type={providerType} noIcon /> targets.
-    </p>
-    <InputSearch
-        autofocus
-        disabled={totalResults === 0 && !search}
-        placeholder="Search for topics"
-        bind:value={search} />
-    {#if Object.keys(topicResultsById).length > 0}
-        <FormList>
-            <Table noMargin noStyles>
-                <TableBody>
-                    {#each Object.entries(topicResultsById) as [topicId, topic]}
-                        <TableRow>
-                            <TableCell>
-                                <InputCheckbox
-                                    id={topicId}
-                                    disabled={!!topicsById[topicId]}
-                                    checked={!!selected[topicId]}
-                                    on:change={(event) => onTopicSelection(event, topic)}>
-                                    <svelte:fragment slot="description">
-                                        <span class="title">
-                                            <span class="u-line-height-1-5">
-                                                <span class="body-text-1 u-bold" data-private>
-                                                    {topic.name}
-                                                </span>
-                                                <span
-                                                    class="collapsible-button-optional"
-                                                    style="--p-toggle-optional-color: var(--color-neutral-50);">
-                                                    ({getTotal(topic)} targets)
-                                                </span>
-                                            </span></span>
-                                    </svelte:fragment>
-                                </InputCheckbox>
-                            </TableCell>
-                        </TableRow>
-                    {/each}
-                </TableBody>
-            </Table>
-        </FormList>
-        <div class="u-flex u-main-space-between">
-            <p class="text">Total results: {totalResults}</p>
-            <PaginationInline limit={5} bind:offset sum={totalResults} hidePages />
+    <div class="u-flex u-flex-vertical u-gap-32">
+        <p class="text">
+            Select existing topics you want to send this message to its targets. The message will be
+            sent only to <ProviderType type={providerType} noIcon /> targets.
+        </p>
+        <div class="u-flex u-flex-vertical u-gap-16">
+            <InputSearch
+                autofocus
+                disabled={totalResults === 0 && !search}
+                placeholder="Search for topics"
+                bind:value={search} />
+            <div>
+                {#if Object.keys(topicResultsById).length > 0}
+                    <div class="u-flex-vertical u-gap-8">
+                        <FormList>
+                            <Table noMargin noStyles>
+                                <TableBody>
+                                    {#each Object.entries(topicResultsById) as [topicId, topic]}
+                                        <TableRow>
+                                            <TableCell>
+                                                <InputCheckbox
+                                                    id={topicId}
+                                                    disabled={!!topicsById[topicId]}
+                                                    checked={!!selected[topicId]}
+                                                    on:change={(event) =>
+                                                        onTopicSelection(event, topic)}>
+                                                    <svelte:fragment slot="description">
+                                                        <span class="title">
+                                                            <span class="u-line-height-1-5">
+                                                                <span
+                                                                    class="body-text-1 u-bold"
+                                                                    data-private>
+                                                                    {topic.name}
+                                                                </span>
+                                                                <span
+                                                                    class="collapsible-button-optional"
+                                                                    style="--p-toggle-optional-color: var(--color-neutral-50);">
+                                                                    ({getTotal(topic)} targets)
+                                                                </span>
+                                                            </span></span>
+                                                    </svelte:fragment>
+                                                </InputCheckbox>
+                                            </TableCell>
+                                        </TableRow>
+                                    {/each}
+                                </TableBody>
+                            </Table>
+                        </FormList>
+                        <div class="u-flex u-main-space-between u-cross-center">
+                            <p class="text">Total results: {totalResults}</p>
+                            <PaginationInline limit={5} bind:offset sum={totalResults} hidePages />
+                        </div>
+                    </div>
+                {:else if search}
+                    <div class="u-flex-vertical u-gap-8">
+                        <EmptySearch hidePagination>
+                            <div class="common-section">
+                                <div class="u-text-center common-section">
+                                    <b class="body-text-2 u-bold"
+                                        >Sorry we couldn't find "{search}"</b>
+                                    <p>There are no topics that match your search.</p>
+                                </div>
+                                <div class="u-flex u-gap-16 common-section u-main-center">
+                                    <Button
+                                        external
+                                        href="https://appwrite.io/docs/products/messaging/topics"
+                                        text>Documentation</Button>
+                                    <Button secondary on:click={() => (search = '')}
+                                        >Clear search</Button>
+                                </div>
+                            </div>
+                        </EmptySearch>
+                        <div class="u-flex u-main-space-between u-cross-center">
+                            <p class="text">Total results: {totalResults}</p>
+                            <PaginationInline limit={5} bind:offset sum={totalResults} hidePages />
+                        </div>
+                    </div>
+                {:else}
+                    <EmptySearch hidePagination>
+                        <div class="common-section">
+                            <div class="u-text-center common-section">
+                                <p class="text u-line-height-1-5">
+                                    You have no topics. Create a topic to see them here.
+                                </p>
+                                <p class="text u-line-height-1-5">
+                                    Need a hand? Learn more in our <Button
+                                        link
+                                        external
+                                        href="https://appwrite.io/docs/products/messaging/topics"
+                                        >documentation</Button
+                                    >.
+                                </p>
+                            </div>
+                        </div>
+                    </EmptySearch>
+                {/if}
+            </div>
         </div>
-    {:else if search}
-        <EmptySearch hidePages>
-            <div class="common-section">
-                <div class="u-text-center common-section">
-                    <b class="body-text-2 u-bold">Sorry we couldn't find "{search}"</b>
-                    <p>There are no topics that match your search.</p>
-                </div>
-                <div class="u-flex u-gap-16 common-section u-main-center">
-                    <Button external href="https://appwrite.io/docs/products/messaging/topics" text
-                        >Documentation</Button>
-                    <Button secondary on:click={() => (search = '')}>Clear search</Button>
-                </div>
-            </div>
-        </EmptySearch>
-    {:else}
-        <EmptySearch hidePages>
-            <div class="common-section">
-                <div class="u-text-center common-section">
-                    <p class="text u-line-height-1-5">
-                        You have no topics. Create a topic to see them here.
-                    </p>
-                    <p class="text u-line-height-1-5">
-                        Need a hand? Learn more in our <a
-                            href="https://appwrite.io/docs/products/messaging/topics"
-                            target="_blank"
-                            rel="noopener noreferrer">
-                            documentation</a
-                        >.
-                    </p>
-                </div>
-            </div>
-        </EmptySearch>
-    {/if}
-
+    </div>
     <svelte:fragment slot="footer">
-        <Button submit disabled={!hasSelection}>Add</Button>
+        <div class="u-flex u-gap-16 u-cross-center">
+            <div class="u-flex u-gap-8">
+                <span class="inline-tag"><span class="text">{selectedSize}</span></span>
+                <span class="body-text-2">Topics selected</span>
+            </div>
+            <Button submit disabled={!hasSelection}>Add</Button>
+        </div>
     </svelte:fragment>
 </Modal>
