b43c76c9bbeb068871f446dcb8d3d56b320d0c40
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-03-11 16:13:07 +0100

    fix(messaging): fix files not fully populating in provier details
    If we don't wait until files is populated, the InputFile will mount with
    files being empty and then when files gets updated, it won't set the
    input.files.

--- src/routes/console/project-[project]/messaging/providers/provider-[provider]/updateSettings.svelte
+++ src/routes/console/project-[project]/messaging/providers/provider-[provider]/updateSettings.svelte
@@ -26,6 +26,7 @@
     let updated = false;
     let newMemberModal = false;
     let message = '';
+    let ready = false;
 
     const { displayName } = getProviderDisplayNameAndIcon(provider);
 
@@ -61,6 +62,7 @@
                 originalfiles[input.name] = dataTransfer.files;
             }
         }
+        ready = true;
     });
 
     async function update() {
@@ -229,7 +231,10 @@
             provider settings.
         </p>
         <svelte:fragment slot="aside">
-            <SettingsFormList bind:files {inputs} bind:params={formValues} />
+            <!-- Must wait until ready or else the files input won't be set properly -->
+            {#if ready}
+                <SettingsFormList bind:files {inputs} bind:params={formValues} />
+            {/if}
         </svelte:fragment>
 
         <svelte:fragment slot="actions">
