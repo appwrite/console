bee05359d9568e746c071fca650902331ad1668c
ItzNotABug <itznotabug@gmail.com>
2024-04-04 12:40:22 +0530

    fix: convert array to object for perf.

--- src/lib/helpers/date.ts
+++ src/lib/helpers/date.ts
@@ -99,17 +99,17 @@ export const utcHourToLocaleHour = (utcTimeString: string) => {
 export const utcWeekDayToLocaleWeekDay = (utcDay: WeekDay, utcTimeString: string) => {
     const now = new Date();
 
-    const daysOfTheWeek: WeekDay[] = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-    ];
+    const daysOfTheWeek = {
+        Sunday: 0,
+        Monday: 1,
+        Tuesday: 2,
+        Wednesday: 3,
+        Thursday: 4,
+        Friday: 5,
+        Saturday: 6
+    };
 
-    const utcDayNumber = daysOfTheWeek.indexOf(utcDay);
+    const utcDayNumber = daysOfTheWeek[utcDay];
     const [hours, minutes] = utcTimeString.split(':').map(Number);
 
     const daysUntilNextUtcDay = (7 + utcDayNumber - now.getUTCDay()) % 7;
