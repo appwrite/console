4b45edc4966e43c30c133b5bf79921118148058d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-04-29 20:48:17 +0200

    fix: static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import { preprocessMeltUI } from '@melt-ui/pp';
 import sequence from 'svelte-sequential-preprocessor';
@@ -15,7 +15,9 @@ const config = {
         alias: {
             $routes: './src/routes'
         },
-        adapter: adapter(),
+        adapter: adapter({
+            fallback: 'index.html'
+        }),
         paths: {
             base: '/console'
         }
