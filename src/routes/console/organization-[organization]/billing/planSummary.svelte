1943520834210f6c7c937662ca6ce96abe40e00f
Arman <arman@appwrite.io>
2024-05-08 14:04:18 +0200

    fix: design review

--- src/routes/console/organization-[organization]/billing/planSummary.svelte
+++ src/routes/console/organization-[organization]/billing/planSummary.svelte
@@ -59,63 +59,48 @@
                         {isTrial ? formatCurrency(0) : formatCurrency(currentPlan?.price)}
                     </div>
                 </CollapsibleItem>
-
-                <CollapsibleItem isInfo>
-                    <svelte:fragment slot="beforetitle">
-                        Add-ons <span class="inline-tag"
-                            >{extraMembers
-                                ? currentInvoice?.usage?.length + 1
-                                : currentInvoice?.usage?.length}</span>
-                        <div class="icon">
-                            <span class="icon-cheveron-down" aria-hidden="true"></span>
-                        </div>
-                    </svelte:fragment>
-                    <svelte:fragment slot="end">
-                        <div class="body-text-2 u-margin-inline-start-auto">
-                            {formatCurrency(extraUsage)}
-                        </div>
-                    </svelte:fragment>
-                    {#if extraUsage && $organization?.billingPlan !== BillingPlan.STARTER && !isTrial}
-                        <ul>
-                            {#if extraMembers}
-                                <li class="u-flex-vertical u-gap-4 u-padding-block-8">
-                                    <div class="u-flex u-gap-4">
-                                        <h5 class="body-text-2 u-stretch">Additional members</h5>
-                                        <div>
-                                            {formatCurrency(extraMembers * 15)}
-                                        </div>
-                                    </div>
-                                    <div class="u-flex u-gap-4">
-                                        <h5 class="body-text-2 u-stretch u-color-text-offline">
-                                            {extraMembers}
-                                        </h5>
-                                    </div>
-                                </li>
-                            {/if}
-                        </ul>
-                    {/if}
-                    {#if currentInvoice?.usage}
-                        {#each currentInvoice.usage as excess}
-                            <li class="u-flex-vertical u-gap-4 u-padding-block-8">
-                                {#if ['storage', 'bandwidth'].includes(excess.name)}
-                                    {@const excessValue = humanFileSize(excess.value)}
-                                    <div class="u-flex u-gap-4">
-                                        <h5 class="body-text-2 u-stretch">{excess.name}</h5>
-                                        <div>
-                                            {formatCurrency(excess.amount)}
-                                        </div>
-                                    </div>
-                                    <div class="u-flex u-gap-4">
-                                        <h5
-                                            class="body-text-2 u-stretch u-color-text-offline"
-                                            title={formatNumberWithCommas(excess.value ?? 0) +
-                                                'bytes'}>
-                                            {excessValue.value ?? 0}{excessValue.unit}
-                                        </h5>
-                                    </div>
-                                {/if}
-                                {#if ['users', 'executions'].includes(excess.name)}
+                {#if $organization?.billingPlan !== BillingPlan.STARTER}
+                    <CollapsibleItem isInfo>
+                        <svelte:fragment slot="beforetitle">
+                            <span class="body-text-2">Add-ons</span><span class="inline-tag"
+                                >{extraMembers
+                                    ? currentInvoice?.usage?.length + 1
+                                    : currentInvoice?.usage?.length}</span>
+                            <div class="icon">
+                                <span class="icon-cheveron-down" aria-hidden="true"></span>
+                            </div>
+                        </svelte:fragment>
+                        <svelte:fragment slot="end">
+                            <div class="body-text-2 u-margin-inline-start-auto">
+                                {formatCurrency(extraUsage)}
+                            </div>
+                        </svelte:fragment>
+                        {#if extraUsage && !isTrial}
+                            <ul>
+                                {#if extraMembers}
                                     <li class="u-flex-vertical u-gap-4 u-padding-block-8">
+                                        <div class="u-flex u-gap-4">
+                                            <h5 class="body-text-2 u-stretch">
+                                                Additional members
+                                            </h5>
+                                            <div>
+                                                {formatCurrency(extraMembers * 15)}
+                                            </div>
+                                        </div>
+                                        <div class="u-flex u-gap-4">
+                                            <h5 class="body-text-2 u-stretch u-color-text-offline">
+                                                {extraMembers}
+                                            </h5>
+                                        </div>
+                                    </li>
+                                {/if}
+                            </ul>
+                        {/if}
+                        {#if currentInvoice?.usage}
+                            {#each currentInvoice.usage as excess}
+                                <li class="u-flex-vertical u-gap-4 u-padding-block-8">
+                                    {#if ['storage', 'bandwidth'].includes(excess.name)}
+                                        {@const excessValue = humanFileSize(excess.value)}
                                         <div class="u-flex u-gap-4">
                                             <h5 class="body-text-2 u-stretch">{excess.name}</h5>
                                             <div>
@@ -125,16 +110,34 @@
                                         <div class="u-flex u-gap-4">
                                             <h5
                                                 class="body-text-2 u-stretch u-color-text-offline"
-                                                title={formatNumberWithCommas(excess.value)}>
-                                                {abbreviateNumber(excess.value)}
+                                                title={formatNumberWithCommas(excess.value ?? 0) +
+                                                    'bytes'}>
+                                                {excessValue.value ?? 0}{excessValue.unit}
                                             </h5>
                                         </div>
-                                    </li>
-                                {/if}
-                            </li>
-                        {/each}
-                    {/if}
-                </CollapsibleItem>
+                                    {/if}
+                                    {#if ['users', 'executions'].includes(excess.name)}
+                                        <li class="u-flex-vertical u-gap-4 u-padding-block-8">
+                                            <div class="u-flex u-gap-4">
+                                                <h5 class="body-text-2 u-stretch">{excess.name}</h5>
+                                                <div>
+                                                    {formatCurrency(excess.amount)}
+                                                </div>
+                                            </div>
+                                            <div class="u-flex u-gap-4">
+                                                <h5
+                                                    class="body-text-2 u-stretch u-color-text-offline"
+                                                    title={formatNumberWithCommas(excess.value)}>
+                                                    {abbreviateNumber(excess.value)}
+                                                </h5>
+                                            </div>
+                                        </li>
+                                    {/if}
+                                </li>
+                            {/each}
+                        {/if}
+                    </CollapsibleItem>
+                {/if}
 
                 <CollapsibleItem noContent isInfo gap={4}>
                     <span class="body-text-2">Current total (USD)</span>
