33474bf7d413fa78185f094c0a5ea5163fe14342
Arman <arman@appwrite.io>
2024-06-05 12:45:40 +0200

    chore: fix copy

--- src/lib/stores/campaigns.ts
+++ src/lib/stores/campaigns.ts
@@ -11,7 +11,7 @@ campaigns
     .set('startup', {
         title: 'Welcome to the Startups program!',
         description:
-            'We’re excited to have you on board. Add your credit code to your Appwrite Pro account to join.'
+            "We're excited to have you on board. Add your credit code to your Appwrite Pro account to join."
     })
     .set('RenderATL2024', {
         title: 'Claim your RenderATL $100 credits',
