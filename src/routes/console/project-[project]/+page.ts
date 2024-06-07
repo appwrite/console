4fb243f6b7575deaceba7d389f189cb8086ba65c
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-06-07 13:59:01 +0200

    fix: progress bar

--- src/routes/console/project-[project]/+page.ts
+++ src/routes/console/project-[project]/+page.ts
@@ -3,5 +3,5 @@ import { base } from '$app/paths';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params }) => {
-    redirect(302, `${base}/console/project-${params.project}/overview`);
+    redirect(303, `${base}/console/project-${params.project}/overview`);
 };
