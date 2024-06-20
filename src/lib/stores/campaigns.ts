a34762677591c3f60a499e14b10e0b5bdf29ae83
Arman <arman@appwrite.io>
2024-06-20 15:05:58 +0200

    feat: 'review' template for campaigns

--- src/lib/stores/campaigns.ts
+++ src/lib/stores/campaigns.ts
@@ -1,40 +1,65 @@
 //campaign welcome and startup
 
-type CampaignData = {
+export type CampaignData = {
     title: string;
     description: string;
+    template: 'card' | 'review';
+    data?: Record<string, unknown>;
+    onlyNewOrgs?: boolean;
 };
 
 export const campaigns: Map<string, CampaignData> = new Map();
 
 campaigns
     .set('welcome', {
+        template: 'card',
         title: "You've received $VALUE in credits",
         description:
             'Get $VALUE in credits when you upgrade or create an organization with a Pro plan.'
     })
     .set('startup', {
+        template: 'card',
         title: 'Welcome to the Startups program!',
         description:
             "We're excited to have you on board. Add the coupon code to your Appwrite Pro account to join."
     })
     .set('RenderATL2024', {
+        template: 'card',
         title: 'Claim your $100 RenderATL credits',
         description:
             'Get $100 in Cloud credits when you upgrade or create an organization with a Pro plan.'
     })
     .set('dealsfordevs', {
+        template: 'card',
         title: 'Claim your $50 Deals For Devs credits',
         description:
             'Get $50 in Cloud credits when you upgrade or create an organization with a Pro plan.'
     })
     .set('WelcomeManual', {
+        template: 'card',
         title: 'Here is your $VALUE welcome credit!',
         description:
             'Upgrade to Appwrite Pro and add the credits to enjoy the full capabilities of Cloud.'
     })
     .set('InactiveAccounts', {
+        template: 'card',
         title: 'Get everything out of Cloud. Claim your $VALUE credits.',
         description:
             'Get $VALUE in Cloud credits when you upgrade or create an organization with a Pro plan.'
+    })
+    .set('Startups', {
+        template: 'review',
+        title: 'Welcome to the Appwrite Startups program.',
+        description:
+            "We're excited to have you on board. Add your credit code to your Appwrite Pro account to join.",
+        onlyNewOrgs: true,
+        data: {
+            reviews: [
+                {
+                    name: 'John Doe',
+                    desc: 'Acme Inc.',
+                    review: 'We really loved working with Appwrite for launching our bootstrapped "Open Mind" App. It was saving us a lot of money in comparison to Firebase since the amount of users grew quite fast and we needed a quick switch.'
+                }
+            ]
+        }
     });
