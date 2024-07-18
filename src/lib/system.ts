a13dc7d0791ed2fadbcc6ee7f4f4cf1ff4deff9f
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-07-18 23:08:10 +0200

    chore run formatter

--- src/lib/system.ts
+++ src/lib/system.ts
@@ -7,10 +7,10 @@ export const enum Mode {
 }
 
 export const VARS = {
-    CONSOLE_MODE: env.PUBLIC_CONSOLE_MODE as Mode ?? undefined,
+    CONSOLE_MODE: (env.PUBLIC_CONSOLE_MODE as Mode) ?? undefined,
     APPWRITE_ENDPOINT: env.PUBLIC_APPWRITE_ENDPOINT ?? undefined,
     GROWTH_ENDPOINT: env.PUBLIC_GROWTH_ENDPOINT ?? undefined,
-    PUBLIC_STRIPE_KEY: env.PUBLIC_STRIPE_KEY ?? undefined,
+    PUBLIC_STRIPE_KEY: env.PUBLIC_STRIPE_KEY ?? undefined
 };
 
 export const ENV = {
