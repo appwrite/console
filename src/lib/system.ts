c4dbe0c5c3cc32d26d648b32f6f59ffe437e46a7
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-07-18 23:05:36 +0200

    fix: env variables

--- src/lib/system.ts
+++ src/lib/system.ts
@@ -1,5 +1,5 @@
 import { env } from '$env/dynamic/public';
-import { PUBLIC_CONSOLE_MODE } from '$env/static/public';
+import { dev } from '$app/environment';
 
 export const enum Mode {
     CLOUD = 'cloud',
@@ -7,15 +7,15 @@ export const enum Mode {
 }
 
 export const VARS = {
-    CONSOLE_MODE: PUBLIC_CONSOLE_MODE as Mode,
+    CONSOLE_MODE: env.PUBLIC_CONSOLE_MODE as Mode ?? undefined,
     APPWRITE_ENDPOINT: env.PUBLIC_APPWRITE_ENDPOINT ?? undefined,
     GROWTH_ENDPOINT: env.PUBLIC_GROWTH_ENDPOINT ?? undefined,
     PUBLIC_STRIPE_KEY: env.PUBLIC_STRIPE_KEY ?? undefined,
 };
 
 export const ENV = {
-    DEV: import.meta.env.DEV,
-    PROD: import.meta.env.PROD,
+    DEV: dev,
+    PROD: !dev,
     PREVIEW: import.meta.env?.VERCEL === '1',
     TEST: !!import.meta.env?.VITEST
 };
