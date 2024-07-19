eb12341f1d861b526609e5f5b5187210026c36c1
Carla <cg.alexander@gmail.com>
2024-07-20 01:22:24 +1000

    fix typos

--- src/routes/(console)/project-[project]/functions/function-[function]/executions/+page.svelte
+++ src/routes/(console)/project-[project]/functions/function-[function]/executions/+page.svelte
@@ -243,7 +243,7 @@
                 <p class="text u-line-height-1-5">
                     You have no execution logs. Create and activate a deployment to see it here.
                 </p>
-                <p class="text u-line-height-1-5">Need a hand? Learn more in our documentation</p>
+                <p class="text u-line-height-1-5">Need a hand? Learn more in our documentation.</p>
             </div>
             <div class="u-flex u-gap-16">
                 <Button text external href="https://appwrite.io/docs/products/functions/execution">
