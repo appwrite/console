d32d1c889e0c04270dfbed99ff5dfc3c729e3222
Bradley Schofield <ionicisere@gmail.com>
2024-07-29 18:01:59 +0900

    Add MFA Creation and deletion templates

--- src/routes/console/project-[project]/auth/templates/+page.svelte
+++ src/routes/console/project-[project]/auth/templates/+page.svelte
@@ -60,6 +60,8 @@
         EmailTemplateLocale
     } from '@appwrite.io/console';
     import Email2FaTemplate from './email2FATemplate.svelte';
+    import EmailMfaAddedTemplate from './emailMfaAddedTemplate.svelte';
+    import EmailMfaRemovedTemplate from './emailMfaRemovedTemplate.svelte';
 
     const projectId = $page.params.project;
 
@@ -69,6 +71,8 @@
     $: emailResetPassword = emailOpen === 'recovery';
     $: emailInviteUser = emailOpen === 'invitation';
     $: email2FAVerificationOpen = emailOpen === 'mfaChallenge';
+    $: emailMFAAddedOpen = emailOpen === 'mfaAdded';
+    $: emailMFARemovedOpen = emailOpen === 'mfaRemoved';
 
     // let smsOpen = 'verification';
     // $: smsVerificationOpen = smsOpen === 'verification';
@@ -189,6 +193,26 @@
                     <p class="text">Send a two-factor authentication email to a user.</p>
                     <Email2FaTemplate />
                 </CollapsibleItem>
+                <CollapsibleItem
+                    bind:open={emailMFAAddedOpen}
+                    on:click={(e) => {
+                        e.preventDefault();
+                        openEmail('mfaAdded');
+                    }}>
+                    <svelte:fragment slot="title">MFA authenticator added</svelte:fragment>
+                    <p class="text">Send a email to a user when an MFA authenticator is added.</p>
+                    <EmailMfaAddedTemplate />
+                </CollapsibleItem>
+                <CollapsibleItem
+                    bind:open={emailMFARemovedOpen}
+                    on:click={(e) => {
+                        e.preventDefault();
+                        openEmail('mfaRemoved');
+                    }}>
+                    <svelte:fragment slot="title">MFA authenticator removed</svelte:fragment>
+                    <p class="text">Send a email to a user when an MFA authenticator is removed.</p>
+                    <EmailMfaRemovedTemplate />
+            </CollapsibleItem>
             </Collapsible>
         </svelte:fragment>
         <svelte:fragment slot="actions">
