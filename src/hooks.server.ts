3ac6b7e277ef7fc255cfdc014fbf0f9827c0a4a0
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-08-27 13:33:29 +0200

    fix: build with sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,9 +2,10 @@ import { sequence } from '@sveltejs/kit/hooks';
 import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 import { isCloud, isProd } from '$lib/system';
+import { SENTRY_AUTH_TOKEN } from '$env/static/private';
 
 Sentry.init({
-    enabled: isCloud && isProd,
+    enabled: isCloud && isProd && !!SENTRY_AUTH_TOKEN,
     dsn: 'https://c7ce178bdedd486480317b72f282fd39@o1063647.ingest.us.sentry.io/4504158071422976',
     tracesSampleRate: 1.0
 });
