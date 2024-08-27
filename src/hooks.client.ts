1bfe5699d3cb6ae918b2f5c15743402e94c449e3
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-08-27 13:37:35 +0200

    fix: remove env var check for sentry

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,12 +2,11 @@ import * as Sentry from '@sentry/sveltekit';
 import { AppwriteException } from '@appwrite.io/console';
 import type { HandleClientError } from '@sveltejs/kit';
 import { isCloud, isProd } from '$lib/system';
-import { SENTRY_AUTH_TOKEN } from '$env/static/private';
 
 // If you don't want to use Session Replay, remove the `Replay` integration,
 // `replaysSessionSampleRate` and `replaysOnErrorSampleRate` options.
 Sentry.init({
-    enabled: isCloud && isProd && !!SENTRY_AUTH_TOKEN,
+    enabled: isCloud && isProd,
     dsn: 'https://c7ce178bdedd486480317b72f282fd39@o1063647.ingest.us.sentry.io/4504158071422976',
     tracesSampleRate: 1,
     replaysSessionSampleRate: 0,
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,10 +2,9 @@ import { sequence } from '@sveltejs/kit/hooks';
 import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 import { isCloud, isProd } from '$lib/system';
-import { SENTRY_AUTH_TOKEN } from '$env/static/private';
 
 Sentry.init({
-    enabled: isCloud && isProd && !!SENTRY_AUTH_TOKEN,
+    enabled: isCloud && isProd,
     dsn: 'https://c7ce178bdedd486480317b72f282fd39@o1063647.ingest.us.sentry.io/4504158071422976',
     tracesSampleRate: 1.0
 });
