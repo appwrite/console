7cb88651f307bbb33e81d392e5759c54b4ae95b6
Arman <arman@appwrite.io>
2024-09-09 17:50:58 +0200

    fix: support modal doens't close after button click

--- src/routes/(console)/wizard/support/mobileSupportModal.svelte
+++ src/routes/(console)/wizard/support/mobileSupportModal.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+    import { beforeNavigate } from '$app/navigation';
     import { Support } from '$lib/components';
     import Modal from '$lib/components/modal.svelte';
 
     export let show: boolean;
+
+    beforeNavigate(() => {
+        show = false;
+    });
 </script>
 
 <Modal title="Contact us" bind:show>
