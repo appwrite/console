8749248f2c24f47c7d5f539eac5f418ce832b7d4
Arman <arman@appwrite.io>
2024-09-09 17:54:46 +0200

    fix: global modal close on navigate

--- src/routes/(console)/wizard/support/mobileSupportModal.svelte
+++ src/routes/(console)/wizard/support/mobileSupportModal.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-    import { beforeNavigate } from '$app/navigation';
     import { Support } from '$lib/components';
     import Modal from '$lib/components/modal.svelte';
 
     export let show: boolean;
-
-    beforeNavigate(() => {
-        show = false;
-    });
 </script>
 
 <Modal title="Contact us" bind:show>
