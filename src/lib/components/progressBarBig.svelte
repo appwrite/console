bd0cb1d52e92c1d51c62fdf298234b1bd0f3926a
ernstmul <ernst.mulders@gmail.com>
2024-09-16 17:04:04 +0200

    Fix project usage

--- src/lib/components/progressBarBig.svelte
+++ src/lib/components/progressBarBig.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import { ProgressBar, type ProgressbarData } from '$lib/components/progressbar';
 
-    export let currentValue: string;
-    export let currentUnit: string;
-    export let maxValue: string;
-    export let maxUnit: string;
+    export let currentValue: string | undefined = undefined;
+    export let currentUnit: string | undefined = undefined;
+    export let maxValue: string | undefined = undefined;
+    export let maxUnit: string | undefined = undefined;
     export let progressValue: number;
     export let progressMax: number;
     export let showBar = true;
@@ -14,19 +14,22 @@
 </script>
 
 <section class="progress-bar">
-    <div class="u-flex u-flex-vertical">
-        <div class="u-flex u-main-space-between">
-            <p>
-                <span class="heading-level-4">{currentValue}</span>
-                <span class="body-text-1 u-bold">{currentUnit}</span>
+    {#if currentValue && currentUnit && progress && maxValue && maxUnit}
+        <div class="u-flex u-flex-vertical">
+            <div class="u-flex u-main-space-between">
+                <p>
+                    <span class="heading-level-4">{currentValue}</span>
+                    <span class="body-text-1 u-bold">{currentUnit}</span>
+                </p>
+                <p class="heading-level-4">{progress}%</p>
+            </div>
+
+            <p class="body-text-2">
+                {maxValue}
+                {maxUnit}
             </p>
-            <p class="heading-level-4">{progress}%</p>
         </div>
-        <p class="body-text-2">
-            {maxValue}
-            {maxUnit}
-        </p>
-    </div>
+    {/if}
     {#if showBar && progressBarData.length > 0}
         <ProgressBar maxSize={progressMax} data={progressBarData} />
     {/if}
