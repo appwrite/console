d17c8a4e06f4bd3a19a9539f15868507fb5919ab
ItzNotABug <itznotabug@gmail.com>
2024-09-23 22:01:50 +0530

    fix: spacing.

--- src/lib/components/bottomModalAlert.svelte
+++ src/lib/components/bottomModalAlert.svelte
@@ -45,6 +45,23 @@
         <div class="alert-container">
             <article class="card">
                 {#key currentModalAlert.id}
+                    <button
+                        class="icon-inline-tag"
+                        on:click={() => handleClose(currentModalAlert)}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 20 20"
+                            fill="none">
+                            <path
+                                fill-rule="evenodd"
+                                clip-rule="evenodd"
+                                d="M4.29289 4.29289C4.68342 3.90237 5.31658 3.90237 5.70711 4.29289L10 8.58579L14.2929 4.29289C14.6834 3.90237 15.3166 3.90237 15.7071 4.29289C16.0976 4.68342 16.0976 5.31658 15.7071 5.70711L11.4142 10L15.7071 14.2929C16.0976 14.6834 16.0976 15.3166 15.7071 15.7071C15.3166 16.0976 14.6834 16.0976 14.2929 15.7071L10 11.4142L5.70711 15.7071C5.31658 16.0976 4.68342 16.0976 4.29289 15.7071C3.90237 15.3166 3.90237 14.6834 4.29289 14.2929L8.58579 10L4.29289 5.70711C3.90237 5.31658 3.90237 4.68342 4.29289 4.29289Z"
+                                fill="#97979B" />
+                        </svg>
+                    </button>
+
                     <div class="content-wrapper u-flex-vertical u-gap-16">
                         {#if $app.themeInUse === 'dark'}
                             <img
@@ -57,25 +74,6 @@
                                 alt={currentModalAlert.title}
                                 class="showcase-image u-image-object-fit-contain u-block u-only-light" />
                         {/if}
-
-                        <div>
-                            <button
-                                class="icon-inline-tag"
-                                on:click={() => handleClose(currentModalAlert)}>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 20 20"
-                                    fill="none">
-                                    <path
-                                        fill-rule="evenodd"
-                                        clip-rule="evenodd"
-                                        d="M4.29289 4.29289C4.68342 3.90237 5.31658 3.90237 5.70711 4.29289L10 8.58579L14.2929 4.29289C14.6834 3.90237 15.3166 3.90237 15.7071 4.29289C16.0976 4.68342 16.0976 5.31658 15.7071 5.70711L11.4142 10L15.7071 14.2929C16.0976 14.6834 16.0976 15.3166 15.7071 15.7071C15.3166 16.0976 14.6834 16.0976 14.2929 15.7071L10 11.4142L5.70711 15.7071C5.31658 16.0976 4.68342 16.0976 4.29289 15.7071C3.90237 15.3166 3.90237 14.6834 4.29289 14.2929L8.58579 10L4.29289 5.70711C3.90237 5.31658 3.90237 4.68342 4.29289 4.29289Z"
-                                        fill="#97979B" />
-                                </svg>
-                            </button>
-                        </div>
 
                         {#if filteredModalAlerts.length > 1}
                             <div class="u-flex u-main-space-between u-cross-center">
