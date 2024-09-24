392bf05a4b5c118d5ea769bb28e5e6c0360931e4
Damodar Lohani <dlohani48@gmail.com>
2024-09-24 07:45:42 +0545

    update

--- src/routes/(console)/organization-[organization]/createMember.svelte
+++ src/routes/(console)/organization-[organization]/createMember.svelte
@@ -70,7 +70,7 @@
                 <b>{formatCurrency(plan.addons.member.price)} each per billing period</b>.
             </Alert> -->
             <Alert type="info">
-                New members are free until 01 December 2024. <a class="link" href="https://appwrite.io/docs/roles" target="_blank" rel="noopener noreferrer">Learn more</a>.
+                New roles are free until 01 December 2024. <a class="link" href="https://appwrite.io/docs/roles" target="_blank" rel="noopener noreferrer">Learn more</a>.
             </Alert>
         {/if}
     {/if}
