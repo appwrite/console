eb6d278b1a03ee04d1f4395e14bbe97969d58d03
ItzNotABug <itznotabug@gmail.com>
2024-09-26 12:41:44 +0530

    add: database backups promo.

--- src/routes/(console)/bottomAlerts.ts
+++ src/routes/(console)/bottomAlerts.ts
@@ -1,6 +1,9 @@
 import { base } from '$app/paths';
 import RolesDark from '$lib/images/roles-dark.png';
 import RolesLight from '$lib/images/roles-light.png';
+import BackupsDark from '$lib/images/backups/backups-empty-dark.svg';
+import BackupsLight from '$lib/images/backups/backups-empty-light.svg';
+
 import { type BottomModalAlertItem, showBottomModalAlert } from '$lib/stores/bottom-alerts';
 
 const listOfPromotions: BottomModalAlertItem[] = [
@@ -19,6 +22,28 @@ const listOfPromotions: BottomModalAlertItem[] = [
         cta: {
             text: 'Try now',
             link: ({ organization }) => `${base}/organization-${organization.$id}/members`
+        },
+        learnMore: {
+            text: 'Learn more',
+            link: () => 'https://appwrite.io/docs/advanced/platform/roles'
+        }
+    },
+    {
+        id: 'databaseBackups',
+        src: {
+            dark: BackupsDark,
+            light: BackupsLight
+        },
+        title: 'Database Backups are available now',
+        message:
+            'Protect your data and ensure quick recovery with our new backups.<br/><b>Try for free until Jan 1st 2025 on paid plans.</b>',
+
+        isHtml: true,
+        plan: 'pro',
+        scope: 'project',
+        cta: {
+            text: 'Try now',
+            link: ({ project }) => `${base}/project-${project.$id}/databases`
         },
         learnMore: {
             text: 'Learn more',
