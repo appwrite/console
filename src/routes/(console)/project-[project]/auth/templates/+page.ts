6b5dccec577c539319ed1e4b5ff78627d28c486a
Arman <arman@appwrite.io>
2024-10-01 12:10:45 +0200

    fix: review

--- src/routes/(console)/project-[project]/auth/templates/+page.ts
+++ src/routes/(console)/project-[project]/auth/templates/+page.ts
@@ -1,10 +1,8 @@
 import { sdk } from '$lib/stores/sdk';
 
-export const load = async ({ parent }) => {
-    const { project } = await parent();
+export const load = async () => {
     const codes = await sdk.forProject.locale.listCodes();
     return {
-        project,
         localeCodes: codes.localeCodes
     };
 };
