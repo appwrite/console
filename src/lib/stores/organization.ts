1c3747017c2b6492ee8a003b75a1ed481d2ec6e8
ernstmul <ernst.mulders@gmail.com>
2024-10-07 14:04:38 +0200

    Remove tier-3 override

--- src/lib/stores/organization.ts
+++ src/lib/stores/organization.ts
@@ -42,8 +42,5 @@ export const organizationList = derived(
     ($page) => $page.data.organizations as Models.TeamList<Record<string, unknown>>
 );
 
-// TODO: remove override to tier-3 when BE PR, https://github.com/appwrite-labs/cloud/pull/816, has been merged and deployed -->
-export const organization = derived(page, ($page) => {
-    return { ...($page.data?.organization as Organization), billingPlan: 'tier-3' as Tier };
-});
+export const organization = derived(page, ($page) => $page.data?.organization as Organization);
 export const members = derived(page, ($page) => $page.data.members as Models.MembershipList);
