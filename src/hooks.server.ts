d18d9fb90994aa9a064d7ba692df2aa01300ba3f
Arman <arman@appwrite.io>
2024-10-18 15:07:41 +0200

    refactor: move header to nginx file

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,22 +9,6 @@ Sentry.init({
     tracesSampleRate: 1.0
 });
 
-const securityHeaders = {
-    'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
-    'Content-Security-Policy': `default-src 'self'; script-src 'report-sample' 'self' https://js.stripe.com/v3; style-src 'report-sample' 'self'; object-src 'none'; base-uri 'self'; connect-src 'self' https://growth.appwrite.io https://*.sentry.io https://plausible.io; font-src 'self' https://fonts.appwrite.io; frame-src 'self' https://js.stripe.com; img-src 'self'; manifest-src 'self'; media-src 'self'; worker-src 'self' blob:;` // We can also add a report-uri for CSP violations e.g. 'report-uri https://some-endpoint/;'
-};
-
-async function setSecurityHeaders({ event, resolve }) {
-    const response = await resolve(event);
-    if (isCloud && isProd) {
-        Object.entries(securityHeaders).forEach(([header, value]) =>
-            response.headers.set(header, value)
-        );
-    }
-
-    return response;
-}
-
-export const handle = sequence(setSecurityHeaders, sentryHandle());
+export const handle = sequence(sentryHandle());
 
 export const handleError = handleErrorWithSentry();
