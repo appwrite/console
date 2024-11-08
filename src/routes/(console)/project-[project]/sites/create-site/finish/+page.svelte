65410791096bc8d2a1e83571b9a23f1bda9753c5
Arman <arman@appwrite.io>
2024-11-08 15:43:32 +0100

    add cards

--- src/routes/(console)/project-[project]/sites/create-site/finish/+page.svelte
+++ src/routes/(console)/project-[project]/sites/create-site/finish/+page.svelte
@@ -11,6 +11,7 @@
     import Link from '$lib/elements/link.svelte';
     import { humanFileSize } from '$lib/helpers/sizeConvertion';
     import { calculateTime } from '$lib/helpers/timeConversion';
+    import { deployment } from '$routes/(console)/project-[project]/functions/function-[function]/deployment-[deployment]/store';
 
     export let data: PageData;
 
@@ -21,67 +22,84 @@
     title="Create a site"
     hideAside={true}
     href={`${base}/project-${$page.params.project}/sites/site-${$page.params.site}`}>
-    <Layout.Stack gap="l" direction="column" alignItems="center">
-        <Check />
-        <Layout.Stack gap="s" direction="column" alignItems="center">
-            <Typography.Text variant="l-400">Congratulations!</Typography.Text>
-            <Typography.Text variant="m-400">You deployed your Site successfully.</Typography.Text>
-        </Layout.Stack>
-    </Layout.Stack>
-    <Card padding="xs">
-        <Layout.Stack gap="m" direction="row">
-            <Image
-                width={445}
-                height={280}
-                src="https://f002.backblazeb2.com/file/meldiron-public/Desktop+-+2.png"
-                alt="Screenshot" />
-            <Layout.Stack justifyContent="center">
-                <Layout.Stack gap="xs">
-                    <Typography.Text variant="m-400" color="--color-fgcolor-neutral-tertiary">
-                        Deployed
-                    </Typography.Text>
-                    {#if data.deployment.providerCommitAuthor}
-                        <Typography.Text variant="m-400">
-                            {timeFromNow(data.deployment.$updatedAt)} ago by {data.deployment
-                                .providerCommitAuthor}
-                        </Typography.Text>
-                    {:else}
-                        <Typography.Text variant="m-400">
-                            {timeFromNow(data.deployment.$updatedAt)}
-                        </Typography.Text>
-                    {/if}
-                </Layout.Stack>
-                <Layout.Stack gap="xs">
-                    <Typography.Text variant="m-400" color="--color-fgcolor-neutral-tertiary"
-                        >Domains</Typography.Text>
-                    <Link href="https://{data.rule.domain}">{data.rule.domain}</Link>
-                </Layout.Stack>
-                <Layout.Stack gap="xl" direction="row">
-                    <Layout.Stack gap="xs" inline>
-                        <Typography.Text variant="m-400" color="--color-fgcolor-neutral-tertiary"
-                            >Build time</Typography.Text>
-                        <Typography.Text variant="m-400"
-                            >{calculateTime(data.deployment.buildTime)}</Typography.Text>
-                    </Layout.Stack>
-                    <Layout.Stack gap="xs" inline>
-                        <Typography.Text variant="m-400" color="--color-fgcolor-neutral-tertiary"
-                            >Total size</Typography.Text>
-                        <Typography.Text variant="m-400">{size.value}{size.unit}</Typography.Text>
-                    </Layout.Stack>
-                </Layout.Stack>
-                {#if data.deployment.providerCommitHash}
-                    <Layout.Stack gap="xs">
-                        <Typography.Text variant="m-400" color="--color-fgcolor-neutral-tertiary"
-                            >Source</Typography.Text>
-                        <Typography.Text variant="m-400"
-                            >{data.deployment.providerBranch} branch</Typography.Text>
-                        <Typography.Text variant="m-400"
-                            >{data.deployment.providerCommitHash} initial commit</Typography.Text>
-                    </Layout.Stack>
-                {/if}
+    <Layout.Stack gap="l">
+        <Layout.Stack gap="l" direction="column" alignItems="center">
+            <Check />
+            <Layout.Stack gap="s" direction="column" alignItems="center">
+                <Typography.Text variant="l-400">Congratulations!</Typography.Text>
+                <Typography.Text variant="m-400"
+                    >You deployed your Site successfully.</Typography.Text>
             </Layout.Stack>
         </Layout.Stack>
-    </Card>
+        <Card padding="xs">
+            <Layout.Stack gap="m" direction="row">
+                <Image
+                    width={445}
+                    height={280}
+                    src="https://f002.backblazeb2.com/file/meldiron-public/Desktop+-+2.png"
+                    alt="Screenshot" />
+                <Layout.Stack justifyContent="center">
+                    <Layout.Stack gap="xs">
+                        <Typography.Text variant="m-400" color="--color-fgcolor-neutral-tertiary">
+                            Deployed
+                        </Typography.Text>
+                        {#if data.deployment.providerCommitAuthor}
+                            <Typography.Text variant="m-400">
+                                {timeFromNow(data.deployment.$updatedAt)} ago by {data.deployment
+                                    .providerCommitAuthor}
+                            </Typography.Text>
+                        {:else}
+                            <Typography.Text variant="m-400">
+                                {timeFromNow(data.deployment.$updatedAt)}
+                            </Typography.Text>
+                        {/if}
+                    </Layout.Stack>
+                    <Layout.Stack gap="xs">
+                        <Typography.Text variant="m-400" color="--color-fgcolor-neutral-tertiary"
+                            >Domains</Typography.Text>
+                        <Link href="https://{data.rule.domain}">{data.rule.domain}</Link>
+                    </Layout.Stack>
+                    <Layout.Stack gap="xl" direction="row">
+                        <Layout.Stack gap="xs" inline>
+                            <Typography.Text
+                                variant="m-400"
+                                color="--color-fgcolor-neutral-tertiary"
+                                >Build time</Typography.Text>
+                            <Typography.Text variant="m-400"
+                                >{calculateTime(data.deployment.buildTime)}</Typography.Text>
+                        </Layout.Stack>
+                        <Layout.Stack gap="xs" inline>
+                            <Typography.Text
+                                variant="m-400"
+                                color="--color-fgcolor-neutral-tertiary"
+                                >Total size</Typography.Text>
+                            <Typography.Text variant="m-400"
+                                >{size.value}{size.unit}</Typography.Text>
+                        </Layout.Stack>
+                    </Layout.Stack>
+                    {#if data.deployment.providerCommitHash}
+                        <Layout.Stack gap="xs">
+                            <Typography.Text
+                                variant="m-400"
+                                color="--color-fgcolor-neutral-tertiary">Source</Typography.Text>
+                            <Typography.Text variant="m-400"
+                                >{data.deployment.providerBranch} branch</Typography.Text>
+                            <Typography.Text variant="m-400"
+                                >{data.deployment.providerCommitHash} initial commit</Typography.Text>
+                        </Layout.Stack>
+                    {/if}
+                </Layout.Stack>
+            </Layout.Stack>
+        </Card>
+
+        {#if !data.deployment.providerCommitHash}
+            <Layout.Stack direction="row">
+                <Card isTile>asdsads</Card>
+                <Card isTile>asdsads</Card>
+                <Card isTile>asdsads</Card>
+            </Layout.Stack>
+        {/if}
+    </Layout.Stack>
     <svelte:fragment slot="footer">
         <Button
             size="s"
