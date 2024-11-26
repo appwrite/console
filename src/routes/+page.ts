aab0d9567a5f1c389b8d7597003f067a958dd3f1
Darshan <itznotabug@gmail.com>
2024-11-26 11:08:21 +0530

    fix: import and env var name.

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 import { base } from '$app/paths';
 import type { PageLoad } from './$types';
 import { sdk } from '$lib/stores/sdk';
-import { PUBLIC_GROWTH_ENDPOINT } from '$env/static/public';
+import { VARS } from '$lib/system';
 
 const handleGithubEducationMembership = async (name: string, email: string) => {
     const result = await sdk.forConsole.billing.setMembership('github-student-developer');
@@ -44,7 +44,7 @@ export const load: PageLoad = async ({ parent, url }) => {
 
 const setToGhStudentMailingList = async (name: string, email: string) => {
     const body = name !== '' ? { name, email } : { email };
-    return fetch(`${PUBLIC_GROWTH_ENDPOINT}/mailinglists/gh-student`, {
+    return fetch(`${VARS.GROWTH_ENDPOINT}/mailinglists/gh-student`, {
         method: 'POST',
         body: JSON.stringify(body),
         headers: {
