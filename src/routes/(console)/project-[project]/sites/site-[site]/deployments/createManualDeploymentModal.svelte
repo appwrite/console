5b4f184a1ba2d7421acdf4a771b1f9aa086c3fcf
Arman <arman@appwrite.io>
2024-12-12 16:14:57 +0100

    feat: file to tar gzip

--- src/routes/(console)/project-[project]/sites/site-[site]/deployments/createManualDeploymentModal.svelte
+++ src/routes/(console)/project-[project]/sites/site-[site]/deployments/createManualDeploymentModal.svelte
@@ -5,19 +5,49 @@
     import { Button } from '$lib/elements/forms';
     import { addNotification } from '$lib/stores/notifications';
     import { sdk } from '$lib/stores/sdk';
-    import type { Framework, Models } from '@appwrite.io/console';
+    import type { Models } from '@appwrite.io/console';
     import { IconInfo } from '@appwrite.io/pink-icons-svelte';
     import { Icon, Layout, Tooltip, Typography, Upload } from '@appwrite.io/pink-svelte';
+    import { createTar, createTarGzip, type TarFileInput } from 'nanotar';
 
     export let show = false;
     export let site: Models.Site;
 
     let files: FileList;
+
+    let uploadFile: File;
+    async function handleChange(event: CustomEvent<FileList>) {
+        if (files?.length === 1) {
+            const file = files.item(0);
+            // If the file is a tar.gz file, then return
+            if (file.type === 'application/gzip' && file.name.split('.').pop() === 'tar') {
+                uploadFile = file;
+            }
+        } else {
+            let TarFileInputArray: TarFileInput[] = [];
+            Array.from(files).forEach((file) => {
+                let fileUint8Array: Uint8Array;
+                file.arrayBuffer().then((buff) => {
+                    fileUint8Array = new Uint8Array(buff);
+                });
+
+                TarFileInputArray.push({
+                    name: file.name,
+                    data: fileUint8Array
+                });
+            });
+            const tar = await createTarGzip(TarFileInputArray);
+            const blob = new Blob([tar], { type: 'application/gzip' });
+            const file = new File([blob], 'deployment.tar.gz', { type: 'application/gzip' });
+            uploadFile = file;
+        }
+    }
+
     async function createDeployment() {
         try {
             await sdk.forProject.sites.createDeployment(
                 site.$id,
-                files,
+                uploadFile,
                 true, //TODO: fix
                 undefined,
                 undefined,
@@ -43,7 +73,7 @@
 
 <Modal title="Create manual deployment" bind:show onSubmit={createDeployment}>
     <span slot="description"> Manually deploy a site by uploading any file(s) or folder. </span>
-    <Upload.Dropzone multiple bind:files>
+    <Upload.Dropzone folder bind:files on:change={handleChange}>
         <Layout.Stack alignItems="center" gap="s">
             <Layout.Stack alignItems="center" justifyContent="center" direction="row" gap="s">
                 <Typography.Text variant="l-500">
