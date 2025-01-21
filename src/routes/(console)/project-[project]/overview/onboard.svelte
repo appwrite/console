8b15b9ca0d96defdc71e4a6c1e95a14aff8a49d3
Ernst Mulders <ernst.mulders@gmail.com>
2025-01-21 14:22:47 +0100

    Update src/routes/(console)/project-[project]/overview/onboard.svelte
    Co-authored-by: Darshan <itznotabug@gmail.com>

--- src/routes/(console)/project-[project]/overview/onboard.svelte
+++ src/routes/(console)/project-[project]/overview/onboard.svelte
@@ -130,7 +130,7 @@
                             class="onboarding-card platform-card"
                             on:click={() => addPlatform(2)}>
                             <img
-                                src={$app.themeInUse == 'dark'
+                                src={$app.themeInUse === 'dark'
                                     ? PlatformAndroidImgSourceDark
                                     : PlatformAndroidImgSource}
                                 alt="" />
