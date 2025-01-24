71a706ebf70cecd543000b7e39c48a05db012c36
Arman <arman@appwrite.io>
2025-01-24 16:14:39 +0100

    feat: invite screens

--- src/routes/(public)/auth/preview/+page.ts
+++ src/routes/(public)/auth/preview/+page.ts
@@ -1,7 +1,14 @@
+import { base } from '$app/paths';
+import { redirect } from '@sveltejs/kit';
+
 export const load = async ({ parent }) => {
     const { account } = await parent();
 
-    return {
-        account
-    };
+    if (!account) {
+        redirect(303, `${base}/auth/preview/access`);
+    } else {
+        return {
+            account
+        };
+    }
 };
