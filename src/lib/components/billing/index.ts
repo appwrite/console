8e994cba4ed814d684e3871f30ccfafd6bcb7326
Damodar Lohani <dlohani48@gmail.com>
2025-01-26 06:21:08 +0000

    remove old component

--- src/lib/components/billing/index.ts
+++ src/lib/components/billing/index.ts
@@ -2,7 +2,6 @@ export { default as PaymentBoxes } from './paymentBoxes.svelte';
 export { default as CouponInput } from './couponInput.svelte';
 export { default as SelectPaymentMethod } from './selectPaymentMethod.svelte';
 export { default as UsageRates } from './usageRates.svelte';
-export { default as EstimatedTotalBox } from './estimatedTotalBox.svelte';
 export { default as PlanComparisonBox } from './planComparisonBox.svelte';
 export { default as EmptyCardCloud } from './emptyCardCloud.svelte';
 export { default as CreditsApplied } from './creditsApplied.svelte';
