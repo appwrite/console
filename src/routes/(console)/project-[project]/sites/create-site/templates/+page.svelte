1155e523935025820365ea2e6aa829355ef4643a
Arman <arman@appwrite.io>
2025-02-10 11:27:12 +0100

    fix: template

--- src/routes/(console)/project-[project]/sites/create-site/templates/+page.svelte
+++ src/routes/(console)/project-[project]/sites/create-site/templates/+page.svelte
@@ -7,6 +7,7 @@
     import { goto } from '$app/navigation';
     import { debounce } from '$lib/helpers/debounce.js';
     import { Card, Layout, Accordion, Selector } from '@appwrite.io/pink-svelte';
+    import { capitalize } from '$lib/helpers/string';
 
     export let data;
 
@@ -69,7 +70,7 @@
     invertColumns
     hideFooter>
     <svelte:fragment slot="aside">
-        <Layout.Stack gap="xxl">
+        <Layout.Stack gap="xl">
             <InputSearch
                 placeholder="Search templates"
                 value={$page.url.searchParams.get('search')}
@@ -84,7 +85,7 @@
                                 <Selector.Checkbox
                                     id={useCase}
                                     size="s"
-                                    label={useCase}
+                                    label={capitalize(useCase)}
                                     checked={isChecked(useCase)}
                                     on:change={(e) => {
                                         applyFilter('useCase', useCase, e);
