016120b60ae58bed2562d5fd0fc54263dec0aaea
Arman <arman@appwrite.io>
2025-02-11 15:37:52 +0100

    fix: variable creation

--- src/routes/(console)/project-[project]/sites/create-site/repositories/repository-[repository]/+page.svelte
+++ src/routes/(console)/project-[project]/sites/create-site/repositories/repository-[repository]/+page.svelte
@@ -90,16 +90,15 @@
             });
 
             //Add variables
-            await Promise.all(
-                Object.keys(variables).map(async (key) => {
-                    await sdk.forProject.sites.createVariable(
-                        site.$id,
-                        key,
-                        variables[key].value,
-                        variables[key].secret
-                    );
-                })
+            const promises = variables.map((variable) =>
+                sdk.forProject.sites.createVariable(
+                    site.$id,
+                    variable.key,
+                    variable.value,
+                    variable?.secret ?? false
+                )
             );
+            await Promise.all(promises);
 
             const { deployments } = await sdk.forProject.sites.listDeployments(site.$id, [
                 Query.limit(1)
@@ -117,6 +116,8 @@
             trackError(e, Submit.SiteCreate);
         }
     }
+
+    $: console.log(variables);
 </script>
 
 <svelte:head>
