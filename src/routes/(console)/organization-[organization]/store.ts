cacb2d25f80f01041d5dc4c466eeb500c5bbbf70
Darshan <itznotabug@gmail.com>
2025-02-16 14:40:34 +0530

    update: safe-fallback.

--- src/routes/(console)/organization-[organization]/store.ts
+++ src/routes/(console)/organization-[organization]/store.ts
@@ -6,6 +6,8 @@ import { sdk } from '$lib/stores/sdk';
 
 export const regions = writable<RegionList | undefined>(undefined);
 export const regionFlagUrls = derived(regions, ($regions) => {
+    if (!$regions?.regions?.length) return [];
+
     return $regions?.regions?.map((region) => {
         return `${sdk.forConsole.client.config.endpoint}/avatars/flags/${region.flag}?width=80&height=60&quality=100&mode=admin`;
     });
