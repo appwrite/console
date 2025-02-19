e496fb45820f06087005deeedd9aebc86b7305ec
Darshan <itznotabug@gmail.com>
2025-02-19 09:53:40 +0530

    update: component for friction as per latest changes.

--- src/routes/(console)/project-[project]/sites/site-[site]/settings/deleteModal.svelte
+++ src/routes/(console)/project-[project]/sites/site-[site]/settings/deleteModal.svelte
@@ -3,13 +3,17 @@
     import { base } from '$app/paths';
     import { page } from '$app/stores';
     import { Submit, trackEvent, trackError } from '$lib/actions/analytics';
+    import { FormList, InputCheckbox } from '$lib/elements/forms';
     import Confirm from '$lib/components/confirm.svelte';
     import { addNotification } from '$lib/stores/notifications';
     import { sdk } from '$lib/stores/sdk';
 
     export let showDelete = false;
     const siteId = $page.params.site;
+
     let error: string;
+    let confirmedDeletion = false;
+
     const handleSubmit = async () => {
         try {
             await sdk.forProject.sites.delete(siteId);
@@ -27,6 +31,20 @@
     };
 </script>
 
-<Confirm onSubmit={handleSubmit} title="Delete site" bind:open={showDelete} bind:error>
-    Are you sure you want to delete this site and all associated deployments from your project?
+<Confirm
+    onSubmit={handleSubmit}
+    disabled={!confirmedDeletion}
+    title="Delete site"
+    bind:open={showDelete}
+    bind:error>
+    <FormList>
+        Are you sure you want to delete this site and all associated deployments from your project?
+
+        <InputCheckbox
+            size="s"
+            required
+            id="delete_site"
+            bind:checked={confirmedDeletion}
+            label="I understand and confirm" />
+    </FormList>
 </Confirm>
