cacb67e1d718fe48a48313b259850cbf72fb7d88
ernstmul <ernst.mulders@gmail.com>
2025-03-06 10:46:47 +0100

    WIP messaging

--- src/routes/(console)/project-[project]/messaging/topics/topic-[topic]/+page.svelte
+++ src/routes/(console)/project-[project]/messaging/topics/topic-[topic]/+page.svelte
@@ -14,7 +14,7 @@
     import type { PageData } from './$types';
     import { sdk } from '$lib/stores/sdk';
     import { addNotification } from '$lib/stores/notifications';
-    import { Submit, trackError, trackEvent } from '$lib/actions/analytics';
+    import { Click, Submit, trackError, trackEvent } from '$lib/actions/analytics';
     import { invalidate } from '$app/navigation';
     import { Dependencies } from '$lib/constants';
     import Table from './table.svelte';
@@ -91,7 +91,12 @@
         <div class="u-flex u-main-space-between">
             <Typography.Title>Subscribers</Typography.Title>
             <div class="is-only-mobile">
-                <Button on:click={() => (showAdd = true)} event="create_subscriber">
+                <Button
+                    on:click={() => {
+                        showAdd = true;
+                        trackEvent(Click.MessagingTargetCreateClick);
+                    }}
+                    event="create_subscriber">
                     <Icon icon={IconPlus} slot="start" size="s" />
                     Add subscriber
                 </Button>
