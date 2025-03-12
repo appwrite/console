c41e1da1e853bbb6e66174270b2a3de72f11a254
Arman <arman@appwrite.io>
2025-03-12 17:42:03 +0100

    fix: increase template default page limit

--- src/routes/(console)/project-[project]/functions/templates/+page.ts
+++ src/routes/(console)/project-[project]/functions/templates/+page.ts
@@ -1,4 +1,4 @@
-import { CARD_LIMIT, Dependencies } from '$lib/constants';
+import { Dependencies, PAGE_LIMIT } from '$lib/constants';
 import { getLimit, getPage, getSearch, getView, pageToOffset, View } from '$lib/helpers/load';
 import { sdk } from '$lib/stores/sdk';
 import type { PageLoad } from './$types';
@@ -6,7 +6,7 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ url, route, depends, parent }) => {
     depends(Dependencies.FUNCTIONS);
 
-    const limit = getLimit(url, route, CARD_LIMIT);
+    const limit = getLimit(url, route, PAGE_LIMIT);
     const page = getPage(url);
     const search = getSearch(url);
     const view = getView(url, route, View.Grid);
