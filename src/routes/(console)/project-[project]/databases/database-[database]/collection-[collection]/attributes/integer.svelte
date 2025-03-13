8a44b92add3328ef8ea104ec23e68c14bf879d42
ernstmul <ernst.mulders@gmail.com>
2025-03-13 14:21:50 +0100

    Use safe integer check

--- src/routes/(console)/project-[project]/databases/database-[database]/collection-[collection]/attributes/integer.svelte
+++ src/routes/(console)/project-[project]/databases/database-[database]/collection-[collection]/attributes/integer.svelte
@@ -32,8 +32,8 @@
             originalKey,
             data.required,
             data.default,
-            data.min === -9223372036854776000 ? undefined : data.min,
-            data.max === 9223372036854776000 ? undefined : data.max,
+            Math.abs(data.min) > Number.MAX_SAFE_INTEGER ? undefined : data.min,
+            Math.abs(data.max) > Number.MAX_SAFE_INTEGER ? undefined : data.max,
             data.key !== originalKey ? data.key : undefined
         );
     }
