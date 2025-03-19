3b9f1e93f45c8329ee3bdf58e3fe742cca2e9ae3
Arman <arman@appwrite.io>
2025-03-19 16:04:35 +0100

    feat: add search to functions

--- src/routes/(console)/project-[project]/functions/+page.ts
+++ src/routes/(console)/project-[project]/functions/+page.ts
@@ -1,12 +1,12 @@
 import { Query } from '@appwrite.io/console';
 import { sdk } from '$lib/stores/sdk';
-import { getLimit, getPage, pageToOffset } from '$lib/helpers/load';
+import { getLimit, getPage, getSearch, pageToOffset } from '$lib/helpers/load';
 import { CARD_LIMIT, Dependencies } from '$lib/constants';
 
 export const load = async ({ url, depends, route, parent }) => {
     depends(Dependencies.FUNCTIONS);
     const { templatesList } = await parent();
-
+    const search = getSearch(url);
     const page = getPage(url);
     const limit = getLimit(url, route, CARD_LIMIT);
     const offset = pageToOffset(page, limit);
@@ -14,11 +14,11 @@ export const load = async ({ url, depends, route, parent }) => {
     return {
         offset,
         limit,
-        functions: await sdk.forProject.functions.list([
-            Query.limit(limit),
-            Query.offset(offset),
-            Query.orderDesc('')
-        ]),
+        functions: await sdk.forProject.functions.list(
+            [Query.limit(limit), Query.offset(offset), Query.orderDesc('')],
+            search
+        ),
+        search,
         templatesList
     };
 };
