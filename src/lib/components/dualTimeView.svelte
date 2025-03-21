01272d6a04e89f8dea60f320fe65768feea608d6
Darshan <itznotabug@gmail.com>
2025-03-21 16:31:57 +0530

    update: as per designs.

--- src/lib/components/dualTimeView.svelte
+++ src/lib/components/dualTimeView.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import { getUTCOffset, timeFromNow, toLocaleDateTime } from '$lib/helpers/date';
+    import { type ComponentProps } from 'svelte';
     import { capitalize } from '$lib/helpers/string';
+    import { getUTCOffset, timeFromNow, toLocaleDateTime } from '$lib/helpers/date';
     import { Badge, InteractiveText, Layout, Popover, Typography } from '@appwrite.io/pink-svelte';
-    import type { ComponentProps } from 'svelte';
 
     export let time: string = '';
     export let placement: ComponentProps<Popover>['placement'] = 'bottom-end';
@@ -36,36 +36,104 @@
         return formattedTime ? `${formattedTime} ago` : 'Just now';
     }
 
+    let tooltipWrapperId = 'tooltip-wrapper-' + Math.random().toString(36).substring(2, 9);
+
     $: timeToString = time ? timeDifference(time) : 'Invalid time';
+
+    function shouldHidePopover(hideTooltip: () => void) {
+        let isMouseOverTooltip = false;
+        const tooltip = document.querySelector(`.${tooltipWrapperId}`);
+
+        if (!tooltip) {
+            hideTooltip();
+            return;
+        }
+
+        if (tooltip) {
+            tooltip.addEventListener('mouseenter', () => {
+                isMouseOverTooltip = true;
+            });
+
+            tooltip.addEventListener('mouseleave', () => {
+                isMouseOverTooltip = false;
+                hideTooltip();
+            });
+        }
+
+        setTimeout(() => {
+            if (!isMouseOverTooltip) {
+                hideTooltip();
+            }
+        }, 50);
+    }
 </script>
 
-<Popover let:toggle {placement} portal>
-    <button on:mouseenter={(e) => toggle(e)}>
-        <slot>{capitalize(timeFromNow(time))}</slot>
-    </button>
+<div class="dual-time-stamp-wrapper">
+    <Popover let:showTooltip let:hideTooltip {placement} portal>
+        <button
+            on:mouseenter={() => setTimeout(showTooltip, 25)}
+            on:mouseleave={() => shouldHidePopover(hideTooltip)}>
+            <slot>{capitalize(timeFromNow(time))}</slot>
+        </button>
 
-    <Layout.Stack gap="m" alignContent="flex-start" slot="tooltip">
-        <Typography.Text color="--fgcolor-neutral-tertiary" variant="m-400"
-            >{timeToString}</Typography.Text>
+        <Layout.Stack gap="s" alignContent="flex-start" slot="tooltip" class={tooltipWrapperId}>
+            <!-- `Raw time` as per design -->
+            <Typography.Caption color="--fgcolor-neutral-tertiary" variant="400">
+                {timeToString}
+            </Typography.Caption>
 
-        <Layout.Stack gap="xxs">
-            <Layout.Stack direction="row" justifyContent="space-between">
-                <Typography.Text variant="m-400">
-                    <InteractiveText
-                        variant="copy"
-                        text={toLocaleDateTime(time, 'UTC')}
-                        isVisible />
-                </Typography.Text>
-                <Badge variant="secondary" content="UTC" size="xs" />
-            </Layout.Stack>
+            <!-- `Absolute time` as per design -->
+            <Layout.Stack gap="xxs">
+                <!--
+                    PR for custom copy [`copyText`]:
+                    https://github.com/appwrite/pink/pull/305
+                -->
+                <Layout.Stack direction="row" justifyContent="space-between">
+                    <div class="body-400">
+                        <InteractiveText
+                            isVisible
+                            variant="copy"
+                            text={toLocaleDateTime(time, 'UTC')}
+                            copyText={new Date(time).toISOString()} />
+                    </div>
 
-            <Layout.Stack direction="row" justifyContent="space-between">
-                <Typography.Text variant="m-400">
-                    <InteractiveText variant="copy" text={toLocaleDateTime(time)} isVisible />
-                </Typography.Text>
+                    <Badge variant="secondary" content="UTC" size="xs" />
+                </Layout.Stack>
 
-                <Badge variant="secondary" content="UTC{getUTCOffset()}" size="xs" />
+                <Layout.Stack direction="row" justifyContent="space-between">
+                    <div class="body-400">
+                        <InteractiveText
+                            isVisible
+                            variant="copy"
+                            text={toLocaleDateTime(time)}
+                            copyText={new Date(time).toLocaleTimeString()} />
+                    </div>
+
+                    <Badge variant="secondary" content="UTC{getUTCOffset()}" size="xs" />
+                </Layout.Stack>
             </Layout.Stack>
         </Layout.Stack>
-    </Layout.Stack>
-</Popover>
+    </Popover>
+</div>
+
+<style lang="scss">
+    :global(body [id^='popover-']:has([class^='tooltip-wrapper-'])) {
+        transition: all 0.25s;
+    }
+
+    .body-400 {
+        & :global(span) {
+            font-weight: 400;
+            line-height: 140%;
+            text-align: start;
+            letter-spacing: -0.063px;
+            font-size: var(--font-size-s) !important;
+            color: var(--fgcolor-neutral-secondary) !important;
+            font-family: var(--font-family-sansserif) !important;
+        }
+
+        & :global(.interactiveTextContainer) {
+            padding-inline-start: unset !important;
+        }
+    }
+</style>
