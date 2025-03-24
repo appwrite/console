8f85fc9e629b99d7d7f95168bdcf2351e1b8d0d2
Arman <arman@appwrite.io>
2025-03-24 17:50:58 +0100

    simplyfy createb by component

--- src/lib/components/git/deploymentCreatedBy.svelte
+++ src/lib/components/git/deploymentCreatedBy.svelte
@@ -1,20 +1,13 @@
 <script lang="ts">
     import Link from '$lib/elements/link.svelte';
-    import { capitalize } from '$lib/helpers/string';
     import type { Models } from '@appwrite.io/console';
-    import { timeFromNow } from '$lib/helpers/date';
     import DualTimeView from '$lib/components/dualTimeView.svelte';
 
     export let deployment: Models.Deployment;
 </script>
 
 <p>
-    <DualTimeView time={deployment.$updatedAt}>
-        {deployment.providerCommitAuthor
-            ? timeFromNow(deployment.$updatedAt)
-            : capitalize(timeFromNow(deployment.$updatedAt))}
-    </DualTimeView>
-
+    <DualTimeView time={deployment.$updatedAt} />
     {#if deployment.providerCommitAuthor}
         by
         <Link href={deployment.providerCommitAuthorUrl} external>
