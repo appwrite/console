b6615012a096b3576e9dadad4447af22e1cb5312
Darshan <itznotabug@gmail.com>
2025-03-31 17:13:42 +0530

    update: bigger target area.

--- src/lib/components/dualTimeView.svelte
+++ src/lib/components/dualTimeView.svelte
@@ -78,10 +78,8 @@
         let:hide
         slot="tooltip"
         role="tooltip"
-        style:padding-top="1rem"
-        style:margin-top="-1rem"
-        style:padding-bottom="1rem"
-        style:margin-bottom="-1rem"
+        style:padding="1rem"
+        style:margin="-1rem"
         on:mouseenter={() => (isMouseOverTooltip = true)}
         on:mouseleave={() => hidePopover(hide, false)}>
         <Layout.Stack gap="s" alignContent="flex-start">
