0f8d35aaca14b7e366baddf535f504428722933b
ernstmul <ernst.mulders@gmail.com>
2025-04-01 14:12:28 +0200

    Menu setuping

---
+++ src/routes/(studio)/+layout.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+    import { Avatar, Layout } from '@appwrite.io/pink-svelte';
+    import { AvatarInitials, Breadcrumbs } from '$lib/components';
+    import { page } from '$app/stores';
+    import { BillingPlan } from '$lib/constants';
+    import { isCloud } from '$lib/system';
+    import { tierToPlan } from '$lib/stores/billing';
+    import { organization, organizationList } from '$lib/stores/organization';
+    import { user } from '$lib/stores/user';
+    import SidebarOrganization from '$lib/components/studio/sidebarOrganization.svelte';
+
+    const hasProjectSidebar = $page.url.pathname.startsWith('/studio/proj');
+
+    $: loadedProjects = $page.data.projects.map((project) => {
+        return {
+            name: project?.name,
+            $id: project.$id,
+            isSelected: $page.data.currentProjectId === project.$id,
+            platformCount: project.platforms.length,
+            pingCount: project.pingCount
+        };
+    });
+
+    $: organizations = $organizationList.teams.map((org) => {
+        const billingPlan = org['billingPlan'];
+        return {
+            name: org.name,
+            $id: org.$id,
+            showUpgrade: billingPlan === BillingPlan.FREE,
+            tierName: isCloud ? tierToPlan(billingPlan).name : null,
+            isSelected: $organization?.$id === org.$id,
+            projects: loadedProjects
+        };
+    });
+</script>
+
+<main>
+    <Layout.Stack>
+        <header>
+            <Layout.Stack direction="row" justifyContent="space-between" alignItems="center">
+                <Breadcrumbs {organizations} />
+                <AvatarInitials name={$user.name} size="s" />
+            </Layout.Stack>
+        </header>
+        <div class="studio-content" class:project-sidebar={hasProjectSidebar}><slot /></div>
+        <SidebarOrganization organization={$page.data.currentOrganization} />
+    </Layout.Stack>
+</main>
+
+<style lang="scss">
+    main {
+        min-height: 100vh;
+        background-color: var(--bgcolor-neutral-default);
+    }
+
+    header {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        padding: var(--space-4);
+    }
+
+    .studio-content {
+        min-height: calc(100vh - 48px);
+        margin-top: 48px;
+        width: calc(100vw - 200px);
+        margin-left: 200px;
+        padding-right: var(--space-4);
+
+        &.hasProjectSidebar {
+            background-color: red;
+        }
+    }
+</style>
--- src/routes/(studio)/org-[organization]/+page.svelte
+++ src/routes/(studio)/org-[organization]/+page.svelte
@@ -1,1 +1,5 @@
-Hey studio!
+<script lang="ts">
+    import { Card } from '@appwrite.io/pink-svelte';
+</script>
+
+<Card.Base>dus</Card.Base>
