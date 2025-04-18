2b807b4569281e1042bcaf0bf0304caaad442572
Arman <arman@appwrite.io>
2025-04-18 17:07:06 +0200

    feat: reenable usage

--- src/routes/(console)/project-[project]/sites/site-[site]/deployments/+page.svelte
+++ src/routes/(console)/project-[project]/sites/site-[site]/deployments/+page.svelte
@@ -10,14 +10,13 @@
     import CreateGitDeploymentModal from './createGitDeploymentModal.svelte';
     import { columns } from './store';
     import CreateManualDeploymentModal from './createManualDeploymentModal.svelte';
-    // import DeploymentMetrics from './deploymentMetrics.svelte';
+    import DeploymentMetrics from './deploymentMetrics.svelte';
     import { IconPlus } from '@appwrite.io/pink-icons-svelte';
     import { onMount } from 'svelte';
     import { sdk } from '$lib/stores/sdk';
     import { invalidate } from '$app/navigation';
     import { Dependencies } from '$lib/constants';
     import CreateCliModal from './createCliModal.svelte';
-    import { ParsedTagList, QuickFilters } from '$lib/components/filters';
     import { page } from '$app/state';
     import { ConnectRepoModal } from '$lib/components/git';
 
@@ -74,55 +73,54 @@
 
 <Container>
     <Layout.Stack gap="xxxl">
-        <!-- TODO: re-enable metrics after backend changes -->
-        <!-- <DeploymentMetrics deploymentList={data.deploymentList} /> -->
+        <DeploymentMetrics deploymentList={data.deploymentList} />
 
         <!-- TODO: re-enable once component is complete -->
-        <!-- <ResponsiveContainerHeader
-            view={View.Table}
-            {columns}
-            hasFilters
-            analyticsSource="site_deployments"
-            hideView>
-            <Popover padding="none" let:toggle>
-                <Button size="s" on:click={toggle}>
-                    <Icon size="s" icon={IconPlus} />
-                    Create deployment
-                </Button>
-                <svelte:fragment slot="tooltip" let:toggle>
-                    <ActionMenu.Root>
-                        <ActionMenu.Item.Button
-                            badge="Recommended"
-                            on:click={(e) => {
-                                toggle(e);
-                                if (!hasInstallation) {
-                                    showConnectRepo = true;
-                                } else {
-                                    showCreateDeployment = true;
-                                }
-                            }}>
-                            Git
-                        </ActionMenu.Item.Button>
-                        <ActionMenu.Item.Button
-                            on:click={(e) => {
-                                toggle(e);
-                                showConnectCLI = true;
-                            }}>
-                            CLI
-                        </ActionMenu.Item.Button>
-                        <ActionMenu.Item.Button
-                            on:click={(e) => {
-                                toggle(e);
-                                showConnectManual = true;
-                            }}>
-                            Manual
-                        </ActionMenu.Item.Button>
-                    </ActionMenu.Root>
-                </svelte:fragment>
-            </Popover>
-        </ResponsiveContainerHeader> -->
         <Layout.Stack gap="l">
-            <Layout.Stack>
+            <ResponsiveContainerHeader
+                view={View.Table}
+                {columns}
+                hasFilters
+                analyticsSource="site_deployments"
+                hideView>
+                <Popover padding="none" let:toggle>
+                    <Button size="s" on:click={toggle}>
+                        <Icon size="s" icon={IconPlus} />
+                        Create deployment
+                    </Button>
+                    <svelte:fragment slot="tooltip" let:toggle>
+                        <ActionMenu.Root>
+                            <ActionMenu.Item.Button
+                                badge="Recommended"
+                                on:click={(e) => {
+                                    toggle(e);
+                                    if (!hasInstallation) {
+                                        showConnectRepo = true;
+                                    } else {
+                                        showCreateDeployment = true;
+                                    }
+                                }}>
+                                Git
+                            </ActionMenu.Item.Button>
+                            <ActionMenu.Item.Button
+                                on:click={(e) => {
+                                    toggle(e);
+                                    showConnectCLI = true;
+                                }}>
+                                CLI
+                            </ActionMenu.Item.Button>
+                            <ActionMenu.Item.Button
+                                on:click={(e) => {
+                                    toggle(e);
+                                    showConnectManual = true;
+                                }}>
+                                Manual
+                            </ActionMenu.Item.Button>
+                        </ActionMenu.Root>
+                    </svelte:fragment>
+                </Popover>
+            </ResponsiveContainerHeader>
+            <!-- <Layout.Stack>
                 <Layout.Stack justifyContent="space-between" direction="row">
                     <Layout.Stack alignItems="center" direction="row">
                         {#if data.deploymentList.total || data?.query}
@@ -173,7 +171,7 @@
                     </Layout.Stack>
                 </Layout.Stack>
                 <ParsedTagList />
-            </Layout.Stack>
+            </Layout.Stack> -->
 
             {#if data.deploymentList.total}
                 <Table {data} />
