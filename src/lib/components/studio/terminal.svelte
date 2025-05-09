79cded26b72360f44d38e12f358b267d6b5f5e79
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-05-09 14:35:59 +0200

    Reduce terminal width by additional 2 columns

--- src/lib/components/studio/terminal.svelte
+++ src/lib/components/studio/terminal.svelte
@@ -41,12 +41,12 @@
                 const { cols, rows } = fitAddon.proposeDimensions();
                 if (term.cols === cols && term.rows === rows) return;
                 if (!Number.isInteger(cols) || !Number.isInteger(rows)) return;
-                term.resize(cols - 1, rows);
+                term.resize(cols - 3, rows);
                 synapse.dispatch('terminal', {
                     operation: 'updateSize',
                     params: {
                         rows,
-                        cols: cols - 1
+                        cols: cols - 3
                     }
                 });
             });
