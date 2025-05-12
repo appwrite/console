69f81f562ff2160cbaea35b0aa703ecaf30f0dc5
ernstmul <ernst.mulders@gmail.com>
2025-05-12 10:26:43 +0200

    Fix select disable safari when sizing terminal

--- src/routes/(console)/project-[project]/studio/artifact-[artifact]/+layout.svelte
+++ src/routes/(console)/project-[project]/studio/artifact-[artifact]/+layout.svelte
@@ -20,6 +20,8 @@
     import { previewFrameRef } from '$routes/(console)/project-[project]/store';
     import type { Terminal as XTerm } from '@xterm/xterm';
     import {
+        disableBodySelect,
+        enabledBodySelect,
         getTerminalHeightFromPrefs,
         getTerminalOpenFromPrefs,
         saveTerminalHeightToPrefs,
@@ -99,7 +101,7 @@
         window.addEventListener('mouseup', stopResize);
         window.addEventListener('touchmove', resize);
         window.addEventListener('touchend', stopResize);
-        document.body.style.userSelect = 'none';
+        disableBodySelect();
         if ($previewFrameRef) {
             $previewFrameRef.style.pointerEvents = 'none';
         }
@@ -127,7 +129,7 @@
         window.removeEventListener('mouseup', stopResize);
         window.removeEventListener('touchmove', resize);
         window.removeEventListener('touchend', stopResize);
-        document.body.style.userSelect = '';
+        enabledBodySelect();
         if ($previewFrameRef) {
             $previewFrameRef.style.pointerEvents = '';
         }
