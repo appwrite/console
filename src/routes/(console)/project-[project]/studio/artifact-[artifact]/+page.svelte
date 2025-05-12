f70f2f581349fa62acf214a49beebed8d07713e8
ernstmul <ernst.mulders@gmail.com>
2025-05-12 09:59:00 +0200

    Fix add spacing for iframe

--- src/routes/(console)/project-[project]/studio/artifact-[artifact]/+page.svelte
+++ src/routes/(console)/project-[project]/studio/artifact-[artifact]/+page.svelte
@@ -5,7 +5,7 @@
         IconExternalLink,
         IconRefresh
     } from '@appwrite.io/pink-icons-svelte';
-    import { Layout, Icon } from '@appwrite.io/pink-svelte';
+    import { Layout, Icon, Divider } from '@appwrite.io/pink-svelte';
     import { previewFrameRef } from '$routes/(console)/project-[project]/store';
 
     const previewUrl = 'https://www.aloyoga.com/';
@@ -14,13 +14,16 @@
     $: previewFrameRef.set(iframeRef);
 </script>
 
-<Layout.Stack direction="column">
+<Layout.Stack direction="column" gap="s">
     <Layout.Stack direction="row" alignItems="center">
         <Icon icon={IconRefresh} color="--fgcolor-neutral-tertiary" />
         <InputText id="previewUrl" value={previewUrl} />
         <Icon icon={IconDeviceMobile} color="--fgcolor-neutral-tertiary" />
         <Icon icon={IconExternalLink} color="--fgcolor-neutral-tertiary" />
     </Layout.Stack>
+    <div class="divider-wrapper">
+        <Divider />
+    </div>
 </Layout.Stack>
 <div class="iframe-container">
     <iframe src={previewUrl} bind:this={iframeRef} id="preview-iframe" title="preview"></iframe>
@@ -40,5 +43,10 @@
         width: calc(100% + 2 * var(--space-7));
         height: 100%;
         margin-inline-start: calc(-1 * var(--space-7));
+    }
+
+    .divider-wrapper {
+        margin-inline-start: calc(-1 * var(--space-7));
+        width: calc(100% + var(--space-10));
     }
 </style>
