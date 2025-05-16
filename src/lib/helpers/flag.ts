043dc543f9adf52cfe2ca7ee145d06c597888447
Arman <arman@appwrite.io>
2025-05-16 10:58:55 +0200

    fix: get flags method

--- src/lib/helpers/flag.ts
+++ src/lib/helpers/flag.ts
@@ -1,12 +1,8 @@
 import { isValueOfStringEnum } from '$lib/helpers/types';
 import { Flag } from '@appwrite.io/console';
 import { sdk } from '$lib/stores/sdk';
-import { page } from '$app/state';
 
 export function getFlagUrl(countryCode: string) {
     if (!isValueOfStringEnum(Flag, countryCode)) return '';
-    return sdk
-        .forProject(page.params.region, 'console')
-        .avatars.getFlag(countryCode, 22, 15, 100)
-        ?.toString();
+    return sdk.forConsole.avatars.getFlag(countryCode, 22, 15, 100)?.toString();
 }
