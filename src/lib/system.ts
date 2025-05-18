18cc9f84bca582c6ac2e8a68db23c29f667fe34c
Darshan <itznotabug@gmail.com>
2025-05-18 15:01:45 +0530

    update: show off time!

--- src/lib/system.ts
+++ src/lib/system.ts
@@ -28,4 +28,6 @@ export const isDev = ENV.DEV;
 export const isProd = ENV.PROD;
 export const hasStripePublicKey = !!VARS.PUBLIC_STRIPE_KEY;
 export const GRACE_PERIOD_OVERRIDE = false;
-export const SHOW_INIT_FEATURES = !ENV.PROD || env.PUBLIC_APPWRITE_INIT_FEATURES === 'true';
+
+// start showing 1.7.x feats!
+export const SHOW_INIT_FEATURES = true;
