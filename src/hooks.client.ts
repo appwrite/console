60ca8eccc9f057aa02083e62476217e33c222ef2
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-05-19 00:03:24 +0200

    Enable Sentry again and future-proof nginx caching configuration

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,15 +1,15 @@
-// import * as Sentry from '@sentry/sveltekit';
-// import { isCloud, isProd } from '$lib/system';
+import * as Sentry from '@sentry/sveltekit';
+import { isCloud, isProd } from '$lib/system';
 import { AppwriteException } from '@appwrite.io/console';
 import type { HandleClientError } from '@sveltejs/kit';
 
-// Sentry.init({
-//     enabled: isCloud && isProd,
-//     dsn: 'https://c7ce178bdedd486480317b72f282fd39@o1063647.ingest.us.sentry.io/4504158071422976',
-//     tracesSampleRate: 1,
-//     replaysSessionSampleRate: 0,
-//     replaysOnErrorSampleRate: 0
-// });
+Sentry.init({
+    enabled: isCloud && isProd,
+    dsn: 'https://c7ce178bdedd486480317b72f282fd39@o1063647.ingest.us.sentry.io/4504158071422976',
+    tracesSampleRate: 1,
+    replaysSessionSampleRate: 0,
+    replaysOnErrorSampleRate: 0
+});
 
 export const handleError: HandleClientError = ({ error, message, status }) => {
     console.error(error);
