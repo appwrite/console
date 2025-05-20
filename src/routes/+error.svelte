41e8da0c56db4cb293d4265546250482295bbf14
ernstmul <ernst.mulders@gmail.com>
2025-05-20 11:17:49 +0200

    Update error button CTA

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,6 +4,7 @@
     import { Button } from '$lib/elements/forms';
     import { Unauthenticated } from '$lib/layout';
     import { Typography } from '@appwrite.io/pink-svelte';
+    import { isStudio } from '$lib/system.js';
 </script>
 
 <Unauthenticated>
@@ -12,5 +13,5 @@
             ? page.error.status || 'Invalid Argument'
             : 'Invalid Argument'}</Typography.Title>
     <Typography.Title>{page.error.message}</Typography.Title>
-    <Button href={base}>Back to the console</Button>
+    <Button href={base}>Back to the {isStudio ? 'studio' : 'console'}</Button>
 </Unauthenticated>
