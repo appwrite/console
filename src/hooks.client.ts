d890c4bb78c87974a1d9b4618f3e78a8a0e6d405
ernstmul <ernst.mulders@gmail.com>
2025-05-20 14:39:36 +0200

    Adjust loader

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -27,6 +27,24 @@ function setFavicon() {
     document.head.appendChild(maskLink);
 }
 
+function setLoader() {
+    if (document.getElementsByTagName('body').length > 0) {
+        const color = isStudio
+            ? themeInUse === 'light'
+                ? 'rgba(86, 86, 92, 100%)'
+                : 'rgba(195, 195, 198, 100%)'
+            : 'rgb(219, 26, 90)';
+        document
+            .getElementsByTagName('body')[0]
+            .setAttribute('style', '--console-loading-color: ' + color);
+    }
+
+    document.getElementById('loader-logo').innerHTML = isStudio
+        ? '<img src="/console/images/imagine-logo-light.svg" class="logo-light" width="96" height="24" alt="Imagine Logo"/><img src="/console/images/imagine-logo-dark.svg" class="logo-dark" width="96" height="24" alt="Imagine Logo"/>'
+        : '<img src="/console/images/appwrite-logo-light.svg" width="120" height="22" class="logo-light" alt="Appwrite Logo" /> <img src="/console/images/appwrite-logo-dark.svg" width="120" height="22" class="logo-dark" alt="Appwrite Logo" />';
+}
+
+setLoader();
 setFavicon();
 
 export const handleError: HandleClientError = Sentry.handleErrorWithSentry(
