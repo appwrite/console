b18aa1ccb55e672918866b72b1000bdf8f720ea6
Darshan <itznotabug@gmail.com>
2025-05-22 17:15:10 +0530

    lint.

--- src/lib/helpers/tlds.ts
+++ src/lib/helpers/tlds.ts
@@ -3,7 +3,7 @@ import { parse } from 'tldts';
 /**
  * Returns the apex/root domain from a full domain string.
  */
-export function getApexDomain(domain: string): string|null {
+export function getApexDomain(domain: string): string | null {
     return parse(domain).domain;
 }
 
