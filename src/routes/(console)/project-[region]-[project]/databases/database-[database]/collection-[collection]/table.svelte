352d7d8c763cf1ecba69f83ca560aebfb0c60f28
Darshan <itznotabug@gmail.com>
2025-05-28 19:33:51 +0530

    revert: last change.

--- src/routes/(console)/project-[region]-[project]/databases/database-[database]/collection-[collection]/table.svelte
+++ src/routes/(console)/project-[region]-[project]/databases/database-[database]/collection-[collection]/table.svelte
@@ -199,7 +199,7 @@
                 {@const attr = $attributes.find((n) => n.key === id)}
                 <Table.Cell column={id} {root}>
                     {#if isRelationship(attr)}
-                        {@const args = displayNames?.[attr.relatedCollection] ?? ['name']}
+                        {@const args = displayNames?.[attr.relatedCollection] ?? ['$id']}
                         {#if !isRelationshipToMany(attr)}
                             {#if document[id]}
                                 {@const related = document[id]}
