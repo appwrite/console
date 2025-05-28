bd0dc7b894939b4993afad9e74cba674d4c0e9f5
Darshan <itznotabug@gmail.com>
2025-05-28 19:29:02 +0530

    address design comments.

--- src/routes/(console)/organization-[organization]/domains/domain-[domain]/domainMetrics.svelte
+++ src/routes/(console)/organization-[organization]/domains/domain-[domain]/domainMetrics.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     import { UsageCard } from '$lib/components';
-    import { Button } from '$lib/elements/forms';
     import type { Domain } from '$lib/sdk/domains';
     import { toLocaleDate } from '$lib/helpers/date';
-    import { IconRefresh } from '@appwrite.io/pink-icons-svelte';
-    import { Icon, Layout, Status, Tooltip } from '@appwrite.io/pink-svelte';
+    import { Layout, Status } from '@appwrite.io/pink-svelte';
+    import { Link } from '$lib/elements';
 
     let { domain, retryVerification }: { domain: Domain; retryVerification: () => void } = $props();
 
@@ -43,21 +42,13 @@
         {#each metrics.slice(0, 3) as metric}
             {#if metric.description === 'Status'}
                 <UsageCard description={metric.description}>
-                    <Layout.Stack direction="row" gap="xxs" alignItems="center">
+                    <Layout.Stack direction="row" gap="xs" alignItems="center">
                         <Status
                             label={metric.value.toString()}
                             status={isDomainVerified ? 'complete' : 'pending'} />
 
                         {#if !isDomainVerified}
-                            <Tooltip>
-                                <Button text icon on:click={retryVerification}>
-                                    <Icon
-                                        size="s"
-                                        icon={IconRefresh}
-                                        color="--fgcolor-neutral-secondary" />
-                                </Button>
-                                <span slot="tooltip">Verify domain</span>
-                            </Tooltip>
+                            <Link on:click={retryVerification}>Retry</Link>
                         {/if}
                     </Layout.Stack>
                 </UsageCard>
