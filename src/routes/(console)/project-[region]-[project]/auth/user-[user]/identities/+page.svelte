76a78bbced42a11998e814cd8bfd39f9bdb98489
Steven Nguyen <stnguyen90@users.noreply.github.com>
2025-05-28 13:15:31 -0700

    fix: remove identities table cell widths
    The set widths were causing issues with the table as it wasn't filling
    the entire space and looked bad. We'll need to figure out how to make
    the widths look better later.

--- src/routes/(console)/project-[region]-[project]/auth/user-[user]/identities/+page.svelte
+++ src/routes/(console)/project-[region]-[project]/auth/user-[user]/identities/+page.svelte
@@ -13,18 +13,17 @@
     export let data;
 
     const columns = writable<Column[]>([
-        { id: '$id', title: 'Identity ID', type: 'string', width: 200 },
-        { id: 'provider', title: 'Provider', type: 'string', width: 80 },
-        { id: 'providerUid', title: 'Provider UID', type: 'string', hide: true, width: 80 },
-        { id: 'providerEmail', title: 'Email', type: 'string', width: 80 },
+        { id: '$id', title: 'Identity ID', type: 'string' },
+        { id: 'provider', title: 'Provider', type: 'string' },
+        { id: 'providerUid', title: 'Provider UID', type: 'string', hide: true },
+        { id: 'providerEmail', title: 'Email', type: 'string' },
         {
             id: 'providerAccessTokenExpiry',
             title: 'Expiry Date',
-            type: 'datetime',
-            width: 60
+            type: 'datetime'
         },
-        { id: '$createdAt', title: 'Created', type: 'datetime', width: 80 },
-        { id: '$updatedAt', title: 'Updated', type: 'datetime', hide: true, width: 80 }
+        { id: '$createdAt', title: 'Created', type: 'datetime' },
+        { id: '$updatedAt', title: 'Updated', type: 'datetime', hide: true }
     ]);
 </script>
 
