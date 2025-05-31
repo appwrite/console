9225fd650a9381f149c4e31a951a67840b56bee3
Eldad Fux <eldad.fux@gmail.com>
2025-05-31 19:27:12 +0100

    Updated description, fixed empty chat bug

--- src/lib/components/studio/chat/chat.svelte
+++ src/lib/components/studio/chat/chat.svelte
@@ -174,7 +174,7 @@
                         <Button.Button type="button" icon variant="secondary" size="s">
                             <Icon icon={IconPaperClip} color="--fgcolor-neutral-tertiary" />
                         </Button.Button>
-                        <Button.Button icon variant="secondary" size="s" type="submit">
+                        <Button.Button icon variant="secondary" size="s" type="submit" disabled={!studio.streaming && !message.trim()}>
                             {#if studio.streaming}
                                 <Icon icon={IconStop} color="--fgcolor-neutral-tertiary" />
                             {:else}
