787cd527801f594e83ef2a8c8fbaf36465f7bcf5
Darshan <itznotabug@gmail.com>
2025-06-02 16:58:56 +0530

    update: compute things later.

--- src/routes/(console)/organization-[organization]/+page.ts
+++ src/routes/(console)/organization-[organization]/+page.ts
@@ -8,12 +8,14 @@ import { redirect } from '@sveltejs/kit';
 export const load: PageLoad = async ({ params, url, route, depends, parent }) => {
     const { scopes } = await parent();
     depends(Dependencies.ORGANIZATION);
-    const page = getPage(url);
-    const limit = getLimit(url, route, CARD_LIMIT);
-    const offset = pageToOffset(page, limit);
+
     if (!scopes.includes('projects.read') && scopes.includes('billing.read')) {
         return redirect(301, `/console/organization-${params.organization}/billing`);
     }
+
+    const page = getPage(url);
+    const limit = getLimit(url, route, CARD_LIMIT);
+    const offset = pageToOffset(page, limit);
 
     const projects = await sdk.forConsole.projects.list([
         Query.offset(offset),
