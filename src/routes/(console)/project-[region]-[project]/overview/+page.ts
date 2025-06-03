ffb207383af968ee48e45cb592b77ec967e698c7
Darshan <itznotabug@gmail.com>
2025-06-03 10:59:24 +0530

    address comments;

--- src/routes/(console)/project-[region]-[project]/overview/+page.ts
+++ src/routes/(console)/project-[region]-[project]/overview/+page.ts
@@ -5,8 +5,18 @@ import type { PageLoad } from './$types';
 import { selectedTab } from './store';
 
 export const load: PageLoad = async ({ params }) => {
+    let subroute: string;
     const tab = get(selectedTab);
-    const subroute = ['keys', 'dev-keys'].includes(tab) ? tab : 'platforms';
+
+    switch (tab) {
+        case 'keys':
+        case 'dev-keys':
+            subroute = tab;
+            break;
+        default:
+            subroute = 'platforms';
+            break;
+    }
 
     redirect(302, `${base}/project-${params.region}-${params.project}/overview/${subroute}`);
 };
