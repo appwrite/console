0e5c14e8802480e44dabb4d94630e2858bd8e4f5
Matej Bačo <matejbaco2000@gmail.com>
2025-06-04 10:54:40 +0200

    Document next flags iteration

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -3,8 +3,17 @@ import { env } from '$env/dynamic/public';
 export const PUBLIC_CONSOLE_FEATURE_FLAGS = env.PUBLIC_CONSOLE_FEATURE_FLAGS ?? '';
 
 function setupFlag(name: string, _description: string) {
-    // TODO: Use flags library that provides visual component
-    return PUBLIC_CONSOLE_FEATURE_FLAGS.includes(name);
+    // TODO: Use flags library that provides visual component during development
+
+    if (PUBLIC_CONSOLE_FEATURE_FLAGS.includes(name)) {
+        return true;
+    }
+
+    // TODO: Check team prefs
+
+    // TODO: Check user prefs
+
+    return false;
 }
 
 export const flags = {
