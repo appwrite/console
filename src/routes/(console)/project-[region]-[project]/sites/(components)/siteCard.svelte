82e3394175c8abc71b225751987a87dd21d87bac
Arman <arman@appwrite.io>
2025-06-05 15:30:15 +0200

    fix: site image previews

--- src/routes/(console)/project-[region]-[project]/sites/(components)/siteCard.svelte
+++ src/routes/(console)/project-[region]-[project]/sites/(components)/siteCard.svelte
@@ -20,8 +20,9 @@
     import { app } from '$lib/stores/app';
     import { base } from '$app/paths';
     import { isCloud } from '$lib/system';
-    import { getApiEndpoint } from '$lib/stores/sdk';
+    import { getApiEndpoint, sdk } from '$lib/stores/sdk';
     import { capitalize } from '$lib/helpers/string';
+    import { page } from '$app/state';
 
     export let deployment: Models.Deployment;
     export let proxyRuleList: Models.ProxyRuleList;
@@ -46,7 +47,7 @@
 
     function getFilePreview(fileId: string) {
         // TODO: @Meldiron use sdk.forConsole.storage.getFilePreview
-        const endpoint = getApiEndpoint();
+        const endpoint = getApiEndpoint(page.params.region);
         return endpoint + `/storage/buckets/screenshots/files/${fileId}/view?project=console`;
     }
 </script>
