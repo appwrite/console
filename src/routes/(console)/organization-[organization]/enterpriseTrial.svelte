f65942b26690813817dae37e1ce80a570b85ab90
Darshan <itznotabug@gmail.com>
2025-06-08 10:53:06 +0530

    fix: nav bar hidden under alert.

--- src/routes/(console)/organization-[organization]/enterpriseTrial.svelte
+++ src/routes/(console)/organization-[organization]/enterpriseTrial.svelte
@@ -4,46 +4,14 @@
     import { organization } from '$lib/stores/organization';
     import { calculateEnterpriseTrial, hideBillingHeaderRoutes } from '$lib/stores/billing';
     import { base } from '$app/paths';
-
-    import { onDestroy, onMount } from 'svelte';
     import { isTabletViewport } from '$lib/stores/viewport';
     import GradientBanner from '$lib/components/billing/gradientBanner.svelte';
     import { Layout, Typography } from '@appwrite.io/pink-svelte';
-
-    let container: HTMLElement | null = null;
-
-    function setNavigationHeight() {
-        const alertHeight = container ? container.getBoundingClientRect().height : 0;
-        const header: HTMLHeadingElement = document.querySelector('main > header');
-        const sidebar: HTMLElement = document.querySelector('main > div > nav');
-        const contentSection: HTMLElement = document.querySelector('main > div > section');
-
-        if (header) {
-            header.style.top = `${alertHeight}px`;
-        }
-        if (sidebar) {
-            sidebar.style.top = `${alertHeight + ($isTabletViewport ? 0 : header.getBoundingClientRect().height)}px`;
-            sidebar.style.height = `calc(100vh - (${alertHeight + ($isTabletViewport ? 0 : header.getBoundingClientRect().height)}px))`;
-        }
-        if (contentSection) {
-            contentSection.style.paddingBlockStart = `${alertHeight}px`;
-        }
-    }
-
-    onMount(() => {
-        setNavigationHeight();
-    });
-
-    onDestroy(() => {
-        container = null;
-        setNavigationHeight();
-    });
 
     $: upgradeUrl = `${base}/organization-${$organization?.$id}/change-plan`;
     $: remainingDays = calculateEnterpriseTrial($organization);
 </script>
 
-<svelte:window on:resize={setNavigationHeight} />
 {#if $organization?.$id && remainingDays > 0 && !hideBillingHeaderRoutes.includes(page.url.pathname)}
     <GradientBanner>
         <Layout.Stack
