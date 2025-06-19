3799fae41fd52f81acf772cb76014dbbf8c06773
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-06-19 17:51:41 +0200

    Update Column type references to PinkColumn

--- src/lib/helpers/types.ts
+++ src/lib/helpers/types.ts
@@ -60,8 +60,8 @@ export type TableRootProp = {
     selectedAll: boolean;
     selectedNone: boolean;
     selectedSome: boolean;
-    columns: Array<Column> | number;
-    columnsMap: Record<Column['id'], Column>;
+    columns: Array<PinkColumn> | number;
+    columnsMap: Record<PinkColumn['id'], PinkColumn>;
     toggle: (id: string) => void;
     toggleAll: () => void;
     addAvailableId: (id: string) => void;
