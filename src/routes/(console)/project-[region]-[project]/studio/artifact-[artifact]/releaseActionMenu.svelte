fb6451d1b66b8ca3efdc370a67150b32440f2af1
Jesse Winton <jesse@appwrite.io>
2025-07-17 16:10:38 -0400

    dynamic heights

--- src/routes/(console)/project-[region]-[project]/studio/artifact-[artifact]/releaseActionMenu.svelte
+++ src/routes/(console)/project-[region]-[project]/studio/artifact-[artifact]/releaseActionMenu.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { measure } from '$lib/helpers/measure.svelte';
     import {
         Button,
         Popover,
@@ -23,29 +22,26 @@
         return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     });
 
-    const getHeight = () => {
-        switch (publishing) {
-            case true:
-                height = 46;
-                break;
-            case false:
-                height = 130;
-                break;
-            default:
-                return 130; // Default height
-        }
-    };
+    type Dimension = number | null;
+
+    const dimensions = $state<{
+        height: Dimension;
+        width: Dimension;
+    }>({
+        width: null,
+        height: null
+    });
 
     const handlePublish = () => {
         publishing = true;
-        getHeight();
 
         setTimeout(() => {
             published = true;
             publishing = false;
-            getHeight();
         }, 4000);
     };
+
+    $inspect(dimensions);
 </script>
 
 <Popover let:toggle padding="none" placement="bottom-end">
@@ -58,50 +54,53 @@
         >Release
     </Button.Button>
     <svelte:fragment slot="tooltip">
-        <div class="popover" style:width="284px" style:--height={`${height}px`}>
-            {#if !publishing}
-                <Layout.Stack direction="column" gap="none">
+        <div class="popover" style:width="284px" style:--height={`${dimensions.height}px`}>
+            <div bind:clientHeight={dimensions.height}>
+                {#if !publishing}
+                    <Layout.Stack direction="column" gap="none">
+                        <div class="popover-section">
+                            <Layout.Stack direction="column" gap="s">
+                                <Link.Anchor variant="quiet" href="/" icon
+                                    >s-1309123843s9399...imagine.dev</Link.Anchor>
+                                <Status
+                                    label={published
+                                        ? `Released ${daysSinceRelease} days ago`
+                                        : 'Not published'}
+                                    status={published ? 'complete' : 'waiting'} />
+                            </Layout.Stack>
+                        </div>
+
+                        <Divider />
+                        <Layout.Stack
+                            direction="row"
+                            class="popover-section"
+                            gap="s"
+                            justifyContent="flex-end">
+                            <Button.Button
+                                size="s"
+                                variant="secondary"
+                                onclick={() => alert('connect clicked')}
+                                >Connect domain</Button.Button>
+                            <Button.Button size="s" onclick={handlePublish}>Deploy</Button.Button>
+                        </Layout.Stack>
+                    </Layout.Stack>
+                {:else}
                     <div class="popover-section">
-                        <Layout.Stack direction="column" gap="s">
-                            <Link.Anchor variant="quiet" href="/" icon
-                                >s-1309123843s9399...imagine.dev</Link.Anchor>
-                            <Status
-                                label={published
-                                    ? `Released ${daysSinceRelease} days ago`
-                                    : 'Not published'}
-                                status={published ? 'complete' : 'waiting'} />
+                        <Layout.Stack
+                            direction="row"
+                            gap="m"
+                            justifyContent="space-between"
+                            alignItems="center">
+                            <Typography.Text
+                                class="status-text"
+                                variant="m-500"
+                                color="var(--fgcolor-neutral-secondary)">
+                                Releasing...</Typography.Text>
+                            <Spinner size="s" />
                         </Layout.Stack>
                     </div>
-
-                    <Divider />
-                    <Layout.Stack
-                        direction="row"
-                        class="popover-section"
-                        gap="s"
-                        justifyContent="flex-end">
-                        <Button.Button
-                            size="s"
-                            variant="secondary"
-                            onclick={() => alert('connect clicked')}>Connect domain</Button.Button>
-                        <Button.Button size="s" onclick={handlePublish}>Deploy</Button.Button>
-                    </Layout.Stack>
-                </Layout.Stack>
-            {:else}
-                <div class="popover-section">
-                    <Layout.Stack
-                        direction="row"
-                        gap="m"
-                        justifyContent="space-between"
-                        alignItems="center">
-                        <Typography.Text
-                            class="status-text"
-                            variant="m-500"
-                            color="var(--fgcolor-neutral-secondary)">
-                            Releasing...</Typography.Text>
-                        <Spinner size="s" />
-                    </Layout.Stack>
-                </div>
-            {/if}
+                {/if}
+            </div>
         </div>
     </svelte:fragment>
 </Popover>
