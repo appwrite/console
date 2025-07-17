c72c3aec59b4f8c4e022bd4c70c166b0aae9456d
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-07-17 11:08:36 -0500

    unused hardcoded artifact id

--- ai-service/src/lib/constants.ts
+++ ai-service/src/lib/constants.ts
@@ -1,2 +1,1 @@
-export const OVERRIDE_BASE_DIR = `./tmp/workspace/artifact/${process.env.ARTIFACT_ID}`;
 export const ANTHROPIC_MAX_CACHE_CONTROL_BLOCKS = 4; // Anthropic only allows 4 cache control blocks
