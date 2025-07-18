bc6099d67afc0bc287ed8b43a9d94f91d9915a36
Jesse Winton <jesse@appwrite.io>
2025-07-18 13:49:53 -0400

    Update messageActions.svelte

--- src/lib/components/studio/chat/messageActions.svelte
+++ src/lib/components/studio/chat/messageActions.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
     import type { ActionsContainer } from './parser';
-    import { Card, Layout, ShimmerText, Spinner, Typography, Icon } from '@appwrite.io/pink-svelte';
+    import {
+        Card,
+        Layout,
+        ShimmerText,
+        Spinner,
+        Typography,
+        Icon,
+        Button
+    } from '@appwrite.io/pink-svelte';
     import { queue } from './queue.svelte';
     import { IconCheckCircle } from '@appwrite.io/pink-icons-svelte';
+    import { fade } from 'svelte/transition';
+    import { measure } from '$lib/helpers/measure.svelte';
 
     type Props = {
         message: ActionsContainer;
     };
 
     const { message }: Props = $props();
+
+    const { dimensions } = measure();
+    $inspect({ dimensions });
+
+    const firstActions = $derived(message.actions.slice(0, 3));
+    const remainingActions = $derived(message.actions.slice(0, -3));
 </script>
 
-<Card.Base variant="primary" padding="none">
+<Card.Base variant="primary" padding="none" class="message-action">
     <Card.Base variant="secondary" padding="xs" class="title">
         <Layout.Stack direction="row" alignItems="center" justifyContent="space-between">
             <Typography.Text variant="m-500">Version 0</Typography.Text>
@@ -20,68 +36,74 @@
             {/if}
         </Layout.Stack>
     </Card.Base>
-    <div class="actions">
-        <div class="grid-line"></div>
-        {#each message.actions as action}
-            {@const actionInQueue = queue.lists[action.group]?.find((n) => n.data.id === action.id)}
-            {#if actionInQueue}
-                <Layout.Stack direction="row" alignItems="center" justifyContent="space-between">
-                    {#if action.type === 'file'}
-                        <Typography.Code size="s" class="file">
-                            <span class="icon">
-                                {#if actionInQueue.status === 'done'}
-                                    <Icon size="s" --icon-size-s="12px" icon={IconCheckCircle} />
-                                {:else}
-                                    <Spinner size="s" --icon-size-s="12px" />
+    <div class="wrap" style:height={`${dimensions.height}px`}>
+        <div bind:clientHeight={dimensions.height}>
+            <Layout.Stack gap="m" class="actions" direction="column">
+                <div class="grid-line"></div>
+                {#each firstActions as action}
+                    {@const actionInQueue = queue.lists[action.group]?.find(
+                        (n) => n.data.id === action.id
+                    )}
+                    {#if actionInQueue}
+                        <div in:fade={{ duration: 200 }}>
+                            <Layout.Stack
+                                direction="row"
+                                alignItems="center"
+                                justifyContent="space-between">
+                                {#if action.type === 'file'}
+                                    <Typography.Code size="s" class="file">
+                                        <span class="icon">
+                                            {#if actionInQueue.status === 'done'}
+                                                <Icon
+                                                    size="s"
+                                                    --icon-size-s="12px"
+                                                    icon={IconCheckCircle} />
+                                            {:else}
+                                                <Spinner size="s" --icon-size-s="12px" />
+                                            {/if}
+                                        </span>
+                                        <span class="filename">
+                                            {action.src}</span
+                                        ></Typography.Code>
+                                {:else if action.type === 'shell'}
+                                    <Typography.Code class="file" size="s">
+                                        <span class="icon">
+                                            {#if actionInQueue.status === 'done'}
+                                                <Icon
+                                                    size="s"
+                                                    --icon-size-s="12px"
+                                                    icon={IconCheckCircle} />
+                                            {:else}
+                                                <Spinner size="s" --icon-size-s="12px" />
+                                            {/if}
+                                        </span>
+                                        <span class="filename">{action.content}</span
+                                        ></Typography.Code>
                                 {/if}
-                            </span>
-                            {action.src}</Typography.Code>
-                        <Typography.Code size="s">
-                            {#if actionInQueue.status === 'waiting'}
-                                Waiting
-                            {:else if actionInQueue.status === 'processing'}
-                                <ShimmerText>Generating</ShimmerText>
-                            {:else if actionInQueue.status === 'done'}
-                                Generated
-                            {:else if actionInQueue.status === 'failed'}
-                                <span style:color="var(--fgcolor-error)">Failed</span>
-                            {/if}
-                        </Typography.Code>
-                    {:else if action.type === 'shell'}
-                        <Typography.Code class="file" size="s">
-                            <span class="icon">
-                                {#if actionInQueue.status === 'done'}
-                                    <Icon size="s" --icon-size-s="12px" icon={IconCheckCircle} />
-                                {:else}
-                                    <Spinner size="s" --icon-size-s="12px" />
-                                {/if}
-                            </span>
-                            {action.content}</Typography.Code>
-                        <Typography.Code size="s">
-                            {#if actionInQueue.status === 'waiting'}
-                                Waiting
-                            {:else if actionInQueue.status === 'processing'}
-                                <ShimmerText>Running</ShimmerText>
-                            {:else if actionInQueue.status === 'done'}
-                                Completed
-                            {:else if actionInQueue.status === 'failed'}
-                                <span style:color="var(--fgcolor-error)">Failed</span>
-                            {/if}
-                        </Typography.Code>
+                            </Layout.Stack>
+                        </div>
                     {/if}
-                </Layout.Stack>
-            {/if}
-        {/each}
-        {#if !message.complete}
-            <Typography.Code size="s">
-                <ShimmerText>thinking...</ShimmerText>
-            </Typography.Code>
-        {/if}
+                {/each}
+                {#if !message.complete}
+                    <Typography.Code size="s">
+                        <ShimmerText>thinking...</ShimmerText>
+                    </Typography.Code>
+                {/if}
+            </Layout.Stack>
+        </div>
     </div>
+    {#if firstActions.length >= 3}
+        <Button.Button variant="secondary" size="xs" class="show-more">Show more</Button.Button>
+    {/if}
 </Card.Base>
 
 <style>
-    .actions {
+    :global(.message-action) {
+        padding-bottom: var(--space-2);
+        position: relative;
+    }
+
+    :global(.actions) {
         padding: var(--space-6);
         position: relative;
         margin-left: var(--space-6);
@@ -90,6 +112,13 @@
             display: flex;
             align-items: center;
             gap: 4px;
+
+            .filename {
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                flex: 1;
+            }
         }
 
         .icon {
@@ -102,6 +131,19 @@
             position: relative;
             z-index: 10;
         }
+    }
+
+    .wrap {
+        transition: all ease-out 175ms;
+    }
+
+    :global(.show-more) {
+        position: absolute;
+        bottom: -12px;
+        background-color: black;
+        left: 50%;
+        transform: translateX(-50%);
+        padding: var(--space-1) var(--space-2) !important;
     }
 
     .grid-line {
