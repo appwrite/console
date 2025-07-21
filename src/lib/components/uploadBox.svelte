1be8468fc63368fe75456c81511da23087070218
Darshan <itznotabug@gmail.com>
2025-07-21 14:32:19 +0530

    fix: uneven upload box widths.

--- src/lib/components/uploadBox.svelte
+++ src/lib/components/uploadBox.svelte
@@ -4,13 +4,26 @@
 </script>
 
 {#if $uploader?.isOpen}
-    <Upload.Box
-        files={$uploader.files.map((file) => {
-            return {
-                name: file.name,
-                size: file.size,
-                status: file.status
-            };
-        })}
-        on:close={() => uploader.close()} />
+    <div class="upload-box-wrapper">
+        <Upload.Box
+            files={$uploader.files.map((file) => {
+                return {
+                    name: file.name,
+                    size: file.size,
+                    status: file.status
+                };
+            })}
+            on:close={() => uploader.close()} />
+    </div>
 {/if}
+
+<style lang="scss">
+    .upload-box-wrapper {
+        min-width: 400px;
+        max-width: 100vw;
+
+        & :global(.upload-box) {
+            width: 100%;
+        }
+    }
+</style>
