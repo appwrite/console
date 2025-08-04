2921432e208e629d62fbaac29890cd218ac9e952
Darshan <itznotabug@gmail.com>
2025-08-04 09:53:08 +0530

    fix: import realtime subscription.

--- src/lib/components/csvImportBox.svelte
+++ src/lib/components/csvImportBox.svelte
@@ -150,7 +150,7 @@
                 migrations.migrations.forEach(updateOrAddItem);
             });
 
-        return sdk.forConsole.client.subscribe('console', (response) => {
+        return sdk.forConsoleIn(page.params.region).client.subscribe('console', (response) => {
             if (!response.channels.includes(`projects.${getProjectId()}`)) return;
             if (response.events.includes('migrations.*')) {
                 updateOrAddItem(response.payload as Payload);
