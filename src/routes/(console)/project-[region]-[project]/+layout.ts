3a38ca4750067ba2b9840df8337f03937533e8f4
Darshan <itznotabug@gmail.com>
2025-08-09 11:05:28 +0530

    linting.

--- src/routes/(console)/project-[region]-[project]/+layout.ts
+++ src/routes/(console)/project-[region]-[project]/+layout.ts
@@ -48,10 +48,12 @@ export const load: LayoutLoad = async ({ params, depends, parent }) => {
     const scopes = rolesResult?.scopes ?? defaultScopes;
 
     if (prefs?.organization !== project.teamId) {
-        sdk.forConsole.account.updatePrefs({ prefs: {
-            ...prefs,
-            organization: project.teamId
-        } });
+        sdk.forConsole.account.updatePrefs({
+            prefs: {
+                ...prefs,
+                organization: project.teamId
+            }
+        });
     }
 
     preferences.loadTeamPrefs(project.teamId);
