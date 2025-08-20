1fc6cbb470060f577d09fba89110e977c955afd5
Damodar Lohani <dlohani48@gmail.com>
2025-08-20 02:26:45 +0000

    Fix: adding payment method error when PM is empty

--- src/lib/components/billing/paymentModal.svelte
+++ src/lib/components/billing/paymentModal.svelte
@@ -42,7 +42,7 @@
 
             const card = await submitStripeCard(name, page?.params?.organization ?? null);
             if (card && Object.hasOwn(card, 'id')) {
-                if ((card as PaymentMethod).card.country === 'US') {
+                if ((card as PaymentMethod).card?.country === 'US') {
                     paymentMethod = card as PaymentMethod;
                     showState = true;
                     return;
