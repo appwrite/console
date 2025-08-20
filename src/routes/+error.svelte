229116177d3e77b36e0d635e692d0f664acd50a1
Harsh Mahajan <127186841+HarshMN2345@users.noreply.github.com>
2025-08-20 13:46:37 +0530

    fix: added pink components instead of normal components

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,75 +2,15 @@
     import { base } from '$app/paths';
     import { Unauthenticated } from '$lib/layout';
     import { Button } from '$lib/elements/forms';
+    import { Badge, Typography, Layout } from '@appwrite.io/pink-svelte';
 </script>
 
 <Unauthenticated>
-    <div class="content">
-        <div class="center">
-            <span class="error-badge">404 Page not found</span>
-        </div>
-        <h1 class="error-title">The page you're looking for doesn't exist.</h1>
-        <div class="center">
-            <a href={base}>
-                <Button>Back to console</Button>
-            </a>
-        </div>
-    </div>
+    <Layout.Stack gap="l" alignItems="center">
+        <Badge variant="secondary" content="404 Page not found" />
+        <Typography.Title size="l" align="center">
+            The page you're looking for doesn't exist.
+        </Typography.Title>
+        <Button href={base}>Back to console</Button>
+    </Layout.Stack>
 </Unauthenticated>
-
-<style>
-    .content {
-        text-align: center;
-    }
-
-    .error-badge {
-        padding: var(--space-1, 2px) var(--space-3, 6px);
-        border-radius: var(--border-radius-XS, 6px);
-        background: rgba(0, 0, 0, 0.08);
-        color: #56565c;
-        text-align: center;
-        font-family: var(--font-family-sansSerif, Inter), sans-serif;
-        font-size: var(--font-size-S, 14px);
-        font-style: normal;
-        font-weight: 400;
-        line-height: 140%;
-        letter-spacing: -0.063px;
-        display: inline-block;
-    }
-
-    .error-title {
-        color: #2d2d31;
-        text-align: center;
-        font-family: var(--font-family-sansSerif, Inter), sans-serif;
-        font-size: var(--font-size-XXXL, 32px);
-        font-style: normal;
-        font-weight: 400;
-        line-height: 140%;
-        letter-spacing: -0.144px;
-        margin-top: 8px;
-        margin-bottom: 32px;
-    }
-
-    .center {
-        display: flex;
-        justify-content: center;
-    }
-
-    :global(.theme-light) .error-badge {
-        background: rgba(0, 0, 0, 0.08);
-        color: #56565c;
-    }
-
-    :global(.theme-light) .error-title {
-        color: #2d2d31;
-    }
-
-    :global(.theme-dark) .error-badge {
-        background: rgba(255, 255, 255, 0.2);
-        color: #c3c3c6;
-    }
-
-    :global(.theme-dark) .error-title {
-        color: #ededf0;
-    }
-</style>
