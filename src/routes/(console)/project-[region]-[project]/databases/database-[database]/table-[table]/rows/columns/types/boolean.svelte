7af2514d2c00b3486baf851406a84c75a9af6f55
Darshan <itznotabug@gmail.com>
2025-08-29 19:53:54 +0530

    address comment.

--- src/routes/(console)/project-[region]-[project]/databases/database-[database]/table-[table]/rows/columns/types/boolean.svelte
+++ src/routes/(console)/project-[region]-[project]/databases/database-[database]/table-[table]/rows/columns/types/boolean.svelte
@@ -13,18 +13,20 @@
         label = undefined;
         optionalText = undefined;
     }
+
+    $: options = [
+        !column.required && { label: 'NULL', value: null },
+        { label: limited ? 'true' : 'True', value: true },
+        { label: limited ? 'false' : 'False', value: false }
+    ].filter(Boolean);
 </script>
 
 <InputSelect
     {id}
     {label}
     bind:value
+    {options}
     {optionalText}
     autofocus={limited}
     placeholder="Select a value"
-    required={column.required}
-    options={[
-        !column.required && { label: 'NULL', value: null },
-        { label: 'True', value: true },
-        { label: 'False', value: false }
-    ].filter(Boolean)} />
+    required={column.required} />
