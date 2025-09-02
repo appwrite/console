57e072c1a380df8468c8d8539b36da6cea315a3d
Harsh Mahajan <127186841+HarshMN2345@users.noreply.github.com>
2025-09-02 14:55:57 +0530

    feat: promo inversion queries

--- src/routes/(console)/bottomAlerts.ts
+++ src/routes/(console)/bottomAlerts.ts
@@ -3,8 +3,8 @@ import { isSameDay } from '$lib/helpers/date';
 import { type BottomModalAlertItem, showBottomModalAlert } from '$lib/stores/bottom-alerts';
 import TablesApiLight from '$lib/images/promos/tables-api-light.png';
 import TablesApiDark from '$lib/images/promos/tables-api-dark.png';
-import TimestampOverridesDark from '$lib/images/promos/timestamp-overrides-dark.png';
-import TimestampOverridesLight from '$lib/images/promos/timestamp-overrides-light.png';
+import InversionQueriesDark from '$lib/images/promos/inversion-queries-dark.png';
+import InversionQueriesLight from '$lib/images/promos/inversion-queries-light.png';
 import OptInRelationDark from '$lib/images/promos/opt-relation-dark.png';
 import OptInRelationLight from '$lib/images/promos/opt-relation-light.png';
 
@@ -32,21 +32,20 @@ if (isCloud) {
         show: true
     };
 
-    const timestampOverridesPromo: BottomModalAlertItem = {
-        id: 'modal:timestamp_overrides_announcement',
+    const inversionQueriesPromo: BottomModalAlertItem = {
+        id: 'modal:inversion_queries_announcement',
         src: {
-            dark: TimestampOverridesDark,
-            light: TimestampOverridesLight
+            dark: InversionQueriesDark,
+            light: InversionQueriesLight
         },
-        title: 'Announcing Timestamp Overrides',
-        message:
-            'Move historical data into Appwrite without losing context or disrupting chronological accuracy.',
+        title: 'Announcing Inversion queries',
+        message: 'New NOT operators to exclude data directly in queries.',
         plan: 'free',
         importance: 8,
         scope: 'project',
         cta: {
             text: 'Read announcement',
-            link: () => 'https://appwrite.io/blog/post/announcing-timestamp-overrides',
+            link: () => 'https://appwrite.io/blog/post/announcing-inversion-queries',
             external: true,
             hideOnClick: true
         },
@@ -73,7 +72,7 @@ if (isCloud) {
         },
         show: true
     };
-    listOfPromotions.push(optInRelationPromo, tablesApiPromo, timestampOverridesPromo);
+    listOfPromotions.push(inversionQueriesPromo, optInRelationPromo, tablesApiPromo);
 }
 
 export function addBottomModalAlerts() {
