49f08b6fbbf3cb662fe1a1254c5a9df446b970a5
Khushboo Verma <khush.vv@gmail.com>
2025-09-03 13:14:08 +0530

    Add view certificate logs option

--- src/routes/(console)/project-[region]-[project]/sites/site-[site]/domains/table.svelte
+++ src/routes/(console)/project-[region]-[project]/sites/site-[site]/domains/table.svelte
@@ -3,7 +3,12 @@
     import { Link } from '$lib/elements';
     import { Button } from '$lib/elements/forms';
     import type { Models } from '@appwrite.io/console';
-    import { IconDotsHorizontal, IconRefresh, IconTrash } from '@appwrite.io/pink-icons-svelte';
+    import {
+        IconDotsHorizontal,
+        IconRefresh,
+        IconTrash,
+        IconTerminal
+    } from '@appwrite.io/pink-icons-svelte';
     import {
         ActionMenu,
         Badge,
@@ -11,10 +16,12 @@
         Layout,
         Popover,
         Table,
-        Typography
+        Typography,
+        Divider
     } from '@appwrite.io/pink-svelte';
     import DeleteDomainModal from './deleteDomainModal.svelte';
     import RetryDomainModal from './retryDomainModal.svelte';
+    import ViewLogsModal from './viewLogsModal.svelte';
     import { columns } from './store';
     import { regionalProtocol } from '$routes/(console)/project-[region]-[project]/store';
 
@@ -26,6 +33,7 @@
 
     let showDelete = $state(false);
     let showRetry = $state(false);
+    let showLogs = $state(false);
     let selectedProxyRule: Models.ProxyRule = $state(null);
 
     const proxyTarget = (proxy: Models.ProxyRule) => {
@@ -91,6 +99,17 @@
 
                         <svelte:fragment slot="tooltip" let:toggle>
                             <ActionMenu.Root>
+                                {#if rule.logs}
+                                    <ActionMenu.Item.Button
+                                        leadingIcon={IconTerminal}
+                                        on:click={(e) => {
+                                            selectedProxyRule = rule;
+                                            showLogs = true;
+                                            toggle(e);
+                                        }}>
+                                        View logs
+                                    </ActionMenu.Item.Button>
+                                {/if}
                                 {#if rule.status !== 'verified' && rule.status !== 'verifying'}
                                     <ActionMenu.Item.Button
                                         leadingIcon={IconRefresh}
@@ -101,6 +120,14 @@
                                         }}>
                                         Retry
                                     </ActionMenu.Item.Button>
+                                {/if}
+                                {#if rule.logs || (rule.status !== 'verified' && rule.status !== 'verifying')}
+                                    <div
+                                        style:margin-inline="-1rem"
+                                        style:padding-block-start="0.25rem"
+                                        style:padding-block-end="0.25rem">
+                                        <Divider />
+                                    </div>
                                 {/if}
                                 <ActionMenu.Item.Button
                                     status="danger"
@@ -130,4 +157,8 @@
 
 {#if showRetry}
     <RetryDomainModal bind:show={showRetry} {selectedProxyRule} />
+{/if}
+
+{#if showLogs}
+    <ViewLogsModal bind:show={showLogs} {selectedProxyRule} />
 {/if}
