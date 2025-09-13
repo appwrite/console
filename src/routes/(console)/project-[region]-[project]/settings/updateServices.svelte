d4165fec1598469b8be481acab9eb89b85e3f8c3
Darshan <itznotabug@gmail.com>
2025-09-13 10:04:08 +0530

    update: improve and inline project service UX.

--- src/routes/(console)/project-[region]-[project]/settings/updateServices.svelte
+++ src/routes/(console)/project-[region]-[project]/settings/updateServices.svelte
@@ -8,22 +8,36 @@
     import { services, type Service } from '$lib/stores/project-services';
     import { sdk } from '$lib/stores/sdk';
     import { project } from '../store';
-    import EnableAllServices from './enableAllServices.svelte';
-    import DisableAllServices from './disableAllServices.svelte';
     import Button from '$lib/elements/forms/button.svelte';
-    import { Divider, Layout } from '@appwrite.io/pink-svelte';
+    import { Dialog, Divider, Layout, Spinner } from '@appwrite.io/pink-svelte';
+    import type { ApiService } from '@appwrite.io/console';
 
-    let showDisableAll = false;
-    let showEnableAll = false;
+    let isUpdatingStatus = $state(false);
+    let showUpdateServiceDialog = $state(false);
+    let updateServicesEnabledMode = $state<boolean | null>(null);
+
+    let singleApiServiceUpdate = $state<ApiService | null>(null);
+
+    const allServicesEnabled = $derived.by(
+        () => isUpdatingStatus || $services.list.every((service) => service.value)
+    );
+    const allServicesDisabled = $derived.by(
+        () => isUpdatingStatus || $services.list.every((service) => !service.value)
+    );
 
     async function serviceUpdate(service: Service) {
+        isUpdatingStatus = true;
+        singleApiServiceUpdate = service.method;
+
         try {
             await sdk.forConsole.projects.updateServiceStatus({
                 projectId: $project.$id,
                 service: service.method,
                 status: service.value
             });
+
             await invalidate(Dependencies.PROJECT);
+
             addNotification({
                 type: 'success',
                 message: `${service.label} service has been ${
@@ -40,25 +54,23 @@
                 message: error.message
             });
             trackError(error, Submit.ProjectService);
+        } finally {
+            isUpdatingStatus = false;
+            singleApiServiceUpdate = null;
         }
     }
 
     async function toggleAllServices(status: boolean) {
-        if (status && !showEnableAll) {
-            showEnableAll = true;
-            return;
-        }
-        if (!status && !showDisableAll) {
-            showDisableAll = true;
-            return;
-        }
+        isUpdatingStatus = true;
 
         try {
             await sdk.forConsole.projects.updateServiceStatusAll({
                 projectId: $project.$id,
                 status
             });
-            invalidate(Dependencies.PROJECT);
+
+            await invalidate(Dependencies.PROJECT);
+
             addNotification({
                 type: 'success',
                 message:
@@ -75,12 +87,30 @@
             });
             trackError(error, Submit.ProjectService);
         } finally {
-            showDisableAll = false;
-            showEnableAll = false;
+            isUpdatingStatus = false;
+            showUpdateServiceDialog = false;
+            updateServicesEnabledMode = null;
         }
     }
 
-    $: services.load($project);
+    const dialogDetails = $derived.by(() => {
+        if (updateServicesEnabledMode) {
+            return {
+                title: 'Enable all services',
+                message: 'All project services will be enabled.',
+                actionButton: 'Enable all'
+            };
+        } else {
+            return {
+                title: 'Disable all services',
+                message:
+                    'Are you sure you want to disable all services? This will disable API requests to this project for all Client SDKs.',
+                actionButton: 'Disable all'
+            };
+        }
+    });
+
+    $effect(() => services.load($project));
 </script>
 
 <CardGrid>
@@ -90,22 +120,43 @@
     <svelte:fragment slot="aside">
         <Layout.Stack gap="m">
             <Layout.Stack direction="row" alignItems="center" gap="s">
-                <Button extraCompact on:click={() => toggleAllServices(true)}>Enable all</Button>
+                <Button
+                    extraCompact
+                    on:click={() => {
+                        showUpdateServiceDialog = true;
+                        updateServicesEnabledMode = true;
+                    }}
+                    disabled={allServicesEnabled}>Enable all</Button>
                 <span style:height="20px">
                     <Divider vertical />
                 </span>
-                <Button extraCompact on:click={() => toggleAllServices(false)}>Disable all</Button>
+                <Button
+                    extraCompact
+                    on:click={() => {
+                        showUpdateServiceDialog = true;
+                        updateServicesEnabledMode = false;
+                    }}
+                    disabled={allServicesDisabled}>Disable all</Button>
             </Layout.Stack>
             <Layout.Stack gap="l">
                 <Divider />
                 <Layout.Stack direction="row" wrap="wrap">
                     {#each $services.list as service}
                         <span style:flex-basis="30%">
-                            <InputSwitch
-                                label={service.label}
-                                id={service.method}
-                                bind:value={service.value}
-                                on:change={() => serviceUpdate(service)} />
+                            <Layout.Stack direction="row" alignItems="center">
+                                <InputSwitch
+                                    id={service.method}
+                                    label={service.label}
+                                    bind:value={service.value}
+                                    on:change={() => serviceUpdate(service)}
+                                    disabled={singleApiServiceUpdate === service.method} />
+
+                                {#if singleApiServiceUpdate === service.method}
+                                    <span style:opacity="0.75">
+                                        <Spinner size="s" />
+                                    </span>
+                                {/if}
+                            </Layout.Stack>
                         </span>
                     {/each}
                 </Layout.Stack>
@@ -114,5 +165,20 @@
     </svelte:fragment>
 </CardGrid>
 
-<DisableAllServices handleDisableAll={() => toggleAllServices(false)} bind:show={showDisableAll} />
-<EnableAllServices handleEnableAll={() => toggleAllServices(true)} bind:show={showEnableAll} />
+<Dialog title={dialogDetails.title} bind:open={showUpdateServiceDialog}>
+    <p class="text" data-private>{dialogDetails.message}</p>
+    <svelte:fragment slot="footer">
+        <Layout.Stack direction="row" gap="s" justifyContent="flex-end">
+            <Button text on:click={() => (showUpdateServiceDialog = false)}>Cancel</Button>
+
+            <Button
+                secondary
+                submissionLoader
+                disabled={isUpdatingStatus}
+                forceShowLoader={isUpdatingStatus}
+                on:click={() => toggleAllServices(updateServicesEnabledMode)}>
+                {dialogDetails.actionButton}
+            </Button>
+        </Layout.Stack>
+    </svelte:fragment>
+</Dialog>
