a86d4f416b3ef38a363557e4dc2a955fb0cdbdbd
Hemachandar <hmac.devo@gmail.com>
2025-09-26 20:39:06 +0530

    Redesign authorize contributor page

--- src/routes/(authenticated)/git/authorize-contributor/+page.svelte
+++ src/routes/(authenticated)/git/authorize-contributor/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-    import { app } from '$lib/stores/app';
-    import AppwriteLogoDark from '$lib/images/appwrite-logo-dark.svg';
-    import AppwriteLogoLight from '$lib/images/appwrite-logo-light.svg';
     import { Vcs, Client } from '@appwrite.io/console';
     import { onMount } from 'svelte';
     import { getApiEndpoint } from '$lib/stores/sdk';
+    import { Badge, Layout, Typography } from '@appwrite.io/pink-svelte';
+    import { Button } from '$lib/elements/forms';
 
     export let data;
 
@@ -52,49 +51,15 @@
     }
 </script>
 
-<section class="container" style="display: grid; place-items: center; min-height: 100vh;">
-    <div class="u-flex u-flex-vertical u-cross-center" style="width: 100%">
-        <div class="card" style="min-width: 600px; max-width: 100%;">
-            <h1 class="heading-level-2">Authorize External Deployment</h1>
-            <small style="margin-block-start: 8px;display: block;"
-                >The deployment for pull request <code class="inline-code"
-                    >#{providerPullRequestId}</code> is awaiting approval. When authorized, deployments
-                will be started.
-            </small>
-
-            <div class="with-borders" style="margin-block-start: 1rem;display: block;">
-                <button disabled={loading} on:click={approveDeployment} class="button" type="button"
-                    >Approve Deployment</button>
-            </div>
-
-            {#if error}
-                <p style="margin-block-start: 1rem" class="u-color-text-danger">{error}</p>
-            {/if}
-
-            {#if success}
-                <p style="margin-block-start: 1rem" class="u-color-text-success">{success}</p>
-            {/if}
-        </div>
-        <div class="u-gap-4 u-flex u-main-center u-cross-center" style="margin-block-start: 2rem;">
-            <span class="text">Powered by</span>
-            <a
-                href="https://appwrite.io/"
-                target="_blank"
-                style="display: grid;place-items: center;">
-                {#if $app.themeInUse === 'dark'}
-                    <img
-                        src={AppwriteLogoDark}
-                        width="120"
-                        class="u-block u-only-dark"
-                        alt="Appwrite Logo" />
-                {:else}
-                    <img
-                        src={AppwriteLogoLight}
-                        width="120"
-                        class="u-block u-only-light"
-                        alt="Appwrite Logo" />
-                {/if}
-            </a>
-        </div>
-    </div>
+<section class="container" style="display: grid; place-items: center;">
+    <Layout.Stack gap="l" alignItems="center" style="width: 500px;">
+        {#if error}
+            <Badge type="error" variant="secondary" content={error} />
+        {/if}
+        <Typography.Title size="l" align="center" variant="m-500">
+            The deployment for pull request #{providerPullRequestId} 
+            is awaiting approval. When authorized, deployments will be started.
+        </Typography.Title>
+        <Button on:click={approveDeployment} secondary>Approve Deployment</Button>
+    </Layout.Stack>
 </section>
