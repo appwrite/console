19400e8e383f368ddb297127ee5bf06d483d58dd
Harsh Mahajan <127186841+HarshMN2345@users.noreply.github.com>
2025-10-13 14:26:46 +0530

    new permissions table

--- src/lib/components/permissions/row.svelte
+++ src/lib/components/permissions/row.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+    import { type ComponentProps } from 'svelte';
     import { sdk } from '$lib/stores/sdk';
     import type { Models } from '@appwrite.io/console';
     import { AvatarInitials } from '../';
+    import { isSmallViewport } from '$lib/stores/viewport';
     import {
-        Button,
+        Badge,
         Divider,
         Icon,
+        InteractiveText,
         Layout,
         Link,
         Popover,
@@ -13,11 +16,14 @@
         Typography
     } from '@appwrite.io/pink-svelte';
     import Avatar from '../avatar.svelte';
-    import { IconAnonymous, IconExternalLink, IconMinusSm } from '@appwrite.io/pink-icons-svelte';
-    import { base } from '$app/paths';
+    import { IconAnonymous, IconMinusSm } from '@appwrite.io/pink-icons-svelte';
     import { page } from '$app/state';
+    import { menuOpen } from '$lib/components/menu/store';
+    import { base } from '$app/paths';
+    import { formatName } from '$lib/helpers/string';
 
     export let role: string;
+    export let placement: ComponentProps<Popover>['placement'] = 'bottom-start';
 
     async function getData(
         permission: string
@@ -39,6 +45,20 @@
             return team;
         }
     }
+
+    let isMouseOverTooltip = false;
+    function hidePopover(hideTooltip: () => void, timeout = true) {
+        if (!timeout) {
+            isMouseOverTooltip = false;
+            return hideTooltip();
+        }
+
+        setTimeout(() => {
+            if (!isMouseOverTooltip) {
+                hideTooltip();
+            }
+        }, 150);
+    }
 </script>
 
 {#if role === 'users'}
@@ -48,70 +68,120 @@
 {:else if role === 'any'}
     <div>Any</div>
 {:else}
-    <Popover let:toggle placement="bottom-start">
-        <Link.Button on:click={toggle}>{role}</Link.Button>
-        <div let:showing slot="tooltip" style:width="200px">
-            {#key showing}
-                {#await getData(role)}
-                    <Layout.Stack alignItems="center">
-                        <Spinner />
-                    </Layout.Stack>
-                {:then data}
-                    {@const isUser = role.startsWith('user')}
-                    {@const isTeam = role.startsWith('team')}
-                    {@const isAnonymous = !data.email && !data.phone && !data.name && isUser}
-                    <Layout.Stack>
-                        <Layout.Stack direction="row" gap="s" alignItems="center">
-                            {#if isAnonymous}
-                                <Avatar alt="avatar" size="xs">
-                                    <Icon icon={IconAnonymous} size="s" />
-                                </Avatar>
-                            {:else if data.name}
-                                <AvatarInitials name={data.name} size="xs" />
-                            {:else}
-                                <Avatar alt="avatar" size="xs">
-                                    <Icon icon={IconMinusSm} size="s" />
-                                </Avatar>
-                            {/if}
-                            <Typography.Text truncate color="--fgcolor-neutral-primary">
-                                {data.name ?? data?.email ?? data?.phone ?? '-'}
-                            </Typography.Text>
-                        </Layout.Stack>
+    <Popover let:show let:hide {placement} portal>
+        <button
+            on:mouseenter={() => {
+                if (!$menuOpen) {
+                    setTimeout(show, 150);
+                }
+            }}
+            on:mouseleave={() => hidePopover(hide)}>
+            <slot>
+                <Layout.Stack direction="row" gap="s" alignItems="center" inline>
+                    <Typography.Text>
+                        {#await getData(role)}
+                            {role}
+                        {:then data}
+                            {formatName(
+                                data.name ?? data?.email ?? data?.phone ?? '-',
+                                $isSmallViewport ? 5 : 12
+                            )}
+                        {/await}
+                    </Typography.Text>
+                    <Badge
+                        size="xs"
+                        variant="secondary"
+                        content={role.startsWith('user') ? 'User' : 'Team'} />
+                </Layout.Stack>
+            </slot>
+        </button>
 
-                        <Divider />
-                        {#if isUser}
-                            {#if data?.email}
-                                <Typography.Text truncate>Email: {data?.email}</Typography.Text>
+        <div
+            let:hide
+            let:showing
+            slot="tooltip"
+            role="tooltip"
+            style:padding="1rem"
+            style:margin="-1rem"
+            on:mouseenter={() => (isMouseOverTooltip = true)}
+            on:mouseleave={() => hidePopover(hide, false)}>
+            {#if showing}
+                <Layout.Stack gap="s" alignContent="flex-start">
+                    {#await getData(role)}
+                        <Layout.Stack alignItems="center">
+                            <Spinner />
+                        </Layout.Stack>
+                    {:then data}
+                        {@const isUser = role.startsWith('user')}
+                        {@const isAnonymous = !data.email && !data.phone && !data.name && isUser}
+                        {@const id = role.split(':')[1].split('/')[0]}
+
+                        <Layout.Stack gap="s" alignItems="flex-start">
+                            <Layout.Stack
+                                direction="row"
+                                gap="s"
+                                alignItems="center"
+                                justifyContent="flex-start">
+                                {#if isAnonymous}
+                                    <Avatar alt="avatar" size="m">
+                                        <Icon icon={IconAnonymous} size="s" />
+                                    </Avatar>
+                                {:else if data.name}
+                                    <AvatarInitials name={data.name} size="m" />
+                                {:else}
+                                    <Avatar alt="avatar" size="m">
+                                        <Icon icon={IconMinusSm} size="s" />
+                                    </Avatar>
+                                {/if}
+
+                                <Layout.Stack alignItems="flex-start" gap="xxs">
+                                    <Layout.Stack style="padding-left: 0.25rem;">
+                                        <Link.Anchor
+                                            variant="quiet"
+                                            href={role.startsWith('user')
+                                                ? `${base}/project-${page.params.region}-${page.params.project}/auth/user-${id}`
+                                                : `${base}/project-${page.params.region}-${page.params.project}/auth/teams/team-${id}`}>
+                                            <Typography.Text
+                                                size="s"
+                                                color="--fgcolor-neutral-primary">
+                                                {formatName(
+                                                    data.name ?? data?.email ?? data?.phone ?? '-',
+                                                    $isSmallViewport ? 12 : 24
+                                                )}
+                                            </Typography.Text>
+                                        </Link.Anchor>
+                                    </Layout.Stack>
+                                    <InteractiveText
+                                        isVisible
+                                        variant="copy"
+                                        text={id}
+                                        value={id} />
+                                </Layout.Stack>
+                            </Layout.Stack>
+
+                            {#if isUser && (data.email || data.phone)}
+                                <Divider />
+                                <Layout.Stack gap="xs" alignItems="flex-start">
+                                    {#if data.email}
+                                        <Typography.Text
+                                            size="xs"
+                                            color="--fgcolor-neutral-secondary">
+                                            Email: {data.email}
+                                        </Typography.Text>
+                                    {/if}
+                                    {#if data.phone}
+                                        <Typography.Text
+                                            size="xs"
+                                            color="--fgcolor-neutral-secondary">
+                                            Phone: {data.phone}
+                                        </Typography.Text>
+                                    {/if}
+                                </Layout.Stack>
                             {/if}
-                            {#if data?.phone}
-                                <Typography.Text truncate>Phone: {data?.phone}</Typography.Text>
-                            {/if}
-                            <div>
-                                <Button.Anchor
-                                    href={`${base}/project-${page.params.region}-${page.params.project}/auth/user-${data?.$id}`}
-                                    size="xs"
-                                    target="_blank"
-                                    variant="secondary">
-                                    View user
-                                    <Icon slot="end" icon={IconExternalLink} size="s" />
-                                </Button.Anchor>
-                            </div>
-                        {:else if isTeam}
-                            <Typography.Text>Members: {data?.total}</Typography.Text>
-                            <div>
-                                <Button.Anchor
-                                    href={`${base}/project-${page.params.region}-${page.params.project}/auth/teams/team-${data?.$id}`}
-                                    size="s"
-                                    target="_blank"
-                                    variant="secondary">
-                                    View team
-                                    <Icon slot="end" icon={IconExternalLink} size="s" />
-                                </Button.Anchor>
-                            </div>
-                        {/if}
-                    </Layout.Stack>
-                {/await}
-            {/key}
+                        </Layout.Stack>
+                    {/await}
+                </Layout.Stack>
+            {/if}
         </div>
     </Popover>
 {/if}
