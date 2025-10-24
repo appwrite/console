01f70ef170e2a77d1d26dd84ee7b9ede45213a17
Harsh Mahajan <127186841+HarshMN2345@users.noreply.github.com>
2025-10-24 17:48:17 +0530

    region name selection

--- e2e/steps/pro-project.ts
+++ e2e/steps/pro-project.ts
@@ -51,9 +51,13 @@ export async function createProProject(page: Page): Promise<Metadata> {
         await page.getByRole('button', { name: 'create project' }).first().click();
         const dialog = page.locator('dialog[open]');
         await dialog.getByPlaceholder('Project name').fill('test project');
+        const regionSelector = dialog.getByPlaceholder('Select a region');
+        await regionSelector.click();
+        await dialog.getByRole('option', { name: 'New York' }).click();
+
         await dialog.getByRole('button', { name: 'create' }).click();
-        await page.waitForURL(/\/project-nyc-[^/]+/);
-        expect(page.url()).toContain('/console/project-nyc-');
+        await page.waitForURL(/\/project-[^/]+-[^/]+/);
+        expect(page.url()).toMatch(/\/console\/project-[^/]+-/);
 
         return getProjectIdFromUrl(page.url());
     });
