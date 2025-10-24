c6a25dc874cc5f72c512382758ebb13b10315139
Harsh Mahajan <127186841+HarshMN2345@users.noreply.github.com>
2025-10-24 16:39:23 +0530

    Update src/routes/(console)/project-[region]-[project]/messaging/providers/create.svelte
    Co-authored-by: Darshan <itznotabug@gmail.com>

--- src/routes/(console)/project-[region]-[project]/messaging/providers/create.svelte
+++ src/routes/(console)/project-[region]-[project]/messaging/providers/create.svelte
@@ -186,7 +186,7 @@
     }
 </script>
 
-<Wizard title="Create provider" columnSize="l" confirmExit={true}>
+<Wizard title="Create provider" columnSize="l" confirmExit>
     <Form bind:this={formRef} onSubmit={create} isModal={false}>
         <Layout.Stack gap="xxl">
             <Fieldset legend="Provider">
