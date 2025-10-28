c85f5fd7ad18517e67ebc0413efedae9dc0803ea
Harsh Mahajan <127186841+HarshMN2345@users.noreply.github.com>
2025-10-28 19:19:18 +0530

    Update +page.svelte

--- src/routes/(console)/project-[region]-[project]/functions/create-function/manual/+page.svelte
+++ src/routes/(console)/project-[region]-[project]/functions/create-function/manual/+page.svelte
@@ -75,7 +75,7 @@
                     specification: specification || undefined
                 });
 
-            // Add domainn
+            // Add domain
             await sdk.forProject(page.params.region, page.params.project).proxy.createFunctionRule({
                 domain: `${ID.unique()}.${$regionalConsoleVariables._APP_DOMAIN_FUNCTIONS}`,
                 functionId: func.$id
