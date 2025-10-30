04415385b9bda7ee9a7693535681b3a62de26c5e
Darshan <itznotabug@gmail.com>
2025-10-30 15:36:55 +0530

    use: faster tests via dev mode.

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ const config: PlaywrightTestConfig = {
     retries: 3,
     testDir: 'e2e',
     use: {
-        baseURL: 'http://localhost:4173/console/',
+        baseURL: 'http://localhost:3000/console/',
         trace: 'on-first-retry'
     },
     webServer: {
@@ -19,8 +19,9 @@ const config: PlaywrightTestConfig = {
             PUBLIC_STRIPE_KEY:
                 'pk_test_51LT5nsGYD1ySxNCyd7b304wPD8Y1XKKWR6hqo6cu3GIRwgvcVNzoZv4vKt5DfYXL1gRGw4JOqE19afwkJYJq1g3K004eVfpdWn'
         },
-        command: 'pnpm run build && pnpm run preview',
-        port: 4173
+        command: 'pnpm run dev',
+        port: 3000,
+        reuseExistingServer: true
     }
 };
 
