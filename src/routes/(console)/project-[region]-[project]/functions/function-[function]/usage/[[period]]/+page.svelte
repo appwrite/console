728c8845a31e1fb375d5aa5ddf6aeab63122a4aa
Hemachandar <hmac.devo@gmail.com>
2025-11-06 16:17:13 +0530

    remove unnecessary function bandwidth

--- src/routes/(console)/project-[region]-[project]/functions/function-[function]/usage/[[period]]/+page.svelte
+++ src/routes/(console)/project-[region]-[project]/functions/function-[function]/usage/[[period]]/+page.svelte
@@ -2,12 +2,7 @@
     import { Container, Usage } from '$lib/layout';
     import { page } from '$app/state';
     import { base } from '$app/paths';
-    import { Icon, Layout, Typography } from '@appwrite.io/pink-svelte';
-    import Card from '$lib/components/card.svelte';
-    import { humanFileSize } from '$lib/helpers/sizeConvertion.js';
-    import { BarChart } from '$lib/charts/index.js';
-    import { total as totalMetrics } from '$lib/layout/usage.svelte';
-    import { IconChartSquareBar } from '@appwrite.io/pink-icons-svelte';
+    import { Layout } from '@appwrite.io/pink-svelte';
 
     let { data } = $props();
     let total = $derived(data.executionsTotal);
@@ -19,7 +14,6 @@
             value: metric.value / 1000 / 3600
         }))
         .filter(({ value }) => value));
-    let bandwidth = $state([]);
 </script>
 
 <Container>
@@ -42,47 +36,5 @@
             }}
             total={gbHoursTotal}
             count={gbHoursCount} />
-
-        <Card>
-            {@const humanized = humanFileSize(totalMetrics(bandwidth))}
-            <Layout.Stack gap="s" direction="column" alignItems="baseline">
-                <Layout.Stack gap="s" direction="row" alignItems="baseline">
-                    <Typography.Title>
-                        {humanized.value}
-                    </Typography.Title>
-                    <Typography.Text>{humanized.unit} bandwidth</Typography.Text>
-                </Layout.Stack>
-            </Layout.Stack>
-            {#if bandwidth?.length > 0}
-                <BarChart
-                    options={{
-                        yAxis: {
-                            axisLabel: {
-                                formatter: (value) =>
-                                    value
-                                        ? `${humanFileSize(+value).value} ${
-                                            humanFileSize(+value).unit
-                                        }`
-                                        : '0'
-                            }
-                        }
-                    }}
-                    series={[
-                        {
-                            name: 'Bandwidth',
-                            data: [...bandwidth.map((e) => [e.date, e.value])],
-                            tooltip: {
-                                valueFormatter: (value) =>
-                                    `${humanFileSize(+value).value} ${humanFileSize(+value).unit}`
-                            }
-                        }
-                    ]} />
-            {:else}
-                <Layout.Stack gap="xs" alignItems="center" justifyContent="center">
-                    <Icon icon={IconChartSquareBar} size="l" />
-                    <Typography.Text variant="m-600">No data to show</Typography.Text>
-                </Layout.Stack>
-            {/if}
-        </Card>
     </Layout.Stack>
 </Container>
--- src/routes/(console)/project-[region]-[project]/functions/usage/[[period]]/+page.svelte
+++ src/routes/(console)/project-[region]-[project]/functions/usage/[[period]]/+page.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
     import { base } from '$app/paths';
     import { page } from '$app/state';
-    import { BarChart } from '$lib/charts/index.js';
-    import Card from '$lib/components/card.svelte';
-    import { humanFileSize } from '$lib/helpers/sizeConvertion.js';
     import { Container, Usage } from '$lib/layout';
-    import { total as totalMetrics } from '$lib/layout/usage.svelte';
-    import { IconChartSquareBar } from '@appwrite.io/pink-icons-svelte';
-    import { Icon, Layout, Typography } from '@appwrite.io/pink-svelte';
 
     let { data } = $props();
     let total = $derived(data.executionsTotal);
@@ -19,7 +13,6 @@
             value: metric.value / 1000 / 3600
         }))
         .filter(({ value }) => value));
-    let bandwidth = $state([]);
 </script>
 
 <Container>
@@ -41,46 +34,4 @@
         }}
         total={gbHoursTotal}
         count={gbHoursCount} />
-
-    <Card>
-        {@const humanized = humanFileSize(totalMetrics(bandwidth))}
-        <Layout.Stack gap="s" direction="column" alignItems="baseline">
-            <Layout.Stack gap="s" direction="row" alignItems="baseline">
-                <Typography.Title>
-                    {humanized.value}
-                </Typography.Title>
-                <Typography.Text>{humanized.unit} bandwidth</Typography.Text>
-            </Layout.Stack>
-        </Layout.Stack>
-        {#if bandwidth?.length > 0}
-            <BarChart
-                options={{
-                    yAxis: {
-                        axisLabel: {
-                            formatter: (value) =>
-                                value
-                                    ? `${humanFileSize(+value).value} ${
-                                        humanFileSize(+value).unit
-                                    }`
-                                    : '0'
-                        }
-                    }
-                }}
-                series={[
-                    {
-                        name: 'Bandwidth',
-                        data: [...bandwidth.map((e) => [e.date, e.value])],
-                        tooltip: {
-                            valueFormatter: (value) =>
-                                `${humanFileSize(+value).value} ${humanFileSize(+value).unit}`
-                        }
-                    }
-                ]} />
-        {:else}
-            <Layout.Stack gap="xs" alignItems="center" justifyContent="center">
-                <Icon icon={IconChartSquareBar} size="l" />
-                <Typography.Text variant="m-600">No data to show</Typography.Text>
-            </Layout.Stack>
-        {/if}
-    </Card>
 </Container>
