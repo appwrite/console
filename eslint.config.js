c8df58dde0d0dcd0060a42f7bc211c45da3ea121
Darshan <itznotabug@gmail.com>
2026-01-06 19:26:10 +0530

    lint.

--- eslint.config.js
+++ eslint.config.js
@@ -34,9 +34,9 @@ export default ts.config(
             'svelte/no-reactive-reassign': 'off',
             'svelte/no-reactive-literals': 'off',
             // TODO: @itznotabug, this requires a big refactor!
-            'svelte/no-navigation-without-resolve': '"warn"',
-            'svelte/prefer-svelte-reactivity': '"warn"',
-            'svelte/prefer-writable-derived': '"warn"'
+            'svelte/no-navigation-without-resolve': 'warn',
+            'svelte/prefer-svelte-reactivity': 'warn',
+            'svelte/prefer-writable-derived': 'warn'
         }
     },
     {
