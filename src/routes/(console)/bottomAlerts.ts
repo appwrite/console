3fc58eca8cda987947d164c9cb63f0e607710acb
adityaoberai <adityaoberai1@gmail.com>
2026-01-28 12:38:57 +0530

    change link to dub.co

--- src/routes/(console)/bottomAlerts.ts
+++ src/routes/(console)/bottomAlerts.ts
@@ -34,8 +34,7 @@ if (isCloud && SHOW_IMAGINE_PROMO) {
                 light: '#333333',
                 dark: '#CCCCCC'
             },
-            link: () =>
-                'https://www.producthunt.com/products/imagine-2?launch=imagine-00018118-5267-43ba-b456-efeeb7cbfbc9',
+            link: () => 'https://apwr.dev/imagine-ph-appwrite-console',
             external: true,
             hideOnClick: true
         },
