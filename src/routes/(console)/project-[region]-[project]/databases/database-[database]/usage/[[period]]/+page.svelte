1f2aab991617f13068f7842346c65d6c13b77c9b
harsh mahajan <harshmahajan2345@gmail.com>
2026-04-30 17:40:07 +0530

    fix: remove databases reads/writes usage tab and clear loaders

--- src/routes/(console)/project-[region]-[project]/databases/database-[database]/usage/[[period]]/+page.svelte
+++ src/routes/(console)/project-[region]-[project]/databases/database-[database]/usage/[[period]]/+page.svelte
@@ -1,24 +1,5 @@
 <script lang="ts">
-    import { Container, UsageMultiple } from '$lib/layout';
-
-    export let data;
-
-    $: reads = data.databaseReads;
-    $: readsTotal = data.databaseReadsTotal;
-
-    $: writes = data.databaseWrites;
-    $: writesTotal = data.databaseWritesTotal;
+    import { Container } from '$lib/layout';
 </script>
 
-<Container databasesMainScreen>
-    <UsageMultiple
-        title="Reads and writes"
-        showHeader={false}
-        total={[readsTotal, writesTotal]}
-        count={[reads, writes]}
-        legendNumberFormat="abbreviate"
-        legendData={[
-            { name: 'Reads', value: readsTotal },
-            { name: 'Writes', value: writesTotal }
-        ]} />
-</Container>
+<Container></Container>
--- src/routes/(console)/project-[region]-[project]/databases/usage/[[period]]/+page.svelte
+++ src/routes/(console)/project-[region]-[project]/databases/usage/[[period]]/+page.svelte
@@ -1,28 +1,5 @@
 <script lang="ts">
-    import { Container, UsageMultiple } from '$lib/layout';
-    import { Layout } from '@appwrite.io/pink-svelte';
-    import type { PageData } from './$types';
-
-    export let data: PageData;
-
-    $: reads = data.databasesReads;
-    $: readsTotal = data.databasesReadsTotal;
-
-    $: writes = data.databasesWrites;
-    $: writesTotal = data.databasesWritesTotal;
+    import { Container } from '$lib/layout';
 </script>
 
-<Container>
-    <Layout.Stack gap="l">
-        <UsageMultiple
-            title="Reads and writes"
-            showHeader={false}
-            total={[readsTotal, writesTotal]}
-            count={[reads, writes]}
-            legendNumberFormat="abbreviate"
-            legendData={[
-                { name: 'Reads', value: readsTotal },
-                { name: 'Writes', value: writesTotal }
-            ]} />
-    </Layout.Stack>
-</Container>
+<Container></Container>
