91d3463e36be4175c638081e8232666ddaa4383c
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-05-08 20:08:51 +0530

    updated

--- src/routes/(console)/project-[region]-[project]/auth/settings/+page.svelte
+++ src/routes/(console)/project-[region]-[project]/auth/settings/+page.svelte
@@ -10,7 +10,7 @@
     import { addNotification } from '$lib/stores/notifications';
     import { oAuthProviders } from '$lib/stores/oauth-providers';
     import { sdk } from '$lib/stores/sdk';
-    import { AuthMethod, type Models } from '@appwrite.io/console';
+    import { AuthMethod as MethodId, type Models } from '@appwrite.io/console';
     import { base } from '$app/paths';
     import {
         Avatar,
@@ -38,7 +38,7 @@
     let isUpdatingAllAuthMethods = $state(false);
     let showUpdateAuthMethodsDialog = $state(false);
     let updateAuthMethodsEnabledMode = $state<boolean | null>(null);
-    let apiAuthMethodUpdates = new SvelteSet<AuthMethod>();
+    let apiAuthMethodUpdates = new SvelteSet<MethodId>();
 
     const isAnyAuthMethodUpdating = $derived(apiAuthMethodUpdates.size > 0);
     const isAnyUpdateInProgress = $derived(isUpdatingAllAuthMethods || isAnyAuthMethodUpdating);
